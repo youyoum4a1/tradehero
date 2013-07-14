f3973c2b26874643d0f9ac64f1aca4963723e76e
MukulEATech <mukul@eatechnologies.com>
2013-07-14 19:26:24 +0530

    ADDED USER VARIABLE

--- src/android/tradehero/Utills/Constants.java
+++ src/android/tradehero/Utills/Constants.java
@@ -22,14 +22,15 @@ public class Constants {
 	public static final String CONTENT_TYPE="Content-Type";
 	public static final String CONTENT_TYPE_VALUE="application/x-www-form-urlencoded";
 	public static final String CHARSET="charset" ;
-	public static final String CHARSET_VALUE="utf-8";
+	public static final String CHARSET_VALUE="utf-8";	
 	//SingUp 
 	public static final String PASSWORD_CONFIRMATION ="passwordConfirmation";
 	public static final String PASSWORD="password";
 	public static final String DISPLAY_NAME="displayName";
 	public static final String FIRST_NAME="firstName";
-	public static final String LAST_NAME="lastName";
 	public static final String EMAIL="email";
+	public static final String LAST_NAME="lastName";
+	
 
 	//SignUp FaceBook
 	public static final String FB_ACCESS_TOKEN="facebook_access_token";
@@ -49,7 +50,7 @@ public class Constants {
 	public static final String CCPERMONTHBALANCE="ccPerMonthBalance";
 	//public static final String DISPLAY_NAME="displayName";
 	//public static final String EMAIL="email" ;
-	public static final String EMAIL_NOTIFICATION="emailNotificationsEnabled" A;
+	public static final String EMAIL_NOTIFICATION="emailNotificationsEnabled";
 	public static final String ENROLLED_PROVIDERS="enrolledProviders" ;
 	public static final String FB_LINKED="fbLinked";
 	public static final String FIRST_FOLLOW_ALL_TIME="firstFollowAllTime";
@@ -70,41 +71,37 @@ public class Constants {
 	public static final String PORTFOLIO_CURRENCY="Currency";
 	public static final String PORTFOLIO_CASH_BAL="cashBalance";
 	public static final String PORTFOLIO_COUNT_EXCHANGE="countExchanges";
-			countTrades = 0;
-			creationDate = "2013-07-10T17:23:16";
-			description = Default;
-			id = 235787;
-			initialCash = 100000;
-			markingAsOfUtc = "2013-07-10T17:23:16";
-			plM2D = 0;
-			plQ2D = 0;
-			plSinceInception = 0;
-			plY2D = 0;
-			roiM2D = 0;
-			roiM2DAnnualized = 0;
-			roiQ2D = 0;
-			roiQ2DAnnualized = 0;
-			roiSinceInception = "<null>";
-			roiSinceInceptionAnnualized = "<null>";
-			roiY2D = 0;
-			roiY2DAnnualized = 0;
-			title = Default;
-			totalExtraCashGiven = 0;
-			totalExtraCashPurchased = 0;
-			totalValue = 100000;
-			yahooSymbols =         (
-					);
-	};
-	pushNotificationsEnabled = 1;
-	rank =     (
-			);
-	thLinked = 0;
-	"tradesSharedCount_FB" = 0;
-	twLinked = 1;
-	unreadCount = 0;
-	useTHPrice = 0;
-	userAlertPlans =     (
-			);
-	website = "<null>";
-}
+	public static final String PORTFOLIO_COUNT_TRADE="countTrades";
+	public static final String PORTFOLIO_CREATION_DATE="creationDate";
+	public static final String PORTFOLIO_DESCRIPTION="description";
+	public static final String PORTFOLIO_ID="id";
+	public static final String PORTFOLIO_INTIAL_CASH="initialCash";
+	public static final String PORTFOLIO_MARKING_AS_OF_UTC="markingAsOfUtc";
+	public static final String PORTFOLIO_PI_M2D="plM2D";
+	public static final String PORTFOLIO_PI_Q2D="plQ2D"; 
+	public static final String PORTFOLIO_PI_SINCE_INCEPTION="plSinceInception";
+	public static final String PORTFOLIO_PI_Y2D	="plY2D";
+	public static final String PORTFOLIO_ROI_M2D="roiM2D";
+	public static final String PORTFOLIO_ROI_M2D_ANNUALIZES="roiM2DAnnualized";
+	public static final String PORTFOLIO_ROI_Q2D="roiQ2D";
+	public static final String PORTFOLIO_ROI_Q2D_ANNUALIZED	="roiQ2DAnnualized" ;
+	public static final String PORTFOLIO_ROI_SINCE_INCEPTION="roiSinceInception";
+	public static final String PORTFOLIO_ROI_SINCE_INCEPTION_ANNUALIZED	="roiSinceInceptionAnnualized";
+	public static final String PORTFOLIO_ROI_Y2D="roiY2D";
+	public static final String PORTFOLIO_ROI_Y2D_ANNUALIZED	="roiY2DAnnualized";
+	public static final String TITLE="title";
+	public static final String TOTAL_EXTRA_CASH_GIVEN="totalExtraCashGiven";
+	public static final String TOTAL_EXTRA_CASH_PURCHASED="totalExtraCashPurchased";
+	public static final String TOTAL_VALUE="totalValue";
+	public static final String YAHOO_SYMBOLS="yahooSymbols";        
+	public static final String PUSH_NOTIFICATION_IS_ENABLED="pushNotificationsEnabled" ;
+	public static final String RANK="rank";     
+	public static final String TH_LINKED="thLinked";
+	public static final String TRADE_SHARED_COUNT ="tradesSharedCount_FB" ;
+	public static final String TWITTER_LINK="twLinked" ;
+	public static final String UNREAD_COUNT="unreadCount";
+	public static final String USE_TH_PRICE="useTHPrice";
+	public static final String USER_ALERT_PLAN="userAlertPlans";
+	public static final String WEBSITE="website";
+
 }
