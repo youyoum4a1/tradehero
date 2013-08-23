3f0e2d3241c3d4d192b62324ee810aa7b7affbd0
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2013-08-23 14:25:54 +0800

    Animation for openning dashboard after logged in

--- TradeHero/TH_ANDROID/src/com/tradehero/th/activities/ActivityHelper.java
+++ TradeHero/TH_ANDROID/src/com/tradehero/th/activities/ActivityHelper.java
@@ -1,7 +1,9 @@
 package com.tradehero.th.activities;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import com.tradehero.th.R;
 import com.tradehero.th.application.App;
 
 /** Created with IntelliJ IDEA. User: tho Date: 8/14/13 Time: 6:28 PM Copyright (c) TradeHero */
@@ -14,10 +16,11 @@ public class ActivityHelper
         activity.startActivity(localIntent);
     }
 
-    public static void goRoot(Context activity)
+    public static void goRoot(Activity activity)
     {
         Intent localIntent = new Intent(App.context(), DashboardActivity.class);
         localIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
         activity.startActivity(localIntent);
+        activity.overridePendingTransition(R.anim.alpha_in, R.anim.alpha_out);
     }
 }
