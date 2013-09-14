87040aabf5fae72aa3d833f6675333ba096130ee
nguyentruongtho <nguyentruongtho.sg@gmail.com>
2013-09-14 18:22:52 +0800

    Use squareup's JacksonConverter

--- TradeHero/TH_ANDROID/src/com/tradehero/common/utils/JacksonConverter.java
+++ TradeHero/TH_ANDROID/src/com/tradehero/common/utils/JacksonConverter.java
@@ -1,14 +1,17 @@
 package com.tradehero.common.utils;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import retrofit.converter.ConversionException;
 import retrofit.converter.Converter;
-import retrofit.mime.MimeUtil;
+import retrofit.mime.TypedByteArray;
 import retrofit.mime.TypedInput;
 import retrofit.mime.TypedOutput;
 
@@ -18,94 +21,35 @@ import retrofit.mime.TypedOutput;
  */
 public class JacksonConverter implements Converter
 {
-    /**
-     * A {@link retrofit.converter.Converter} which uses Jackson for serialization and
-     * deserialization of entities.
-     */
-    private final ObjectMapper jacksonObjectMapper;
+  private static final String MIME_TYPE = "application/json; charset=UTF-8";
 
-    public JacksonConverter(ObjectMapper jacksonObjectMapper)
-    {
-        this.jacksonObjectMapper = jacksonObjectMapper;
+  private final ObjectMapper objectMapper;
+
+  public JacksonConverter(ObjectMapper objectMapper) {
+    this.objectMapper = objectMapper;
+  }
+
+  @Override public Object fromBody(TypedInput body, final Type type) throws ConversionException {
+    try {
+      final JavaType javaType = TypeFactory.defaultInstance().constructType(type);
+      return objectMapper.readValue(body.in(), javaType);
+    } catch (final JsonParseException e) {
+      throw new ConversionException(e);
+    } catch (final JsonMappingException e) {
+      throw new ConversionException(e);
+    } catch (final IOException e) {
+      throw new ConversionException(e);
     }
+  }
 
-    @Override
-    public Object fromBody(TypedInput body, Type type) throws ConversionException
-    {
-        String charset = "UTF-8";
-        if (body.mimeType() != null)
-        {
-            charset = MimeUtil.parseCharset(body.mimeType());
-        }
-        InputStreamReader isr = null;
-        try
-        {
-            isr = new InputStreamReader(body.in(), charset);
-            return jacksonObjectMapper.readValue(isr, type.getClass());
-        }
-        catch (IOException e)
-        {
-            throw new ConversionException(e);
-        }
-        finally
-        {
-            if (isr != null)
-            {
-                try
-                {
-                    isr.close();
-                }
-                catch (IOException ignored)
-                {
-                }
-            }
-        }
+  @Override public TypedOutput toBody(Object object) {
+    try {
+      final String json = objectMapper.writeValueAsString(object);
+      return new TypedByteArray(MIME_TYPE, json.getBytes("UTF-8"));
+    } catch (final JsonProcessingException e) {
+      throw new AssertionError(e);
+    } catch (final UnsupportedEncodingException e) {
+      throw new AssertionError(e);
     }
-
-    @Override
-    public TypedOutput toBody(Object object)
-    {
-        try
-        {
-            return new JsonTypedOutput(jacksonObjectMapper.writeValueAsBytes(object));
-        }
-        catch (JsonProcessingException e)
-        {
-            throw new AssertionError(e);
-        }
-    }
-
-    private static class JsonTypedOutput implements TypedOutput
-    {
-        private final byte[] jsonBytes;
-
-        JsonTypedOutput(byte[] jsonBytes)
-        {
-            this.jsonBytes = jsonBytes;
-        }
-
-        @Override
-        public String fileName()
-        {
-            return null;
-        }
-
-        @Override
-        public String mimeType()
-        {
-            return "application/json; charset=UTF-8";
-        }
-
-        @Override
-        public long length()
-        {
-            return jsonBytes.length;
-        }
-
-        @Override
-        public void writeTo(OutputStream out) throws IOException
-        {
-            out.write(jsonBytes);
-        }
-    }
+  }
 }
