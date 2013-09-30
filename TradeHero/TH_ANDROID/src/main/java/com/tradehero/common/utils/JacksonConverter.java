efb08ada90bf475a201f6fa91a371696119b46c4
Xavier Lepretre <xavier@tradehero.mobi>
2013-09-30 19:50:30 +0800

    Added a ImageUrlView that is thread safe.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/utils/JacksonConverter.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/utils/JacksonConverter.java
@@ -2,7 +2,6 @@ package com.tradehero.common.utils;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationConfig;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -17,42 +16,46 @@ import retrofit.mime.TypedByteArray;
 import retrofit.mime.TypedInput;
 import retrofit.mime.TypedOutput;
 
-/**
- * Created with IntelliJ IDEA. Date: 7/1/13 Time: 2:33 PM Copyright (c) 2013 thonguyen. All rights
- * reserved.
- */
+/** Created with IntelliJ IDEA. Date: 7/1/13 Time: 2:33 PM Copyright (c) 2013 thonguyen. All rights reserved. */
 public class JacksonConverter implements Converter
 {
-  private static final String MIME_TYPE = "application/json; charset=UTF-8";
+    private static final String MIME_TYPE = "application/json; charset=UTF-8";
 
-  private final ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
-  public JacksonConverter(ObjectMapper objectMapper) {
-    this.objectMapper = objectMapper;
-      objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-  }
-
-  @Override public Object fromBody(TypedInput body, final Type type) throws ConversionException {
-    try {
-      final JavaType javaType = TypeFactory.defaultInstance().constructType(type);
-      return objectMapper.readValue(body.in(), javaType);
-    } catch (final JsonParseException e) {
-      throw new ConversionException(e);
-    } catch (final JsonMappingException e) {
-      throw new ConversionException(e);
-    } catch (final IOException e) {
-      throw new ConversionException(e);
+    public JacksonConverter(ObjectMapper objectMapper)
+    {
+        this.objectMapper = objectMapper;
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
-  }
 
-  @Override public TypedOutput toBody(Object object) {
-    try {
-      final String json = objectMapper.writeValueAsString(object);
-      return new TypedByteArray(MIME_TYPE, json.getBytes("UTF-8"));
-    } catch (final JsonProcessingException e) {
-      throw new AssertionError(e);
-    } catch (final UnsupportedEncodingException e) {
-      throw new AssertionError(e);
+    @Override public Object fromBody(TypedInput body, final Type type) throws ConversionException
+    {
+        try
+        {
+            final JavaType javaType = TypeFactory.defaultInstance().constructType(type);
+            return objectMapper.readValue(body.in(), javaType);
+        }
+        catch (final JsonParseException|JsonMappingException e)
+        {
+            throw new ConversionException(e);
+        }
+        catch (final IOException e)
+        {
+            throw new ConversionException(e);
+        }
     }
-  }
+
+    @Override public TypedOutput toBody(Object object)
+    {
+        try
+        {
+            final String json = objectMapper.writeValueAsString(object);
+            return new TypedByteArray(MIME_TYPE, json.getBytes("UTF-8"));
+        }
+        catch (final JsonProcessingException|UnsupportedEncodingException e)
+        {
+            throw new AssertionError(e);
+        }
+    }
 }
