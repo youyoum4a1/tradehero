1fbdc086bd16dbabc23847f513165dbc77e2df2f
Xavier Lepretre <xavier@tradehero.mobi>
2013-10-03 20:22:15 +0800

    Nicer wording

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/persistence/DTOCache.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/persistence/DTOCache.java
@@ -3,9 +3,9 @@ package com.tradehero.th.persistence;
 import com.tradehero.th.api.DTOKey;
 
 /** Created with IntelliJ IDEA. User: xavier Date: 10/3/13 Time: 4:48 PM To change this template use File | Settings | File Templates. */
-public interface DTOCache<BaseKeyType, KeyType extends DTOKey<BaseKeyType>, DTOType>
+public interface DTOCache<BaseKeyType, DTOKeyType extends DTOKey<BaseKeyType>, DTOType>
 {
-    DTOType get(KeyType key);
-    DTOType getOrFetch(KeyType key, boolean force);
-    DTOType put(KeyType key, DTOType value);
+    DTOType get(DTOKeyType key);
+    DTOType getOrFetch(DTOKeyType key, boolean force);
+    DTOType put(DTOKeyType key, DTOType value);
 }
