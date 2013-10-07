868da8ab5f4c23153619a8365649fb18c3df49fc
Xavier Lepretre <xavier@tradehero.mobi>
2013-10-07 18:15:13 +0800

    Fixed typo

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/service/PositionService.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/service/PositionService.java
@@ -10,33 +10,33 @@ import retrofit.http.Query;
 /** Created with IntelliJ IDEA. User: xavier Date: 9/20/13 Time: 3:46 PM To change this template use File | Settings | File Templates. */
 public interface PositionService
 {
-    @GET("/users{userId}/portfolios/{portfolioId}/positions")
+    @GET("/users/{userId}/portfolios/{portfolioId}/positions")
     void getPositions(
             @Path("userId") int userId,
             @Path("portfolioId")int portfolioId,
             Callback<GetPositionsDTO> callback);
 
-    @GET("/users{userId}/portfolios/{portfolioId}/positions")
+    @GET("/users/{userId}/portfolios/{portfolioId}/positions")
     GetPositionsDTO getPositions(
             @Path("userId") int userId,
             @Path("portfolioId")int portfolioId)
             throws RetrofitError;
 
-    @GET("/users{userId}/portfolios/{portfolioId}/positions")
+    @GET("/users/{userId}/portfolios/{portfolioId}/positions")
     void getPositions(
             @Path("userId") int userId,
             @Path("portfolioId")int portfolioId,
             @Query("pageNumber") Integer pageNumber,
             Callback<GetPositionsDTO> callback);
 
-    @GET("/users{userId}/portfolios/{portfolioId}/positions")
+    @GET("/users/{userId}/portfolios/{portfolioId}/positions")
     GetPositionsDTO getPositions(
             @Path("userId") int userId,
             @Path("portfolioId")int portfolioId,
             @Query("pageNumber") Integer pageNumber)
             throws RetrofitError;
 
-    @GET("/users{userId}/portfolios/{portfolioId}/positions")
+    @GET("/users/{userId}/portfolios/{portfolioId}/positions")
     void getPositions(
             @Path("userId") int userId,
             @Path("portfolioId")int portfolioId,
@@ -44,7 +44,7 @@ public interface PositionService
             @Query("perPage") Integer perPage,
             Callback<GetPositionsDTO> callback);
 
-    @GET("/users{userId}/portfolios/{portfolioId}/positions")
+    @GET("/users/{userId}/portfolios/{portfolioId}/positions")
     GetPositionsDTO getPositions(
             @Path("userId") int userId,
             @Path("portfolioId")int portfolioId,
