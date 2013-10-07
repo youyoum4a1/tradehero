db96bff43390068b35fa632c880b1bd4513fb6f0
Xavier Lepretre <xavier@tradehero.mobi>
2013-10-07 17:58:48 +0800

    Forgot to put view pager stuff

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/SignatureContainer.java
@@ -0,0 +1,8 @@
+package com.tradehero.th.api;
+
+/** Created with IntelliJ IDEA. User: xavier Date: 10/7/13 Time: 4:43 PM To change this template use File | Settings | File Templates. */
+public class SignatureContainer<SignedType>
+{
+    SignedType signedObject;
+    String Signature;
+}
---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/service/QuoteService.java
@@ -0,0 +1,6 @@
+package com.tradehero.th.network.service;
+
+/** Created with IntelliJ IDEA. User: xavier Date: 10/7/13 Time: 4:28 PM To change this template use File | Settings | File Templates. */
+public interface QuoteService
+{
+}
