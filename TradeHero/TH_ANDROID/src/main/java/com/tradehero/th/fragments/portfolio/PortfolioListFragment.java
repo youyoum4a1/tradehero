9a7190b23d7808a6cd95bd129edc5eff2de9e8aa
Xavier Lepretre <xavier@tradehero.mobi>
2013-10-14 11:51:30 +0800

    First elements to portfolios list.

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/portfolio/PortfolioListFragment.java
@@ -0,0 +1,9 @@
+package com.tradehero.th.fragments.portfolio;
+
+import com.tradehero.th.fragments.base.DashboardFragment;
+
+/** Created with IntelliJ IDEA. User: xavier Date: 10/14/13 Time: 11:47 AM To change this template use File | Settings | File Templates. */
+public class PortfolioListFragment extends DashboardFragment
+{
+
+}
---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/widget/portfolio/PortfolioListView.java
@@ -0,0 +1,37 @@
+package com.tradehero.th.widget.portfolio;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/** Created with IntelliJ IDEA. User: xavier Date: 10/14/13 Time: 11:42 AM To change this template use File | Settings | File Templates. */
+public class PortfolioListView extends ListView
+{
+    //<editor-fold desc="Constructors">
+    public PortfolioListView(Context context)
+    {
+        super(context);
+    }
+
+    public PortfolioListView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public PortfolioListView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+    }
+    //</editor-fold>
+
+
+    @Override protected void onFinishInflate()
+    {
+        super.onFinishInflate();
+        init();
+    }
+
+    protected void init ()
+    {
+    }
+}
