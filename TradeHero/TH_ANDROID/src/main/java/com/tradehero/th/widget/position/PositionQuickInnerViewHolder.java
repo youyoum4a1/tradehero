7b180be685cf7262f115bdcee7ebe77dc4394bb4
Xavier Lepretre <xavier@tradehero.mobi>
2013-10-16 12:04:51 +0800

    First elements for position display

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/widget/position/PositionQuickInnerViewHolder.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/widget/position/PositionQuickInnerViewHolder.java
@@ -1,6 +1,64 @@
 package com.tradehero.th.widget.position;
 
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.tradehero.th.R;
+
 /** Created with IntelliJ IDEA. User: xavier Date: 10/16/13 Time: 11:53 AM To change this template use File | Settings | File Templates. */
 public class PositionQuickInnerViewHolder
 {
+    private ImageView stockLogo;
+    private TextView stockSymbol;
+    private TextView companyName;
+    private TextView stockMovementIndicator;
+    private TextView currencyDisplay;
+    private TextView stockLastPrice;
+    private ImageView marketClose;
+    private TextView positionProfitIndicator;
+    private TextView positionCurrencyDisplay;
+    private TextView positionLastAmount;
+    private ImageButton tradeHistoryButton;
+
+    public void initViews(View view)
+    {
+        if (view != null)
+        {
+            stockLogo = (ImageView) view.findViewById(R.id.stock_logo);
+            stockSymbol = (TextView) view.findViewById(R.id.stock_symbol);
+            companyName = (TextView) view.findViewById(R.id.company_name);
+            stockMovementIndicator = (TextView) view.findViewById(R.id.stock_movement_indicator);
+            currencyDisplay = (TextView) view.findViewById(R.id.currency_display);
+            stockLastPrice = (TextView) view.findViewById(R.id.stock_last_price);
+            marketClose = (ImageView) view.findViewById(R.id.ic_market_close);
+            positionProfitIndicator = (TextView) view.findViewById(R.id.position_profit_indicator);
+            positionCurrencyDisplay = (TextView) view.findViewById(R.id.position_currency_display);
+            positionLastAmount = (TextView) view.findViewById(R.id.position_last_amount);
+
+            tradeHistoryButton = (ImageButton) view.findViewById(R.id.btn_trade_history);
+            if (tradeHistoryButton != null)
+            {
+                tradeHistoryButton.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override public void onClick(View clickedView)
+                    {
+                        handleTradeHistoryButtonClicked(clickedView);
+                    }
+                });
+            }
+        }
+    }
+
+    public void destroyViews()
+    {
+        if (tradeHistoryButton != null)
+        {
+            tradeHistoryButton.setOnClickListener(null);
+        }
+    }
+
+    protected void handleTradeHistoryButtonClicked(View view)
+    {
+    }
 }
