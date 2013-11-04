a8dcb4cb0389cc16c0da9c2876c3694c5a136e64
Xavier Lepretre <xavier@tradehero.mobi>
2013-11-04 12:06:24 +0800

    Updates to pref saver of other people
    [#59960608]

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/persistence/social/VisitedFriendListPrefs.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/persistence/social/VisitedFriendListPrefs.java
@@ -1,7 +1,65 @@
 package com.tradehero.th.persistence.social;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import com.tradehero.th.api.users.UserBaseKey;
+import com.tradehero.th.base.Application;
+import com.tradehero.th.base.THUser;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 /** Created with IntelliJ IDEA. User: xavier Date: 11/1/13 Time: 1:46 PM To change this template use File | Settings | File Templates. */
 public class VisitedFriendListPrefs
 {
     public static final String TAG = VisitedFriendListPrefs.class.getSimpleName();
+    public static final String KEY_PREFS = VisitedFriendListPrefs.class.getName();
+    public static final String KEY_VISITED_ID_SET = VisitedFriendListPrefs.class.getName() + ".VISITED_ID_SET";
+
+    public static void addVisitedId(UserBaseKey userBaseKey)
+    {
+        if (userBaseKey == null)
+        {
+            return;
+        }
+        if (userBaseKey.equals(THUser.getCurrentUserBase().getBaseKey()))
+        {
+            return;
+        }
+        // We cannot modify the returned Set, so we have to go the long route
+        List<UserBaseKey> userBaseKeys = getVisitedIdList();
+        userBaseKeys.add(userBaseKey); // TODO add at head
+        saveVisitedIdList(userBaseKeys);
+    }
+
+    private static void saveVisitedIdList(List<UserBaseKey> userBaseKeys)
+    {
+        Set<String> userKeys = new TreeSet<>();
+        if (userBaseKeys != null)
+        {
+            for (UserBaseKey userBaseKey: userBaseKeys)
+            {
+                userKeys.add(userBaseKey.key.toString());
+            }
+        }
+
+        SharedPreferences.Editor pref = Application.context().getSharedPreferences(KEY_PREFS, Context.MODE_PRIVATE).edit();
+        pref.putStringSet(KEY_VISITED_ID_SET, userKeys);
+        pref.commit();
+    }
+
+    public static List<UserBaseKey> getVisitedIdList()
+    {
+        SharedPreferences preferences = Application.context().getSharedPreferences(KEY_PREFS, Context.MODE_PRIVATE);
+        Set<String> userKeys = preferences.getStringSet(KEY_VISITED_ID_SET, new TreeSet<String>());
+
+        List<UserBaseKey> userBaseKeys = new ArrayList<>();
+        for (String userKey: userKeys)
+        {
+            userBaseKeys.add(new UserBaseKey(Integer.parseInt(userKey)));
+        }
+        return userBaseKeys;
+    }
 }
