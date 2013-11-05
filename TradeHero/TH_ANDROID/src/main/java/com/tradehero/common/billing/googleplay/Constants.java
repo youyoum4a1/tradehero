5a84792fda2fad661f722c96604b9c6126c75513
Julien Grimault <grimaultjulien@gmail.com>
2013-11-05 12:24:21 +0800

    In-app billing connector

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/billing/googleplay/Constants.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/billing/googleplay/Constants.java
@@ -46,4 +46,40 @@ public class Constants
     // some fields on the getSkuDetails response bundle
     public static final String GET_SKU_DETAILS_ITEM_LIST = "ITEM_ID_LIST";
     public static final String GET_SKU_DETAILS_ITEM_TYPE_LIST = "ITEM_TYPE_LIST";
+
+
+    /**
+     * Returns a human-readable description for the given response code.
+     *
+     * @param code The response code
+     * @return A human-readable string explaining the result code.
+     *     It also includes the result code numerically.
+     */
+    public static String getStatusCodeDescription(int code) {
+        String[] iab_msgs = ("0:OK/1:User Canceled/2:Unknown/" +
+                "3:Billing Unavailable/4:Item unavailable/" +
+                "5:Developer Error/6:Error/7:Item Already Owned/" +
+                "8:Item not owned").split("/");
+        String[] iabhelper_msgs = ("0:OK/-1001:Remote exception during initialization/" +
+                "-1002:Bad response received/" +
+                "-1003:Purchase signature verification failed/" +
+                "-1004:Send intent failed/" +
+                "-1005:User cancelled/" +
+                "-1006:Unknown purchase response/" +
+                "-1007:Missing token/" +
+                "-1008:Unknown error/" +
+                "-1009:Subscriptions not available/" +
+                "-1010:Invalid consumption attempt").split("/");
+
+        if (code <= IABHELPER_ERROR_BASE) {
+            int index = IABHELPER_ERROR_BASE - code;
+            if (index >= 0 && index < iabhelper_msgs.length) return iabhelper_msgs[index];
+            else return String.valueOf(code) + ":Unknown IAB Helper Error";
+        }
+        else if (code < 0 || code >= iab_msgs.length)
+            return String.valueOf(code) + ":Unknown";
+        else
+            return iab_msgs[code];
+    }
+
 }
---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/billing/googleplay/IABServiceConnector.java
@@ -0,0 +1,171 @@
+package com.tradehero.common.billing.googleplay;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+import com.android.vending.billing.IInAppBillingService;
+import com.tradehero.common.utils.THLog;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Created by julien on 5/11/13
+ */
+public class IABServiceConnector
+{
+    public static final String TAG = IABServiceConnector.class.getSimpleName();
+    protected Context context;
+
+    protected IInAppBillingService service;
+    protected ServiceConnection serviceConnection;
+
+    private boolean subscriptionSupported;
+    private boolean setupDone = false;
+    boolean disposed = false;
+
+    protected ConnectorListener listener;
+    public static interface ConnectorListener
+    {
+        void onSetupFinished(IABServiceConnector connector, IABResponse response);
+    }
+
+    public IABServiceConnector(Context ctx)
+    {
+        this.context = ctx;
+    }
+
+
+    public void startConnectionSetup()
+    {
+        checkNotDisposed();
+        if (setupDone) throw new IllegalStateException("IAB helper is already set up.");
+
+        THLog.d(TAG, "Starting in-app billing setup.");
+
+        setupServiceConnection();
+        Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
+        serviceIntent.setPackage("com.android.vending");
+
+        if (!context.getPackageManager().queryIntentServices(serviceIntent, 0).isEmpty()) {
+            // service available to handle that Intent
+            context.bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+        }
+        else {
+            // no service available to handle that Intent
+            if (listener != null) {
+                listener.onSetupFinished(this,
+                        new IABResponse(Constants.BILLING_RESPONSE_RESULT_BILLING_UNAVAILABLE, "Billing service unavailable on device."));
+            }
+        }
+    }
+
+    /**
+     * Dispose of object, releasing resources. It's very important to call this
+     * method when you are done with this object. It will release any resources
+     * used by it such as service connections. Naturally, once the object is
+     * disposed of, it can't be used again.
+     */
+    public void dispose() {
+        THLog.d(TAG, "Disposing.");
+        setupDone = false;
+        if (serviceConnection != null) {
+            THLog.d(TAG, "Unbinding from service.");
+            if (context != null) context.unbindService(serviceConnection);
+        }
+        disposed = true;
+        context = null;
+        serviceConnection = null;
+        service = null;
+        listener = null;
+    }
+
+
+    private ServiceConnection setupServiceConnection()
+    {
+        if (serviceConnection != null)
+            return serviceConnection;
+
+        serviceConnection = new ServiceConnection() {
+            @Override public void onServiceDisconnected(ComponentName name) {
+                THLog.d(TAG, "Billing service disconnected.");
+                service = null;
+            }
+
+            @Override public void onServiceConnected(ComponentName name, IBinder binderService) {
+                THLog.d(TAG, "Billing service connected.");
+                service = IInAppBillingService.Stub.asInterface(binderService);
+                String packageName = context.getPackageName();
+                try {
+                    THLog.d(TAG, "Checking for in-app billing 3 support.");
+
+                    // check for in-app billing v3 support
+                    int responseStatus = service.isBillingSupported(3, packageName, Constants.ITEM_TYPE_INAPP);
+                    if (responseStatus != Constants.BILLING_RESPONSE_RESULT_OK) {
+                        if (listener != null)
+                        {
+                            listener.onSetupFinished(IABServiceConnector.this, new IABResponse(responseStatus, "Error checking for billing v3 support."));
+                        }
+
+                        // if in-app purchases aren't supported, neither are subscriptions.
+                        subscriptionSupported = false;
+                        return;
+                    }
+                    THLog.d(TAG, "In-app billing version 3 supported for " + packageName);
+
+                    // check for v3 subscriptions support
+                    responseStatus = service.isBillingSupported(3, packageName, Constants.ITEM_TYPE_SUBS);
+                    if (responseStatus == Constants.BILLING_RESPONSE_RESULT_OK) {
+                        THLog.d(TAG, "Subscriptions AVAILABLE.");
+                        subscriptionSupported = true;
+                    }
+                    else {
+                        THLog.d(TAG, "Subscriptions NOT AVAILABLE. Response: " + responseStatus);
+                    }
+
+                    setupDone = true;
+                }
+                catch (RemoteException e) {
+                    if (listener != null) {
+                        listener.onSetupFinished(IABServiceConnector.this, new IABResponse(Constants.IABHELPER_REMOTE_EXCEPTION,
+                                "RemoteException while setting up in-app billing."));
+                    }
+                    e.printStackTrace();
+                    return;
+                }
+
+                if (listener != null) {
+                    listener.onSetupFinished(IABServiceConnector.this, new IABResponse(Constants.BILLING_RESPONSE_RESULT_OK, "Setup successful."));
+                }
+            }
+        };
+        return serviceConnection;
+    }
+
+    private void checkNotDisposed() {
+        if (disposed) throw new IllegalStateException("IabHelper was disposed of, so it cannot be used.");
+    }
+
+    public boolean areSubscriptionsSupported()
+    {
+        return this.subscriptionSupported;
+    }
+
+    public boolean isSetupDone()
+    {
+        return setupDone;
+    }
+
+    public ConnectorListener getListener()
+    {
+        return listener;
+    }
+
+    public void setListener(ConnectorListener listener)
+    {
+        this.listener = listener;
+    }
+}
