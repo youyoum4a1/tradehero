d29f7e749cb164bc80dc9562374838a5471afaee
Xavier Lepretre <xavier@tradehero.mobi>
2013-11-07 18:08:11 +0800

    Put some IAB dialogs into separate class.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/billing/googleplay/IABAlertUtils.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/billing/googleplay/IABAlertUtils.java
@@ -1,7 +1,59 @@
 package com.tradehero.th.billing.googleplay;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.provider.Settings;
+import com.tradehero.th.R;
+
 /** Created with IntelliJ IDEA. User: xavier Date: 11/7/13 Time: 5:52 PM To change this template use File | Settings | File Templates. */
 public class IABAlertUtils
 {
     public static final String TAG = IABAlertUtils.class.getSimpleName();
+
+    public static void popBillingUnavailable(final Context context)
+    {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder
+                .setTitle(R.string.store_billing_unavailable_window_title)
+                .setMessage(R.string.store_billing_unavailable_window_description)
+                .setCancelable(true)
+                .setNegativeButton(R.string.store_billing_unavailable_cancel, new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int id)
+                    {
+                        dialog.cancel();
+                    }
+                })
+                .setPositiveButton(R.string.store_billing_unavailable_act, new DialogInterface.OnClickListener()
+                {
+                    @Override public void onClick(DialogInterface dialogInterface, int i)
+                    {
+                        Intent addAccountIntent = new Intent(Settings.ACTION_ADD_ACCOUNT);
+                        addAccountIntent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT); // Still cannot get it to go back to TradeHero with back button
+                        context.startActivity(addAccountIntent);
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    public static void popWaitWhileLoading(final Context context)
+    {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder
+                .setTitle(R.string.store_billing_loading_window_title)
+                .setMessage(R.string.store_billing_loading_window_description)
+                .setCancelable(true)
+                .setNegativeButton(R.string.store_billing_loading_cancel, new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int id)
+                    {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
 }
