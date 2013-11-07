d5624cc50a34dd052d8ece34bc4e0a83fbbec2fc
Xavier Lepretre <xavier@tradehero.mobi>
2013-11-07 18:07:36 +0800

    Cleaner adapter

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/billing/googleplay/IABAlertUtils.java
@@ -0,0 +1,7 @@
+package com.tradehero.th.billing.googleplay;
+
+/** Created with IntelliJ IDEA. User: xavier Date: 11/7/13 Time: 5:52 PM To change this template use File | Settings | File Templates. */
+public class IABAlertUtils
+{
+    public static final String TAG = IABAlertUtils.class.getSimpleName();
+}
