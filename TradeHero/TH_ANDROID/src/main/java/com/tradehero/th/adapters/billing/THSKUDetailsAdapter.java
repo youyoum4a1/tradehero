8919407c116920655d64307ab3402d76b24a15df
Xavier Lepretre <xavier@tradehero.mobi>
2013-11-07 18:35:18 +0800

    Made SKUDetails adapter and view use generic

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/adapters/billing/THSKUDetailsAdapter.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/adapters/billing/THSKUDetailsAdapter.java
@@ -12,12 +12,9 @@ import com.tradehero.th.widget.billing.SKUQuickDescriptionView;
 import com.tradehero.th.widget.billing.StoreSKUDetailView;
 
 /** Created with IntelliJ IDEA. User: xavier Date: 11/6/13 Time: 4:14 PM To change this template use File | Settings | File Templates. */
-public class THSKUDetailsAdapter extends DTOAdapter<THSKUDetails, StoreSKUDetailView>
+public class THSKUDetailsAdapter extends SKUDetailsAdapter<THSKUDetails, StoreSKUDetailView>
 {
     public static final String TAG = THSKUDetailsAdapter.class.getSimpleName();
-
-    public static final int ITEM_TYPE_HEADER = 0;
-    public static final int ITEM_TYPE_VALUE = 1;
 
     private String skuDomain;
 
@@ -33,56 +30,13 @@ public class THSKUDetailsAdapter extends DTOAdapter<THSKUDetails, StoreSKUDetail
         this.skuDomain = skuDomain;
     }
 
-    @Override public int getCount()
-    {
-        return super.getCount() + 1;
-    }
-
-    @Override public Object getItem(int i)
-    {
-        return i == 0 ? null : super.getItem(i - 1);
-    }
-
-    @Override public int getViewTypeCount()
-    {
-        return 2;
-    }
-
-    @Override public int getItemViewType(int position)
-    {
-        switch (position)
-        {
-            case 0:
-                return ITEM_TYPE_HEADER;
-            default:
-                return ITEM_TYPE_VALUE;
-        }
-    }
-
-    protected View getHeaderView(int position, View convertView, ViewGroup viewGroup)
+    @Override protected View getHeaderView(int position, View convertView, ViewGroup viewGroup)
     {
         SKUQuickDescriptionView quickDescription = convertView instanceof SKUQuickDescriptionView ?
                 (SKUQuickDescriptionView) convertView :
                 (SKUQuickDescriptionView) inflater.inflate(R.layout.store_quick_message, viewGroup, false);
         quickDescription.linkWithSkuDomain(skuDomain, true);
         return quickDescription;
-    }
-
-    @Override public View getView(int position, View convertView, ViewGroup viewGroup)
-    {
-        return getItemViewType(position) == ITEM_TYPE_HEADER ?
-                getHeaderView(position, convertView, viewGroup) :
-                super.getView(position, convertView instanceof SKUQuickDescriptionView ? null : convertView, viewGroup);
-    }
-
-    @Override public boolean areAllItemsEnabled()
-    {
-        return false;
-    }
-
-    @Override public boolean isEnabled(int position)
-    {
-        return getItemViewType(position) != ITEM_TYPE_HEADER;
     }
 
     @Override protected void fineTune(int position, THSKUDetails dto, StoreSKUDetailView dtoView)
