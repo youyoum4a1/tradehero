798ee4145f1018958ba68127052eb4da44016a36
Xavier Lepretre <xavier@tradehero.mobi>
2013-11-08 17:48:54 +0800

    Made classes use more generics in intermediate classes

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/billing/googleplay/IABActor.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/billing/googleplay/IABActor.java
@@ -6,15 +6,17 @@ import com.tradehero.common.billing.googleplay.exceptions.IABException;
 
 /** Created with IntelliJ IDEA. User: xavier Date: 11/8/13 Time: 11:06 AM To change this template use File | Settings | File Templates. */
 public interface IABActor<
-                        ProductDetailsType extends ProductDetails<IABSKU>,
+                        IABSKUType extends IABSKU,
+                        ProductDetailsType extends ProductDetails<IABSKUType>,
+                        IABOrderIdType extends IABOrderId,
                         IABExceptionType extends IABException,
-                        IABPurchaseType extends IABPurchase<IABOrderId, IABSKU>,
-                        IABPurchaseHandlerType extends IABPurchaseHandler<IABExceptionType, IABPurchaseType>>
+                        IABPurchaseType extends IABPurchase<IABOrderIdType, IABSKUType>,
+                        IABPurchaseHandlerType extends IABPurchaseHandler<IABSKUType, IABOrderIdType, IABExceptionType, IABPurchaseType>>
     extends BillingActor<
-                        IABSKU,
+                        IABSKUType,
                         ProductDetailsType,
                         IABExceptionType,
-                        IABOrderId,
+                        IABOrderIdType,
                         IABPurchaseType,
                         IABPurchaseHandlerType>
 {
--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/billing/googleplay/THIABPurchaseHandler.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/billing/googleplay/THIABPurchaseHandler.java
@@ -1,10 +1,12 @@
 package com.tradehero.th.billing.googleplay;
 
+import com.tradehero.common.billing.googleplay.IABOrderId;
 import com.tradehero.common.billing.googleplay.IABPurchaseHandler;
+import com.tradehero.common.billing.googleplay.IABSKU;
 import com.tradehero.common.billing.googleplay.SKUPurchase;
 import com.tradehero.common.billing.googleplay.exceptions.IABException;
 
 /** Created with IntelliJ IDEA. User: xavier Date: 11/8/13 Time: 12:16 PM To change this template use File | Settings | File Templates. */
-public interface THIABPurchaseHandler extends IABPurchaseHandler<IABException, SKUPurchase>
+public interface THIABPurchaseHandler extends IABPurchaseHandler<IABSKU, IABOrderId, IABException, SKUPurchase>
 {
 }
