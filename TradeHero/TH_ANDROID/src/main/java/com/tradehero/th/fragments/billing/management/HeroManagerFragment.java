c77b5976efb7b1e39af8d05eff33c6c733cc7656
Xavier Lepretre <xavier@tradehero.mobi>
2013-11-11 11:22:37 +0800

    Introduced an intermediate Fragment before StoreScreenFragment.
    Will be used by purchase manage fragments.

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/billing/management/HeroManagerFragment.java
@@ -0,0 +1,7 @@
+package com.tradehero.th.fragments.billing.management;
+
+/** Created with IntelliJ IDEA. User: xavier Date: 11/11/13 Time: 11:04 AM To change this template use File | Settings | File Templates. */
+public class HeroManagerFragment
+{
+    public static final String TAG = HeroManagerFragment.class.getSimpleName();
+}
