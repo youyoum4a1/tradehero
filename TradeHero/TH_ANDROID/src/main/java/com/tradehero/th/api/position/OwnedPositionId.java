7a339c32003db067e1aeaac7c0666eb123f6b764
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2013-11-12 15:34:38 +0800

    Fix typo error

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/position/OwnedPositionId.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/position/OwnedPositionId.java
@@ -10,7 +10,7 @@ import com.tradehero.th.api.users.UserBaseKey;
 /** Created with IntelliJ IDEA. User: xavier Date: 10/16/13 Time: 12:50 PM To change this template use File | Settings | File Templates. */
 public class OwnedPositionId extends OwnedPortfolioId implements DTO
 {
-    public final static String BUNDLE_KEY_POSITION_ID = OwnedPositionId.class.getName() + ".portfolioId";
+    public final static String BUNDLE_KEY_POSITION_ID = OwnedPositionId.class.getName() + ".positionId";
 
     public final Integer positionId;
 
