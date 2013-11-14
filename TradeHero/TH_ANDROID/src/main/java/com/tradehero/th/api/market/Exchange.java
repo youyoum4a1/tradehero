26593c306f146f7d4669ff25556b5686c1a24a0c
Xavier Lepretre <xavier@tradehero.mobi>
2013-11-14 15:51:12 +0800

    Cleanup of drawables

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/market/Exchange.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/market/Exchange.java
@@ -1,73 +1,72 @@
 package com.tradehero.th.api.market;
 
-import com.tradehero.common.utils.THLog;
 import com.tradehero.th.R;
 
 /** Created with IntelliJ IDEA. User: xavier Date: 9/16/13 Time: 11:52 AM To change this template use File | Settings | File Templates. */
 public enum Exchange
 {
     // United Kingdom
-    LSE(R.drawable.united_kingdom),
+    LSE(R.drawable.flag_country_round_united_kingdom),
 
     // United States of America
-    NASDAQ(R.drawable.united_states),
-    NYSE(R.drawable.united_states),
-    OTCBB(R.drawable.united_states),
-    AMEX(R.drawable.united_states),
+    NASDAQ(R.drawable.flag_country_round_united_states),
+    NYSE(R.drawable.flag_country_round_united_states),
+    OTCBB(R.drawable.flag_country_round_united_states),
+    AMEX(R.drawable.flag_country_round_united_states),
 
     // singapore
-    SGX(R.drawable.singapore),
+    SGX(R.drawable.flag_country_round_singapore),
 
     // Australia
-    ASX(R.drawable.australia),
+    ASX(R.drawable.flag_country_round_australia),
 
     // Canada
-    TSX(R.drawable.canada),
-    TSXV(R.drawable.canada),
+    TSX(R.drawable.flag_country_round_canada),
+    TSXV(R.drawable.flag_country_round_canada),
 
     // Hong Kong
-    HKEX(R.drawable.hong_kong),
+    HKEX(R.drawable.flag_country_round_hong_kong),
 
     // france
-    PAR(R.drawable.france),
+    PAR(R.drawable.flag_country_round_france),
 
     // netherlands
-    AMS(R.drawable.netherlands),
+    AMS(R.drawable.flag_country_round_netherlands),
 
     // Belgium
-    BRU(R.drawable.belgium),
+    BRU(R.drawable.flag_country_round_belgium),
 
     // portugal
-    LIS(R.drawable.portugal),
+    LIS(R.drawable.flag_country_round_portugal),
 
     // italy
-    MLSE(R.drawable.italy),
+    MLSE(R.drawable.flag_country_round_italy),
 
     // New Zealand
-    NZX(R.drawable.new_zealand),
+    NZX(R.drawable.flag_country_round_new_zealand),
 
     // china
-    SHA(R.drawable.china),
-    SHE(R.drawable.china),
+    SHA(R.drawable.flag_country_round_china),
+    SHE(R.drawable.flag_country_round_china),
 
     // indonesia
-    JKT(R.drawable.indonesia),
+    JKT(R.drawable.flag_country_round_indonesia),
 
     // South Korea
-    KDQ(R.drawable.korea_south),
-    KRX(R.drawable.korea_south),
+    KDQ(R.drawable.flag_country_round_korea_south),
+    KRX(R.drawable.flag_country_round_korea_south),
 
     // taiwan
-    TPE(R.drawable.taiwan),
+    TPE(R.drawable.flag_country_round_taiwan),
 
     // thailand
-    SET(R.drawable.thailand),
+    SET(R.drawable.flag_country_round_thailand),
 
     // philippines
-    PSE(R.drawable.philippines),
+    PSE(R.drawable.flag_country_round_philippines),
 
     // malaysia
-    MYX(R.drawable.malaysia);
+    MYX(R.drawable.flag_country_round_malaysia);
 
     private static final String TAG = Exchange.class.getSimpleName();
 
