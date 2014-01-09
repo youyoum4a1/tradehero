f096fdf70b64c09498d5d8be134397412fbf9925
Xavier Lepretre <xavier@tradehero.mobi>
2014-01-09 11:22:08 +0800

    Max purchasable shares now takes account of transaction cost
    [#63279570]

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/users/UserProfileDTOUtil.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/users/UserProfileDTOUtil.java
@@ -1,6 +1,7 @@
 package com.tradehero.th.api.users;
 
 import com.tradehero.th.api.quote.QuoteDTO;
+import com.tradehero.th.utils.SecurityUtils;
 
 /**
  * Created by xavier on 12/13/13.
@@ -11,11 +12,16 @@ public class UserProfileDTOUtil
 
     public static Integer getMaxPurchasableShares(UserProfileDTO userProfileDTO, QuoteDTO quoteDTO)
     {
+        return getMaxPurchasableShares(userProfileDTO, quoteDTO, true);
+    }
+
+    public static Integer getMaxPurchasableShares(UserProfileDTO userProfileDTO, QuoteDTO quoteDTO, boolean includeTransactionCost)
+    {
         if (quoteDTO == null || quoteDTO.ask == null || quoteDTO.ask == 0 || quoteDTO.toUSDRate == null || quoteDTO.toUSDRate == 0 ||
                 userProfileDTO == null || userProfileDTO.portfolio == null)
         {
             return null;
         }
-        return (int) Math.floor(userProfileDTO.portfolio.cashBalance / (quoteDTO.ask * quoteDTO.toUSDRate));
+        return (int) Math.floor((userProfileDTO.portfolio.cashBalance - (includeTransactionCost ? SecurityUtils.DEFAULT_TRANSACTION_COST : 0)) / (quoteDTO.ask * quoteDTO.toUSDRate));
     }
 }
