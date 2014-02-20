4035fe60fc662925e936792826e926e5d723007b
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-02-20 14:44:24 +0800

    No Retrofit call log in release mode

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/RetrofitConstants.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/RetrofitConstants.java
@@ -9,5 +9,6 @@ public class RetrofitConstants
 {
     public static final String TAG = RetrofitConstants.class.getSimpleName();
 
-    public static final RestAdapter.LogLevel DEFAULT_SERVICE_LOG_LEVEL = RestAdapter.LogLevel.FULL;
+    public static final RestAdapter.LogLevel DEFAULT_SERVICE_LOG_LEVEL =
+            Constants.RELEASE ? RestAdapter.LogLevel.NONE : RestAdapter.LogLevel.FULL;
 }
