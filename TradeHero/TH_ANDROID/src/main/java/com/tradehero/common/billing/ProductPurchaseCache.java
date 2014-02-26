6d48673f0163cde93bfae5775593b16f50937c87
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-02-26 11:20:56 +0800

    Intermediate cache. [#66320802]

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/billing/ProductPurchaseCache.java
@@ -0,0 +1,63 @@
+package com.tradehero.common.billing;
+
+import com.tradehero.common.persistence.StraightDTOCache;
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Created by xavier on 2/11/14.
+ */
+abstract public class ProductPurchaseCache<
+        ProductIdentifierType extends ProductIdentifier,
+            OrderIdType extends OrderId,
+        ProductPurchaseType extends ProductPurchase<ProductIdentifierType, OrderIdType>>
+        extends StraightDTOCache<ProductIdentifierType, ProductPurchaseType>
+{
+    public static final String TAG = ProductPurchaseCache.class.getSimpleName();
+
+    protected final ArrayList<ProductIdentifierType> keys;
+
+    public ProductPurchaseCache(int maxSize)
+    {
+        super(maxSize);
+        keys = new ArrayList<>();
+    }
+
+    @Override public ProductPurchaseType put(ProductIdentifierType key, ProductPurchaseType value)
+    {
+        ProductPurchaseType previous = super.put(key, value);
+        keys.add(key);
+        return previous;
+    }
+
+    @Override public void invalidate(ProductIdentifierType key)
+    {
+        super.invalidate(key);
+        keys.remove(key);
+    }
+
+    @Override public void invalidateAll()
+    {
+        super.invalidateAll();
+        keys.clear();
+    }
+
+    public void put(Map<ProductIdentifierType, ProductPurchaseType> values)
+    {
+        if (values != null)
+        {
+            for (Map.Entry<ProductIdentifierType, ProductPurchaseType> entry : values.entrySet())
+            {
+                if (entry != null && entry.getKey() != null)
+                {
+                    put(entry.getKey(), entry.getValue());
+                }
+            }
+        }
+    }
+
+    public ArrayList<ProductIdentifierType> getKeys()
+    {
+        return new ArrayList<>(keys);
+    }
+}
--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/billing/googleplay/IABPurchaseCache.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/billing/googleplay/IABPurchaseCache.java
@@ -1,5 +1,6 @@
 package com.tradehero.common.billing.googleplay;
 
+import com.tradehero.common.billing.ProductPurchaseCache;
 import com.tradehero.common.persistence.StraightDTOCache;
 import java.util.ArrayList;
 import java.util.Map;
@@ -11,58 +12,20 @@ public class IABPurchaseCache<
             IABSKUType extends IABSKU,
             IABOrderIdType extends IABOrderId,
             IABPurchaseType extends IABPurchase<IABSKUType, IABOrderIdType>>
-        extends StraightDTOCache<IABSKUType, IABPurchaseType>
+        extends ProductPurchaseCache<
+            IABSKUType,
+            IABOrderIdType,
+            IABPurchaseType>
 {
     public static final String TAG = IABPurchaseCache.class.getSimpleName();
-
-    protected final ArrayList<IABSKUType> keys;
 
     public IABPurchaseCache(int maxSize)
     {
         super(maxSize);
-        keys = new ArrayList<>();
-    }
-
-    @Override public IABPurchaseType put(IABSKUType key, IABPurchaseType value)
-    {
-        IABPurchaseType previous = super.put(key, value);
-        keys.add(key);
-        return previous;
-    }
-
-    @Override public void invalidate(IABSKUType key)
-    {
-        super.invalidate(key);
-        keys.remove(key);
-    }
-
-    @Override public void invalidateAll()
-    {
-        super.invalidateAll();
-        keys.clear();
     }
 
     @Override protected IABPurchaseType fetch(IABSKUType key) throws Throwable
     {
         throw new IllegalStateException("You cannot fetch on this cache");
-    }
-
-    public void put(Map<IABSKUType, IABPurchaseType> values)
-    {
-        if (values != null)
-        {
-            for (Map.Entry<IABSKUType, IABPurchaseType> entry : values.entrySet())
-            {
-                if (entry != null && entry.getKey() != null)
-                {
-                    put(entry.getKey(), entry.getValue());
-                }
-            }
-        }
-    }
-
-    public ArrayList<IABSKUType> getKeys()
-    {
-        return new ArrayList<>(keys);
     }
 }
