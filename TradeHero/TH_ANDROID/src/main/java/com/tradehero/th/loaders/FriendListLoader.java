52e9fc756971dd480e9353acf6cd975667e32002
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-02-28 12:25:35 +0800

    [Fixes #66644564] Fix crash on  refer friend screen when no internet connection

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/loaders/FriendListLoader.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/loaders/FriendListLoader.java
@@ -13,6 +13,7 @@ import dagger.Lazy;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
+import timber.log.Timber;
 
 /**
  * Created with IntelliJ IDEA. User: tho Date: 1/24/14 Time: 11:16 AM Copyright (c) TradeHero
@@ -85,11 +86,11 @@ public class FriendListLoader extends ListLoader<UserFriendsDTO>
             emailRetrieverThread.join();
             friendListRetrieverThread.join();
         }
-        catch (InterruptedException e)
+        catch (Exception e)
         {
+            Timber.e("Unable to get friend list", e);
             return friendsDTOs;
         }
-
 
         if (contactEntries != null && !contactEntries.isEmpty())
         {
