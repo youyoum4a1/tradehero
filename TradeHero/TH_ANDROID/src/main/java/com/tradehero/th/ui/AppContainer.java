390a4591c037ccd92e8cfafdf03d0b91638884bb
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-03-05 16:50:24 +0800

    [WIP] Create UI components using Dagger

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/ui/AppContainer.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/ui/AppContainer.java
@@ -1,6 +1,7 @@
 package com.tradehero.th.ui;
 
 import android.app.Activity;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/ui/UIComponents.java
@@ -0,0 +1,25 @@
+package com.tradehero.th.ui;
+
+import com.tradehero.th.activities.DashboardActivity;
+import dagger.Module;
+import dagger.Provides;
+import javax.inject.Singleton;
+
+/**
+ * Created with IntelliJ IDEA. User: tho Date: 3/5/14 Time: 4:04 PM Copyright (c) TradeHero
+ */
+@Module(
+        addsTo = UIComponents.class,
+        injects = {
+                DashboardActivity.class
+        },
+        complete = false,
+        library = true
+)
+public class UIComponents
+{
+    @Provides @Singleton AppContainer provideAppContainer(AppContainerImpl appContainer)
+    {
+        return appContainer;
+    }
+}
