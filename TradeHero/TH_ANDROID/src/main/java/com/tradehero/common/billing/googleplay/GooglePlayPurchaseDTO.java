2f274a2316f3bd957193b7301c66c243cdfb55e3
Xavier Lepretre <xavier@tradehero.mobi>
2014-03-10 11:32:05 +0800

    Cleanup Caml case on GooglePlayPurchase

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/billing/googleplay/GooglePlayPurchaseDTO.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/billing/googleplay/GooglePlayPurchaseDTO.java
@@ -1,17 +1,21 @@
 package com.tradehero.common.billing.googleplay;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /** Created with IntelliJ IDEA. User: xavier Date: 11/18/13 Time: 11:31 AM To change this template use File | Settings | File Templates. */
 public class GooglePlayPurchaseDTO
 {
     public static final String TAG = GooglePlayPurchaseDTO.class.getSimpleName();
 
     // Pass it back as the original JSON
-    public final String google_play_data;
-    public final String google_play_signature;
+    @JsonProperty("google_play_data")
+    public final String googlePlayData;
+    @JsonProperty("google_play_signature")
+    public final String googlePlaySignature;
 
-    public GooglePlayPurchaseDTO(final String google_play_data, final String google_play_signature)
+    public GooglePlayPurchaseDTO(final String googlePlayData, final String googlePlaySignature)
     {
-        this.google_play_data = google_play_data;
-        this.google_play_signature = google_play_signature;
+        this.googlePlayData = googlePlayData;
+        this.googlePlaySignature = googlePlaySignature;
     }
 }
