f31d3cd4f46c8ce0c2f4b65c8c889722aac5c5a4
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-03-24 18:02:20 +0800

    Cleanup using ButterKnife

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/security/NewsDetailFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/security/NewsDetailFragment.java
@@ -9,7 +9,11 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
@@ -31,32 +35,19 @@ import com.tradehero.th.network.service.NewsServiceWrapper;
 import com.tradehero.th.network.service.SecurityServiceWrapper;
 import dagger.Lazy;
 import java.util.List;
+import javax.inject.Inject;
 import org.ocpsoft.prettytime.PrettyTime;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-import javax.inject.Inject;
-
 /**
- * Created by tradehero on 14-3-10.
+ * Created by Alex on 14-3-10.
  */
 public class NewsDetailFragment extends DashboardFragment /*AbstractSecurityInfoFragment*/
 {
 
     public static final String BUNDLE_KEY_TITLE_BACKGROUND_RES = NewsDetailFragment.class.getName() + ".title_bg";
-
-    TextView titleView;
-    TextView dateView;
-    ImageView titlePlaceholderView;
-    LinearLayout likeContainer;
-    TextView likeTextView;
-    ImageView likeImageView;
-    TextView moreTextView;
-    TextView contentView;
-    TextView loadingView;
-    GridView securityGridView;
-    LinearLayout gridViewContainer;
 
     SimpleSecurityItemViewAdapter simpleSecurityItemViewAdapter;
 
@@ -68,7 +59,26 @@ public class NewsDetailFragment extends DashboardFragment /*AbstractSecurityInfo
 
     @Inject SecurityServiceWrapper securityServiceWrapper;
     @Inject SecurityItemViewAdapterFactory securityItemViewAdapterFactory;
-    protected SecurityItemViewAdapter<SecurityCompactDTO> securityItemViewAdapter;
+    
+    @InjectView(R.id.news_detail_title_placeholder) ImageView mNewsDetailTitlePlaceholder;
+    @InjectView(R.id.news_detail_title) TextView mNewsDetailTitle;
+    @InjectView(R.id.news_detail_date) TextView mNewsDetailDate;
+    @InjectView(R.id.news_detail_title_layout_wrapper) LinearLayout mNewsDetailTitleLayoutWrapper;
+    @InjectView(R.id.new_action_iv_like) ImageView mNewActionIvLike;
+    @InjectView(R.id.new_action_tv_like) TextView mNewActionTvLike;
+    @InjectView(R.id.news_action_button_like_wrapper) LinearLayout mNewsActionButtonLikeWrapper;
+    @InjectView(R.id.news_action_tv_comment) TextView mNewsActionTvComment;
+    @InjectView(R.id.news_action_button_comment_wrapper) LinearLayout mNewsActionButtonCommentWrapper;
+    @InjectView(R.id.news_action_tv_more) TextView mNewsActionTvMore;
+    @InjectView(R.id.news_action_button_more_wrapper) LinearLayout mNewsActionButtonMoreWrapper;
+    @InjectView(R.id.timeline_share_buttons) LinearLayout mTimelineShareButtons;
+    @InjectView(R.id.news_detail_desc) TextView mNewsDetailDesc;
+    @InjectView(R.id.news_detail_content) TextView mNewsDetailContent;
+    @InjectView(R.id.news_detail_loading) TextView mNewsDetailLoading;
+    @InjectView(R.id.news_detail_reference_gv) GridView mNewsDetailReferenceGv;
+    @InjectView(R.id.news_detail_reference_gv_container) LinearLayout mNewsDetailReferenceGvContainer;
+    @InjectView(R.id.news_detail_comment_list) ListView mNewsDetailCommentList;
+    @InjectView(R.id.news_detail_comment_empty) TextView mNewsDetailCommentEmpty;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -94,64 +104,32 @@ public class NewsDetailFragment extends DashboardFragment /*AbstractSecurityInfo
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.news_detail_view, container, false);
+        ButterKnife.inject(this, view);
         initViews(view);
         linkWith();
         return view;
     }
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState)
+    @OnClick(R.id.news_action_button_like_wrapper) void onClickLike(View v)
     {
-        super.onViewCreated(view, savedInstanceState);
-        //linkWith();
+        voteUpOrDown(sampleItemDto.voteDirection == 1 ? false : true);
     }
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState)
+    @OnClick(R.id.news_action_tv_more) void onClickTvMore(View v)
     {
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
+        showShareDialog();
     }
 
     private void initViews(View view)
     {
-        this.titleView = (TextView) view.findViewById(R.id.news_detail_title);
-        this.dateView = (TextView) view.findViewById(R.id.news_detail_date);
-        this.titlePlaceholderView = (ImageView) view.findViewById(R.id.news_detail_title_placeholder);
-        likeContainer = (LinearLayout) view.findViewById(R.id.news_action_button_like_wrapper);
-        likeContainer.setOnClickListener(new View.OnClickListener()
-        {
-            @Override public void onClick(View v)
-            {
-                voteUpOrDown(sampleItemDto.voteDirection == 1 ? false : true);
-            }
-        });
-        likeTextView = (TextView) view.findViewById(R.id.new_action_tv_like);
-        likeImageView = (ImageView) view.findViewById(R.id.new_action_iv_like);
-        moreTextView = (TextView) view.findViewById(R.id.news_action_tv_more);
         Typeface font = Typeface.createFromAsset(getActivity().getApplicationContext().getAssets(), "FontAwesome.ttf");
-        moreTextView.setTypeface(font);
-        moreTextView.setText("\uf141");
-        moreTextView.setOnClickListener(new View.OnClickListener()
-        {
-            @Override public void onClick(View v)
-            {
-                showShareDialog();
-            }
-        });
+        mNewsActionTvMore.setTypeface(font);
+        mNewsActionTvMore.setText("\uf141");
 
-        this.contentView = (TextView) view.findViewById(R.id.news_detail_content);
-        this.loadingView = (TextView) view.findViewById(R.id.news_detail_loading);
-        securityGridView = (GridView) view.findViewById(R.id.news_detail_reference_gv);
         simpleSecurityItemViewAdapter =
                 new SimpleSecurityItemViewAdapter(getActivity(), getActivity().getLayoutInflater(), R.layout.trending_security_item);
-        securityGridView.setAdapter(simpleSecurityItemViewAdapter);
-        securityGridView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        mNewsDetailReferenceGv.setAdapter(simpleSecurityItemViewAdapter);
+        mNewsDetailReferenceGv.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
             @Override public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
             {
@@ -162,32 +140,31 @@ public class NewsDetailFragment extends DashboardFragment /*AbstractSecurityInfo
                 getNavigator().pushFragment(BuySellFragment.class, args);
             }
         });
-        gridViewContainer = (LinearLayout) view.findViewById(R.id.news_detail_reference_gv_container);
     }
 
     private void linkWith()
     {
         Bundle args = getArguments();
         this.sampleItemDto = NewsItemDTO.getSampleNewsItemDTO(args);
-        this.titleView.setText(sampleItemDto.title);
+        mNewsDetailTitle.setText(sampleItemDto.title);
         PrettyTime prettyTime = new PrettyTime();
-        this.dateView.setText(prettyTime.format(sampleItemDto.createdAtUtc));
+        mNewsDetailDate.setText(prettyTime.format(sampleItemDto.createdAtUtc));
 
         int bgRes = args.getInt(BUNDLE_KEY_TITLE_BACKGROUND_RES, 0);
-        titlePlaceholderView.setImageResource(bgRes);
+        mNewsDetailTitlePlaceholder.setImageResource(bgRes);
 
         newsServiceWrapper.getSecurityNewsDetail(sampleItemDto.id, createNewsDetailCallback());
         //TODO change to R.string
-        likeTextView.setText(sampleItemDto.voteDirection == 1 ? "Unlike" : "Like");
-        likeImageView.setImageResource(sampleItemDto.voteDirection == 1 ? R.drawable.icn_actions_downvote : R.drawable.icn_actions_upvote);
+        mNewActionTvLike.setText(sampleItemDto.voteDirection == 1 ? "Unlike" : "Like");
+        mNewActionIvLike.setImageResource(sampleItemDto.voteDirection == 1 ? R.drawable.icn_actions_downvote : R.drawable.icn_actions_upvote);
     }
 
     private void fillDetailData(NewsItemDTO data)
     {
         this.detailItemDto = data;
-        this.contentView.setText(detailItemDto.text);
-        contentView.setVisibility(View.VISIBLE);
-        loadingView.setVisibility(View.GONE);
+        mNewsDetailContent.setText(detailItemDto.text);
+        mNewsDetailContent.setVisibility(View.VISIBLE);
+        mNewsDetailLoading.setVisibility(View.GONE);
 
         securityServiceWrapper.getMultipleSecurities2(createNewsDetailSecurityCallback(),
                 detailItemDto.getSecurityIds());
@@ -219,11 +196,11 @@ public class NewsDetailFragment extends DashboardFragment /*AbstractSecurityInfo
             @Override
             public void success(List<SecurityCompactDTO> securityCompactDTOList, Response response)
             {
-                ViewGroup.LayoutParams lp = gridViewContainer.getLayoutParams();
+                ViewGroup.LayoutParams lp = mNewsDetailReferenceGvContainer.getLayoutParams();
                 //TODO it changes with solution
                 lp.width = 510 * securityCompactDTOList.size();
-                gridViewContainer.setLayoutParams(lp);
-                securityGridView.setNumColumns(securityCompactDTOList.size());
+                mNewsDetailReferenceGvContainer.setLayoutParams(lp);
+                mNewsDetailReferenceGv.setNumColumns(securityCompactDTOList.size());
                 simpleSecurityItemViewAdapter.setItems(securityCompactDTOList);
                 simpleSecurityItemViewAdapter.notifyDataSetChanged();
             }
@@ -259,8 +236,8 @@ public class NewsDetailFragment extends DashboardFragment /*AbstractSecurityInfo
             public void success(DiscussionDTO discussionDTO, Response response)
             {
                 THToast.show("vote " + ((towardUp ? "up" : "down")) + " success");
-                likeTextView.setText(towardUp ? "Unlike" : "Like");
-                likeImageView.setImageResource(towardUp ? R.drawable.icn_actions_downvote : R.drawable.icn_actions_upvote);
+                mNewActionTvLike.setText(towardUp ? "Unlike" : "Like");
+                mNewActionIvLike.setImageResource(towardUp ? R.drawable.icn_actions_downvote : R.drawable.icn_actions_upvote);
                 sampleItemDto.voteDirection = towardUp ? 1 : 0;
             }
 
