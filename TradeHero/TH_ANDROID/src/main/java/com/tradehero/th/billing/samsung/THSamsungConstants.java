a5614ffb6e3680f0654069cbd975ea957d3cf97d
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-03-26 10:41:07 +0800

    First constants for Samsung [#66838718]

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/billing/samsung/THSamsungConstants.java
@@ -0,0 +1,11 @@
+package com.tradehero.th.billing.samsung;
+
+import com.tradehero.common.billing.samsung.SamsungConstants;
+
+/**
+ * Created by xavier on 3/26/14.
+ */
+public class THSamsungConstants extends SamsungConstants
+{
+    public static final double IAP_ITEM_GROUP_ID = 100000103210d;
+}
