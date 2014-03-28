fa55d36381cbd4bbe40d41e28b8a5fb0c0b25e3f
Alex Li <youyoum4a1@hotmail.com>
2014-03-28 18:41:27 +0800

    enable payment of alipay without server [#66486830]

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/billing/StoreScreenFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/billing/StoreScreenFragment.java
@@ -1,6 +1,7 @@
 package com.tradehero.th.fragments.billing;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,10 @@ import android.widget.ListView;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
+import com.alipay.android.app.sdk.AliPay;
 import com.localytics.android.LocalyticsSession;
+import com.tradehero.common.billing.alipay.Result;
+import com.tradehero.common.billing.alipay.Rsa;
 import com.tradehero.common.billing.googleplay.exception.IABBillingUnavailableException;
 import com.tradehero.common.utils.THToast;
 import com.tradehero.th.R;
@@ -24,7 +28,9 @@ import com.tradehero.th.fragments.social.follower.FollowerManagerFragment;
 import com.tradehero.th.fragments.social.hero.HeroManagerFragment;
 import com.tradehero.th.fragments.tutorial.WithTutorial;
 import com.tradehero.th.persistence.portfolio.PortfolioCompactListRetrievedMilestone;
+import com.tradehero.th.utils.Constants;
 import com.tradehero.th.utils.LocalyticsConstants;
+import java.net.URLEncoder;
 import javax.inject.Inject;
 import timber.log.Timber;
 
@@ -112,7 +118,7 @@ public class StoreScreenFragment extends BasePurchaseManagerFragment
         switch (position)
         {
             case StoreItemAdapter.POSITION_BUY_VIRTUAL_DOLLARS:
-                userInteractor.conditionalPopBuyVirtualDollars();
+                //userInteractor.conditionalPopBuyVirtualDollars();
                 beginAlipay(StoreItemAdapter.POSITION_BUY_VIRTUAL_DOLLARS);
                 break;
 
@@ -147,8 +153,78 @@ public class StoreScreenFragment extends BasePurchaseManagerFragment
     private void beginAlipay(int type)
     {
         Timber.d("lyl beginAlipay");
-        //String info = getNewOrderInfo(position);
-        //String sign = Rsa.sign(info, Keys.PRIVATE);
+        String info = getNewOrderInfo();
+        String sign = Rsa.sign(info, Constants.PRIVATE);
+        sign = URLEncoder.encode(sign);
+        info += "&sign=\"" + sign + "\"&" + "sign_type=\"RSA\"";
+        Timber.d("lyl info=%s", info);
+
+        final String orderInfo = info;
+        new Thread() {
+            public void run() {
+                AliPay alipay = new AliPay(getSherlockActivity(), mHandler);
+                String result = alipay.pay(orderInfo);
+                Timber.d("lyl result=%s", result);
+            }
+        }.start();
+    }
+
+    Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            Result result = new Result((String) msg.obj);
+            Timber.d("lyl result=%s", result);
+            //switch (msg.what) {
+            //    case RQF_PAY:
+            //    case RQF_LOGIN: {
+            //        Toast.makeText(getSherlockActivity(), result.getResult(),
+            //                Toast.LENGTH_SHORT).show();
+            //
+            //    }
+            //    break;
+            //    default:
+            //        break;
+            //}
+        };
+    };
+
+    private String getOutTradeNo() {
+
+
+        //return key;
+        return "alipay-2";
+    }
+
+    private String getNewOrderInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("partner=\"");
+        sb.append(Constants.DEFAULT_PARTNER);
+        sb.append("\"&out_trade_no=\"");
+        sb.append(getOutTradeNo());
+        sb.append("\"&subject=\"");
+        sb.append("subject");
+        sb.append("\"&body=\"");
+        sb.append("body");
+        sb.append("\"&total_fee=\"");
+        sb.append("0.01");
+        sb.append("\"&notify_url=\"");
+
+        // 网址需要做URL编码
+        //sb.append(URLEncoder.encode("http://notify.java.jpxx.org/index.jsp"));
+        sb.append(URLEncoder.encode("https://www.tradehero.mobi/api/alipay/notify"));
+        sb.append("\"&service=\"mobile.securitypay.pay");
+        sb.append("\"&_input_charset=\"UTF-8");
+        sb.append("\"&return_url=\"");
+        sb.append(URLEncoder.encode("http://m.alipay.com"));
+        sb.append("\"&payment_type=\"1");
+        sb.append("\"&seller_id=\"");
+        sb.append(Constants.DEFAULT_SELLER);
+
+        // 如果show_url值为空，可不传
+        // sb.append("\"&show_url=\"");
+        sb.append("\"&it_b_pay=\"1m");
+        sb.append("\"");
+
+        return new String(sb);
     }
 
     private void pushStockAlertFragment()
