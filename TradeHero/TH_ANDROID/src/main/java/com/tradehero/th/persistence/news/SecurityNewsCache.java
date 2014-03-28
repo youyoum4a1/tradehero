e2801cdf1d84f0dc88843655bf3dedb4b285dbc3
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-03-28 12:55:53 +0800

    Remove id from securityId

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/persistence/news/SecurityNewsCache.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/persistence/news/SecurityNewsCache.java
@@ -4,13 +4,14 @@ import com.tradehero.common.persistence.StraightDTOCache;
 import com.tradehero.th.api.PaginatedDTO;
 import com.tradehero.th.api.news.NewsCache;
 import com.tradehero.th.api.news.NewsItemDTO;
+import com.tradehero.th.api.security.SecurityCompactDTO;
 import com.tradehero.th.api.security.SecurityId;
 import com.tradehero.th.network.service.NewsServiceWrapper;
+import com.tradehero.th.persistence.security.SecurityCompactCache;
 import dagger.Lazy;
 import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import timber.log.Timber;
 
 /**
  * Created by tradehero on 14-3-7.
@@ -21,23 +22,28 @@ public class SecurityNewsCache extends StraightDTOCache<SecurityId, PaginatedDTO
     public static final int DEFAULT_MAX_SIZE = 15;
     private final Lazy<NewsServiceWrapper> newsServiceWrapper;
     private final Lazy<NewsCache> newsCache;
+    private final Lazy<SecurityCompactCache> securityCompactCache;
 
-    @Inject public SecurityNewsCache(Lazy<NewsCache> newsCache, Lazy<NewsServiceWrapper> newsServiceWrapper)
+    @Inject public SecurityNewsCache(
+            Lazy<NewsCache> newsCache,
+            Lazy<SecurityCompactCache> securityCompactCache,
+            Lazy<NewsServiceWrapper> newsServiceWrapper)
     {
         super(DEFAULT_MAX_SIZE);
+        this.securityCompactCache = securityCompactCache;
         this.newsServiceWrapper = newsServiceWrapper;
         this.newsCache = newsCache;
     }
 
     @Override protected PaginatedDTO<NewsItemDTO> fetch(SecurityId key) throws Throwable
     {
-        Timber.d("NewsHeadlineList fetch news, key:%s", key);
-        return fetchSecurityNews(key.id);
+        return fetchSecurityNews(key);
     }
 
-    private PaginatedDTO<NewsItemDTO> fetchSecurityNews(int securityId) throws Throwable
+    private PaginatedDTO<NewsItemDTO> fetchSecurityNews(SecurityId securityId) throws Throwable
     {
-        PaginatedDTO<NewsItemDTO> paginatedSecurityNews = newsServiceWrapper.get().getSecurityNews(securityId);
+        SecurityCompactDTO security = securityCompactCache.get().getOrFetch(securityId);
+        PaginatedDTO<NewsItemDTO> paginatedSecurityNews = newsServiceWrapper.get().getSecurityNews(security.id);
 
         // populate to NewsCache
         List<NewsItemDTO> newsList = paginatedSecurityNews.getData();
