0571f663f872cbe6ee1efecd98cb4622e8a9b6c6
Alex Li <youyoum4a1@hotmail.com>
2014-04-01 20:03:07 +0800

    enable payment of alipay activity need add portfolio id [#66486830]

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/billing/StoreScreenFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/billing/StoreScreenFragment.java
@@ -1,9 +1,9 @@
 package com.tradehero.th.fragments.billing;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,19 +13,14 @@ import android.widget.ListView;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
-import com.alipay.android.app.net.SSLSocketFactoryEx;
-import com.alipay.android.app.sdk.AliPay;
 import com.localytics.android.LocalyticsSession;
 import com.tradehero.common.billing.alipay.AlipayActivity;
-import com.tradehero.common.billing.alipay.Result;
-import com.tradehero.common.billing.alipay.Rsa;
 import com.tradehero.common.billing.googleplay.exception.IABBillingUnavailableException;
 import com.tradehero.common.utils.THToast;
 import com.tradehero.th.R;
 import com.tradehero.th.activities.DashboardActivity;
 import com.tradehero.th.api.portfolio.OwnedPortfolioId;
 import com.tradehero.th.api.users.CurrentUserId;
-import com.tradehero.th.base.THUser;
 import com.tradehero.th.billing.googleplay.THIABAlertDialogUtil;
 import com.tradehero.th.billing.googleplay.THIABUserInteractor;
 import com.tradehero.th.fragments.alert.AlertManagerFragment;
@@ -33,30 +28,8 @@ import com.tradehero.th.fragments.social.follower.FollowerManagerFragment;
 import com.tradehero.th.fragments.social.hero.HeroManagerFragment;
 import com.tradehero.th.fragments.tutorial.WithTutorial;
 import com.tradehero.th.persistence.portfolio.PortfolioCompactListRetrievedMilestone;
-import com.tradehero.th.utils.Constants;
 import com.tradehero.th.utils.LocalyticsConstants;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.security.KeyStore;
 import javax.inject.Inject;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpVersion;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParams;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
 import timber.log.Timber;
 
 public class StoreScreenFragment extends BasePurchaseManagerFragment
@@ -65,6 +38,7 @@ public class StoreScreenFragment extends BasePurchaseManagerFragment
     public static final String TAG = StoreScreenFragment.class.getSimpleName();
 
     public static boolean alreadyNotifiedNeedCreateAccount = false;
+    public static int mAlipayType = 0;
 
     @Inject CurrentUserId currentUserId;
     @Inject THIABAlertDialogUtil THIABAlertDialogUtil;
@@ -145,21 +119,23 @@ public class StoreScreenFragment extends BasePurchaseManagerFragment
         switch (position)
         {
             case StoreItemAdapter.POSITION_BUY_VIRTUAL_DOLLARS:
-                //userInteractor.conditionalPopBuyVirtualDollars();
-                Intent intent = new Intent(getSherlockActivity(), AlipayActivity.class);
-                getSherlockActivity().startActivity(intent);
+                userInteractor.conditionalPopBuyVirtualDollars();
+                showBuyDialog(position);
                 break;
 
             case StoreItemAdapter.POSITION_BUY_FOLLOW_CREDITS:
                 userInteractor.conditionalPopBuyFollowCredits();
+                showBuyDialog(position);
                 break;
 
             case StoreItemAdapter.POSITION_BUY_STOCK_ALERTS:
                 userInteractor.conditionalPopBuyStockAlerts();
+                showBuyDialog(position);
                 break;
 
             case StoreItemAdapter.POSITION_BUY_RESET_PORTFOLIO:
                 userInteractor.conditionalPopBuyResetPortfolio();
+                showBuyDialog(position);
                 break;
 
             case StoreItemAdapter.POSITION_MANAGE_HEROES:
@@ -178,132 +154,54 @@ public class StoreScreenFragment extends BasePurchaseManagerFragment
         }
     }
 
-    private void beginAlipay(int type)
+    private void showBuyDialog(int type)
     {
-        Timber.d("lyl beginAlipay");
-        String info = getNewOrderInfo();
-        String sign = Rsa.sign(info, Constants.PRIVATE);
-        sign = URLEncoder.encode(sign);
-        info += "&sign=\"" + sign + "\"&" + "sign_type=\"RSA\"";
-        Timber.d("lyl info=%s", info);
-
-        AliPay alipay = new AliPay(getSherlockActivity(), mHandler);
-        String result = alipay.pay(info);
-        Timber.d("lyl result=%s", result);
-
-        Message msg = new Message();
-        msg.what = 1;//RQF_PAY;
-        msg.obj = result;
-        mHandler.sendMessage(msg);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        int array = 0;
+        mAlipayType = type;
+        switch (type)
+        {
+            case StoreItemAdapter.POSITION_BUY_VIRTUAL_DOLLARS:
+                array = R.array.alipay_virtual_dollars_array;
+                break;
+            case StoreItemAdapter.POSITION_BUY_FOLLOW_CREDITS:
+                array = R.array.alipay_follow_credits_array;
+                break;
+            case StoreItemAdapter.POSITION_BUY_STOCK_ALERTS:
+                array = R.array.alipay_stock_alerts_array;
+                break;
+            case StoreItemAdapter.POSITION_BUY_RESET_PORTFOLIO:
+                array = R.array.alipay_reset_portfolio_array;
+                break;
+        }
+        builder.setTitle(R.string.app_name)
+                .setItems(getResources().getStringArray(array),
+                        new DialogInterface.OnClickListener()
+                        {
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                final int position = which;
+                                new Thread()
+                                {
+                                    @Override
+                                    public void run()
+                                    {
+                                        startAlipay(position);
+                                    }
+                                }.start();
+                            }
+                        });
+        builder.create().show();
     }
 
-    Handler mHandler = new Handler()
+    private void startAlipay(int position)
     {
-        public void handleMessage(android.os.Message msg)
-        {
-            Result result = new Result((String) msg.obj);
-            Timber.d("lyl result=%s", result);
-            Timber.d("lyl result=%s", result.getResult());
-            //switch (msg.what) {
-            //    case RQF_PAY:
-            //    case RQF_LOGIN: {
-            //        Toast.makeText(getSherlockActivity(), result.getResult(),
-            //                Toast.LENGTH_SHORT).show();
-            //
-            //    }
-            //    break;
-            //    default:
-            //        break;
-            //}
-        }
+        Timber.d("lyl startAlipay type=%d position=%d", mAlipayType, position);
 
-        ;
-    };
-
-    private String getNewOrderInfo()
-    {
-        StringBuilder sb = new StringBuilder();
-        sb.append("partner=\"");
-        sb.append(Constants.DEFAULT_PARTNER);
-        sb.append("\"&out_trade_no=\"");
-        sb.append("alipay-" + getOrderId());
-        sb.append("\"&subject=\"");
-        sb.append("subject");
-        sb.append("\"&body=\"");
-        sb.append("body");
-        sb.append("\"&total_fee=\"");
-        sb.append("0.01");
-        sb.append("\"&notify_url=\"");
-
-        // 网址需要做URL编码
-        sb.append(URLEncoder.encode("http://www.tradehero.mobi/api/alipay/notify"));
-        sb.append("\"&service=\"mobile.securitypay.pay");
-        sb.append("\"&_input_charset=\"UTF-8");
-        sb.append("\"&return_url=\"");
-        sb.append(URLEncoder.encode("http://m.alipay.com"));
-        sb.append("\"&payment_type=\"1");
-        sb.append("\"&seller_id=\"");
-        sb.append(Constants.DEFAULT_SELLER);
-
-        // 如果show_url值为空，可不传
-        // sb.append("\"&show_url=\"");
-        sb.append("\"&it_b_pay=\"1m");
-        sb.append("\"");
-
-        return new String(sb);
-    }
-
-    private String getOrderId()
-    {
-        String url = "https://www.tradehero.mobi/api/alipay/createOrder/10";
-        String resultStr = "";
-        HttpClient httpclient = getNewHttpClient();
-
-        HttpPost httppost = new HttpPost(url);
-        try
-        {
-            httppost.addHeader(Constants.AUTHORIZATION, THUser.getAuthHeader());
-            HttpResponse response;
-            response = httpclient.execute(httppost);
-            resultStr = EntityUtils.toString(response.getEntity());
-            Timber.d("lyl orderId=%s", resultStr);
-        } catch (UnsupportedEncodingException e)
-        {
-            Timber.d("lyl UnsupportedEncodingException");
-            e.printStackTrace();
-        } catch (ClientProtocolException e)
-        {
-            Timber.d("lyl ClientProtocolException");
-            e.printStackTrace();
-        } catch (IOException e)
-        {
-            Timber.d("lyl IOException");
-            e.printStackTrace();
-        }
-
-        return resultStr;
-    }
-
-    public static HttpClient getNewHttpClient()
-    {
-        try
-        {
-            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            trustStore.load(null, null);
-            SSLSocketFactory sf = new SSLSocketFactoryEx(trustStore);
-            sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-            HttpParams params = new BasicHttpParams();
-            HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-            HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
-            SchemeRegistry registry = new SchemeRegistry();
-            registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-            registry.register(new Scheme("https", sf, 443));
-            ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
-            return new DefaultHttpClient(ccm, params);
-        } catch (Exception e)
-        {
-            return new DefaultHttpClient();
-        }
+        Intent intent = new Intent(getSherlockActivity(), AlipayActivity.class);
+        intent.putExtra(AlipayActivity.ALIPAY_TYPE_KEY, mAlipayType);
+        intent.putExtra(AlipayActivity.ALIPAY_POSITION_KEY, position);
+        getSherlockActivity().startActivity(intent);
     }
 
     private void pushStockAlertFragment()
