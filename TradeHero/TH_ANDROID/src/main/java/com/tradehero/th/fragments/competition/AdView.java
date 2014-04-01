98be1bf101c4f90aca325abb43fc05729ce1ce93
Nguyen Truong Tho <nguyentruongtho.sg@gmail.com>
2014-04-01 21:46:19 +0800

    [Feature #68529336] Open webpage when user click on the tile

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/competition/AdView.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/competition/AdView.java
@@ -1,12 +1,22 @@
 package com.tradehero.th.fragments.competition;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.util.AttributeSet;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import com.squareup.picasso.Picasso;
+import com.tradehero.th.R;
 import com.tradehero.th.api.DTOView;
 import com.tradehero.th.api.competition.AdDTO;
+import com.tradehero.th.api.users.CurrentUserId;
+import com.tradehero.th.base.Navigator;
+import com.tradehero.th.base.NavigatorActivity;
 import com.tradehero.th.fragments.competition.zone.dto.CompetitionZoneAdvertisementDTO;
+import com.tradehero.th.fragments.web.WebViewFragment;
 import com.tradehero.th.utils.DaggerUtils;
 import dagger.Lazy;
 import javax.inject.Inject;
@@ -14,10 +24,30 @@ import javax.inject.Inject;
 /**
  * Created by tho on 4/1/2014.
  */
-public class AdView extends ImageView
+public class AdView extends RelativeLayout
     implements DTOView<CompetitionZoneAdvertisementDTO>
 {
+    @InjectView(R.id.banner) ImageView banner;
+    private AdDTO adDTO;
+
+    @OnClick(R.id.banner) void onBannerClicked(ImageView banner)
+    {
+        if (adDTO != null)
+        {
+            Bundle bundle = new Bundle();
+            String url = adDTO.redirectUrl + String.format("&userId=%d", currentUserId.get());
+            bundle.putString(WebViewFragment.BUNDLE_KEY_URL, url);
+            getNavigator().pushFragment(WebViewFragment.class, bundle);
+        }
+    }
+
+    private Navigator getNavigator()
+    {
+        return ((NavigatorActivity) getContext()).getNavigator();
+    }
+
     @Inject Lazy<Picasso> picasso;
+    @Inject CurrentUserId currentUserId;
 
     //<editor-fold desc="Constructors">
     public AdView(Context context)
@@ -39,6 +69,7 @@ public class AdView extends ImageView
     @Override protected void onFinishInflate()
     {
         super.onFinishInflate();
+        ButterKnife.inject(this);
         DaggerUtils.inject(this);
     }
 
@@ -50,12 +81,34 @@ public class AdView extends ImageView
         }
     }
 
+    @Override protected void onDetachedFromWindow()
+    {
+        ButterKnife.reset(this);
+        super.onDetachedFromWindow();
+    }
+
     private void linkWith(AdDTO adDTO, boolean andDisplay)
     {
+        if (adDTO != null)
+        {
+            this.adDTO = adDTO;
+        }
+        else
+        {
+            this.adDTO = null;
+        }
+
         if (andDisplay)
         {
-            picasso.get().load(adDTO.bannerImageUrl)
-                    .into(this);
+            if (adDTO != null)
+            {
+                picasso.get().load(adDTO.bannerImageUrl)
+                        .into(banner);
+            }
+            else
+            {
+                banner.setImageDrawable(null);
+            }
         }
     }
 }
