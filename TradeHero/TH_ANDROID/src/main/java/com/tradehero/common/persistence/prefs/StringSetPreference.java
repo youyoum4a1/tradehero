e500b16220794e5adb797cdfa6f91863b6560168
xavier <xavier.lepretre@gmail>
2014-04-08 16:58:48 +0800

    Added convenience method

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/persistence/prefs/StringSetPreference.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/persistence/prefs/StringSetPreference.java
@@ -1,6 +1,9 @@
 package com.tradehero.common.persistence.prefs;
 
 import android.content.SharedPreferences;
+
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 import javax.inject.Inject;
 
@@ -22,5 +25,30 @@ public class StringSetPreference extends AbstractPreference<Set<String>>
     @Override public void set(Set<String> value)
     {
         preference.edit().putStringSet(key, value).apply();
+    }
+
+    /**
+     * Adds all and saves
+     * @param values
+     */
+    public void add(Collection<String> values)
+    {
+        if (values == null)
+        {
+            return;
+        }
+
+        Set<String> current = get();
+        if (current == null)
+        {
+            current = new HashSet<>();
+        }
+
+        for (String value: values)
+        {
+            current.add(value);
+        }
+
+        preference.edit().putStringSet(key, current).apply();
     }
 }
