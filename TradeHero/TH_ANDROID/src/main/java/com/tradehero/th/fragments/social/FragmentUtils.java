68698dc726264a5ae58abbf954101f40b9081b99
xjwangliang <shigushuyuan@gmail.com>
2014-04-12 16:12:10 +0800

    save changes

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/social/FragmentUtils.java
@@ -0,0 +1,30 @@
+package com.tradehero.th.fragments.social;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+ 
+public class FragmentUtils {
+    //https://gist.github.com/keyboardr/5455206
+    /**
+	 * @param frag
+	 *            The Fragment whose parent is to be found
+	 * @param callbackInterface
+	 *            The interface class that the parent should implement
+	 * @return The parent of frag that implements the callbackInterface or null
+	 *         if no such parent can be found
+	 */
+	@SuppressWarnings("unchecked") // Casts are checked using runtime methods
+	public static <T> T getParent(Fragment frag, Class<T> callbackInterface) {
+		Fragment parentFragment = frag.getParentFragment();
+		if (parentFragment != null
+				&& callbackInterface.isInstance(parentFragment)) {
+			return (T) parentFragment;
+		} else {
+			FragmentActivity activity = frag.getActivity();
+			if (activity != null && callbackInterface.isInstance(activity)) {
+				return (T) activity;
+			}
+		}
+		return null;
+	}
+}
