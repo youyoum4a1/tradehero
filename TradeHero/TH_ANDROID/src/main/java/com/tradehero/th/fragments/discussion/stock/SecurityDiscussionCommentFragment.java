3e5cd06f16eb8bd4f486ef0e586ef7306ebf9513
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-04-15 11:40:30 +0800

    Placed callbacks so that we can pop paid follow dialogs to the user when he cannot send any more private messages. [#67896192]

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discussion/stock/SecurityDiscussionCommentFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discussion/stock/SecurityDiscussionCommentFragment.java
@@ -7,15 +7,17 @@ import android.view.ViewGroup;
 import com.tradehero.th.R;
 import com.tradehero.th.fragments.discussion.AbstractDiscussionFragment;
 
-/**
- * Created by thonguyen on 12/4/14.
- */
 public class SecurityDiscussionCommentFragment extends AbstractDiscussionFragment
 {
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.security_discussion_comment, container, false);
         return view;
+    }
+
+    @Override protected void initViews(View view)
+    {
+        // Nothing to do
     }
 
     @Override public void onDestroyView()
---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/social/message/PrivatePostCommentView.java
@@ -0,0 +1,75 @@
+package com.tradehero.th.fragments.social.message;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import com.tradehero.th.api.discussion.MessageStatusDTO;
+import com.tradehero.th.fragments.discussion.PostCommentView;
+
+public class PrivatePostCommentView extends PostCommentView
+{
+    private OnMessageNotAllowedToSendListener messageNotAllowedToSendListener;
+    private MessageStatusDTO messageStatusDTO;
+    
+    //<editor-fold desc="Constructors">
+    public PrivatePostCommentView(Context context)
+    {
+        super(context);
+    }
+
+    public PrivatePostCommentView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public PrivatePostCommentView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+    }
+    //</editor-fold>
+
+    @Override protected void onDetachedFromWindow()
+    {
+        this.messageNotAllowedToSendListener = null;
+        super.onDetachedFromWindow();
+    }
+
+    public void setMessageNotAllowedToSendListener(OnMessageNotAllowedToSendListener messageNotAllowedToSendListener)
+    {
+        this.messageNotAllowedToSendListener = messageNotAllowedToSendListener;
+    }
+
+    public void setMessageStatusDTO(MessageStatusDTO messageStatusDTO)
+    {
+        this.messageStatusDTO = messageStatusDTO;
+    }
+
+    /**
+     * Here we want to make sure we are allowed to post a comment before doing it
+     */
+    @Override protected void postComment()
+    {
+        if (messageStatusDTO != null && messageStatusDTO.privateFreeRemainingCount <= 0 &&
+                messageNotAllowedToSendListener != null)
+        {
+            notifyPreSubmissionInterceptListener();
+        }
+        else 
+        {
+            super.postComment();
+        }
+    }
+
+    protected void notifyPreSubmissionInterceptListener()
+    {
+        OnMessageNotAllowedToSendListener listener = messageNotAllowedToSendListener;
+        if (listener != null)
+        {
+            listener.onMessageNotAllowedToSend();
+        }
+    }
+    
+    public static interface OnMessageNotAllowedToSendListener
+    {
+        void onMessageNotAllowedToSend();
+    }
+}
