b37ca262f80a62f90b01c823c122ce9485d75768
Alex Li <youyoum4a1@hotmail.com>
2014-04-15 13:46:38 +0800

    enable share to weChat on buySellScreen

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/SocialSharer.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/SocialSharer.java
@@ -2,6 +2,7 @@ package com.tradehero.th.utils;
 
 import android.content.Context;
 import com.tradehero.common.persistence.DTOKey;
+import com.tradehero.th.api.security.SecurityCompactDTO;
 
 /**
  * Created by alex on 14-4-8.
@@ -9,4 +10,5 @@ import com.tradehero.common.persistence.DTOKey;
 public interface SocialSharer
 {
     void share(Context context, DTOKey shareDtoKey);
+    void share(Context context, SecurityCompactDTO securityCompactDTO);
 }
