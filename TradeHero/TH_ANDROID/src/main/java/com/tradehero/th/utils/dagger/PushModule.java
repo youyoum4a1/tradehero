adddf369d39d2cd481859c550a8ae9739f2d8ddf
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-04-17 16:37:20 +0800

    Remove secret key from BaiduPush & fix injection, compilation problem

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/dagger/PushModule.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/dagger/PushModule.java
@@ -1,19 +1,8 @@
 package com.tradehero.th.utils.dagger;
 
-import android.content.SharedPreferences;
-import com.tradehero.th.activities.DashboardActivity;
-import com.tradehero.th.api.portfolio.DisplayablePortfolioUtil;
-import com.tradehero.th.api.users.CurrentUserId;
-import com.tradehero.th.loaders.FriendListLoader;
 import com.tradehero.th.models.push.DeviceTokenHelper;
-import com.tradehero.th.models.push.baidu.BaiduPushManager;
-import com.tradehero.th.models.push.baidu.PushSender;
-import com.tradehero.th.persistence.social.VisitedFriendListPrefs;
-import com.tradehero.th.persistence.user.AbstractUserStore;
-import com.tradehero.th.persistence.user.UserManager;
-import com.tradehero.th.persistence.user.UserStore;
+import com.tradehero.th.models.push.baidu.BaiduPushMessageReceiver;
 import com.tradehero.th.utils.ForBaiduPush;
-import com.tradehero.th.wxapi.WXEntryActivity;
 import dagger.Module;
 import dagger.Provides;
 import javax.inject.Singleton;
@@ -23,23 +12,20 @@ import javax.inject.Singleton;
  */
 @Module(
         injects = {
-                BaiduPushManager.class,
-                PushSender.class
-
+                BaiduPushMessageReceiver.class,
         },
         staticInjections = {
                 DeviceTokenHelper.class
         },
-        complete = false
+        complete = false,
+        library = true
 )
 public class PushModule
 {
     private static final String BAIDU_API_KEY = "iI9WWqP3SfGApTW37UuSyIdc";
-    private static final String BAIDU_SECRET_KEY = "i5xkWnVUQLYE703cYG85QoSkrPwjl3ip";
 
     @Provides @Singleton @ForBaiduPush String provideBaiduAppKey()
     {
         return BAIDU_API_KEY;
     }
-
 }
