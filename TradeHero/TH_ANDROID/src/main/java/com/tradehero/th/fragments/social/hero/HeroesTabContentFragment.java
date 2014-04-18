2c7d1e0022f44641f1a3211fa9b31c0100bcde8c
xjwangliang <shigushuyuan@gmail.com>
2014-04-18 19:38:34 +0800

    use message.hasCode as nofication id

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/social/hero/HeroesTabContentFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/social/hero/HeroesTabContentFragment.java
@@ -227,7 +227,8 @@ public class HeroesTabContentFragment extends BasePurchaseManagerFragment
                 if (infoFetcher != null)
                 {
                     Timber.d("onUserFollowSuccess,fetchHeroes");
-                    infoFetcher.fetchHeroes(followerId,getHeroType());
+                    infoFetcher.getHeros(followerId,getHeroType());
+                    //infoFetcher.fetchHeroes(followerId,getHeroType());
                 }
             }
 
