8893e6a76b0fbc209f04c3384036cee36220dea0
xjwangliang <shigushuyuan@gmail.com>
2014-04-18 19:19:02 +0800

    change url

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/NetworkConstants.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/NetworkConstants.java
@@ -6,8 +6,8 @@ package com.tradehero.th.network;
 public class NetworkConstants
 {
     public static final String YAHOO_FINANCE_ENDPOINT = "http://finance.yahoo.com";
-    public static final String TRADEHERO_PROD_ENDPOINT = "https://www.tradehero.mobi/api/";
-    //public static final String TRADEHERO_PROD_ENDPOINT = "https://192.168.20.49/api/";
+    //public static final String TRADEHERO_PROD_ENDPOINT = "https://www.tradehero.mobi/api/";
+    public static final String TRADEHERO_PROD_ENDPOINT = "https://192.168.20.49/api/";
     //public static final String TRADEHERO_PROD_ENDPOINT = "https://thvm-proxy.cloudapp.net:8000/api";
     public static final String COMPETITION_PATH = "competitionpages/";
 
