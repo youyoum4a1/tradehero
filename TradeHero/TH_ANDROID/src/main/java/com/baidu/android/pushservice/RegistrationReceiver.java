3956fd9e1c3369c2ffe0011ba94a3feb7541fe8b
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-04-23 16:06:05 +0800

    Fix crash caused by Baidu push, with international phone (Chinese phone works fine without this fix)

---
+++ TradeHero/TH_ANDROID/src/main/java/com/baidu/android/pushservice/RegistrationReceiver.java
@@ -0,0 +1,16 @@
+package com.baidu.android.pushservice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by tho on 4/23/2014.
+ */
+public class RegistrationReceiver extends BroadcastReceiver
+{
+    @Override public void onReceive(Context context, Intent intent)
+    {
+        // do nothing, since it is for China
+    }
+}
