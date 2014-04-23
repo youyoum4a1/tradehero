72a2652911780d602b78a10e9f3abc19c6594234
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-04-23 16:03:49 +0800

    Cleanup.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/timeline/TimelineFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/timeline/TimelineFragment.java
@@ -817,16 +817,6 @@ public class TimelineFragment extends BasePurchaseManagerFragment
     {
         return false;
     }
-
-    // TODO put this thing closer to the object that needs it
-    @Deprecated
-    public static void viewProfile(DashboardNavigatorActivity navigatorActivity, int userId)
-    {
-        Bundle bundle = new Bundle();
-        DashboardNavigator navigator = navigatorActivity.getDashboardNavigator();
-        bundle.putInt(TimelineFragment.BUNDLE_KEY_SHOW_USER_ID, userId);
-        navigator.pushFragment(PushableTimelineFragment.class, bundle);
-    }
     //</editor-fold>
 
     protected void freeFollow(Callback<UserProfileDTO> followCallback)
