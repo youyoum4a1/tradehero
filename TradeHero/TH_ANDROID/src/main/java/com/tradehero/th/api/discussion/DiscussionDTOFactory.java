4852161005007f8a19dcb27c34d58e012c99ce3b
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-04-23 16:21:19 +0800

    Fix bug whereby the discussionDTO is lost if not Private type. [#69989086]

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/discussion/DiscussionDTOFactory.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/discussion/DiscussionDTOFactory.java
@@ -44,9 +44,6 @@ public class DiscussionDTOFactory
                         ((PrivateDiscussionDTO) created).putAll(discussionDTO, PrivateDiscussionDTO.class);
                     }
                     break;
-
-                default:
-                    created = new DiscussionDTO();
             }
         }
         return created;
