139625aa7713dd0a898364000253cdd5c695bb96
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-04-24 19:19:32 +0800

    Have the competition leaderboard mark user list shows the competition name, and not the leaderboard name.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/competition/CompetitionDTO.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/competition/CompetitionDTO.java
@@ -4,11 +4,8 @@ import com.tradehero.common.persistence.DTO;
 import com.tradehero.th.api.competition.key.CompetitionId;
 import com.tradehero.th.api.leaderboard.LeaderboardDefDTO;
 
-/** Created with IntelliJ IDEA. User: xavier Date: 11/14/13 Time: 7:36 PM To change this template use File | Settings | File Templates. */
 public class CompetitionDTO implements DTO
 {
-    public static final String TAG = CompetitionDTO.class.getSimpleName();
-
     public int id;
     public LeaderboardDefDTO leaderboard;
     public String name;
