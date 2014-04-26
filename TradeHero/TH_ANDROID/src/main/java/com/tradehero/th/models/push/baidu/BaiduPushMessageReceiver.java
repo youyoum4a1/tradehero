9ec665938affcc6beb42e6adef7ea5820d988489
xjwangliang <shigushuyuan@gmail.com>
2014-04-27 01:10:02 +0800

    add notification click event

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/push/baidu/BaiduPushMessageReceiver.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/push/baidu/BaiduPushMessageReceiver.java
@@ -2,11 +2,15 @@ package com.tradehero.th.models.push.baidu;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.text.TextUtils;
 import com.baidu.frontia.api.FrontiaPushMessageReceiver;
 import com.tradehero.th.R;
 import com.tradehero.th.api.discussion.DiscussionType;
+import com.tradehero.th.models.push.urbanairship.PushConstants;
+import com.tradehero.th.models.push.urbanairship.handlers.NotificationOpenedHandler;
 import com.tradehero.th.utils.DaggerUtils;
 import java.util.List;
 import javax.inject.Inject;
@@ -18,6 +22,10 @@ import timber.log.Timber;
 public class BaiduPushMessageReceiver extends FrontiaPushMessageReceiver
 {
     public static final String TAG = BaiduPushMessageReceiver.class.getSimpleName();
+    public static final String ACTION_NOTIFICATION_CLICKED = "com.tradehero.th.ACTION_NOTIFICATION_CLICKED";
+    public static final String KEY_NOTIFICATION_ID = "com.tradehero.th.NOTIFICATION_ID";
+    public static final String KEY_NOTIFICATION_CONTENT = "com.tradehero.th.NOTIFICATION_CONTENT";
+
     public static final int CODE_OK = 0;
     public static final int MESSAGE_ID = 100;
 
@@ -27,6 +35,8 @@ public class BaiduPushMessageReceiver extends FrontiaPushMessageReceiver
     {
         DaggerUtils.inject(this);
     }
+
+
 
     /**
      * @return none
@@ -51,7 +61,7 @@ public class BaiduPushMessageReceiver extends FrontiaPushMessageReceiver
         return errorCode == CODE_OK;
     }
 
-    private void showNotification(Context context, String message)
+    private void showNotification(Context context, PushMessageDTO pushMessageDTO)
     {
         com.baidu.android.pushservice.CustomPushNotificationBuilder
                 cBuilder = new com.baidu.android.pushservice.CustomPushNotificationBuilder(
@@ -68,18 +78,44 @@ public class BaiduPushMessageReceiver extends FrontiaPushMessageReceiver
         cBuilder.setLayoutDrawable(R.drawable.notification_logo);
         //cBuilder.setNotificationTitle(context.getApplicationInfo().name);
         cBuilder.setNotificationTitle(context.getString(R.string.app_name));
-        cBuilder.setNotificationText(message);
+        cBuilder.setNotificationText(pushMessageDTO.description);
         Notification notification = cBuilder.construct(context);
+
+        notification.contentIntent = PendingIntent.getBroadcast(context, 0,composeIntent(pushMessageDTO), 0);
 
         NotificationManager nm = (NotificationManager) context
                 .getSystemService(Context.NOTIFICATION_SERVICE);
-        int hashCode = message.hashCode();
+        int hashCode = pushMessageDTO.description.hashCode();
         if (hashCode < 0)
         {
             hashCode = -hashCode;
         }
         int msgId = hashCode % 1000;
         nm.notify(msgId, notification);
+    }
+
+    public static Intent composeIntent(PushMessageDTO pushMessageDTO)
+    {
+        Intent intent = new Intent(ACTION_NOTIFICATION_CLICKED);
+        intent.putExtra(KEY_NOTIFICATION_ID, pushMessageDTO.id);
+        intent.putExtra(KEY_NOTIFICATION_CONTENT, pushMessageDTO.description);
+        return intent;
+    }
+
+
+    public static Intent handle(Intent intent)
+    {
+        String action = intent.getAction();
+        int id = intent.getIntExtra(KEY_NOTIFICATION_ID,-1);
+        String description = intent.getStringExtra(KEY_NOTIFICATION_CONTENT);
+        Timber.d("action %s, id:%s, description:%s",action,id,description);
+
+        //Intent fakeIntent = new Intent();
+        //fakeIntent.putExtra(PushConstants.PUSH_ID_KEY, String.valueOf(id));
+        //NotificationOpenedHandler notificationOpenedHandler = new NotificationOpenedHandler();
+        //notificationOpenedHandler.handle(fakeIntent);
+
+        return intent;
     }
 
     private void handleRecevieMessage(Context context, String message)
@@ -91,7 +127,7 @@ public class BaiduPushMessageReceiver extends FrontiaPushMessageReceiver
             {
                 PushMessageHandler.notifyMessageReceived(context);
             }
-            showNotification(context, pushMessageDTO.description);
+            showNotification(context, pushMessageDTO);
         }
 
     }
