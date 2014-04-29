bdf110f47628e7f4d103c0fc6983f45688f81dda
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-04-29 15:34:56 +0800

    Added a stub key in DiscussionDTO and form. [#70239726]

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/discussion/DiscussionDTOFactory.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/discussion/DiscussionDTOFactory.java
@@ -3,19 +3,16 @@ package com.tradehero.th.api.discussion;
 import com.tradehero.th.api.discussion.form.DiscussionFormDTO;
 import com.tradehero.th.api.users.CurrentUserId;
 import java.util.Date;
-import java.util.Random;
 import javax.inject.Inject;
 
 public class DiscussionDTOFactory
 {
     private final CurrentUserId currentUserId;
-    private final Random random;
 
     @Inject public DiscussionDTOFactory(CurrentUserId currentUserId)
     {
         super();
         this.currentUserId = currentUserId;
-        this.random = new Random(new Date().getTime());
     }
 
     public DiscussionDTO createChildClass(DiscussionDTO unidentified)
@@ -79,24 +76,33 @@ public class DiscussionDTOFactory
         DiscussionDTO created = null;
         if (fromForm != null)
         {
-            created = createEmptyChild(fromForm.getInReplyToType());
-            created.id = - random.nextInt(Integer.MAX_VALUE);
-            created.inReplyToType = DiscussionType.PRIVATE_MESSAGE;
-            created.type = DiscussionType.PRIVATE_MESSAGE;
-            created.inReplyToId = fromForm.inReplyToId;
-            created.text = fromForm.text;
-            created.langCode = fromForm.langCode;
-            created.userId = currentUserId.toUserBaseKey().key;
-            created.createdAtUtc = new Date();
-            created.url = fromForm.url;
-            created.geo_alt = fromForm.geo_alt;
-            created.geo_lat = fromForm.geo_lat;
-            created.geo_long = fromForm.geo_long;
-            created.publishToFb = fromForm.publishToFb;
-            created.publishToLi = fromForm.publishToLi;
-            created.publishToTw = fromForm.publishToTw;
-            created.isPublic = fromForm.isPublic;
+            created = createStubStrict(fromForm);
         }
+        return created;
+    }
+
+    private DiscussionDTO createStubStrict(DiscussionFormDTO fromForm)
+    {
+        DiscussionDTO created = createEmptyChild(fromForm.getInReplyToType());
+        if (fromForm.stubKey != null)
+        {
+            created.id = fromForm.stubKey.id;
+        }
+        created.inReplyToType = DiscussionType.PRIVATE_MESSAGE;
+        created.type = DiscussionType.PRIVATE_MESSAGE;
+        created.inReplyToId = fromForm.inReplyToId;
+        created.text = fromForm.text;
+        created.langCode = fromForm.langCode;
+        created.userId = currentUserId.toUserBaseKey().key;
+        created.createdAtUtc = new Date();
+        created.url = fromForm.url;
+        created.geo_alt = fromForm.geo_alt;
+        created.geo_lat = fromForm.geo_lat;
+        created.geo_long = fromForm.geo_long;
+        created.publishToFb = fromForm.publishToFb;
+        created.publishToLi = fromForm.publishToLi;
+        created.publishToTw = fromForm.publishToTw;
+        created.isPublic = fromForm.isPublic;
         return created;
     }
 }
--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/discussion/form/DiscussionFormDTO.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/discussion/form/DiscussionFormDTO.java
@@ -2,10 +2,17 @@ package com.tradehero.th.api.discussion.form;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tradehero.th.api.discussion.DiscussionType;
+import com.tradehero.th.api.discussion.key.DiscussionKey;
 import com.tradehero.th.api.timeline.form.PublishableFormDTO;
 
 abstract public class DiscussionFormDTO extends PublishableFormDTO
 {
+    /**
+     * This stub discussion key is used to simulate an immediate post,
+     * and also to keep track of the query. Leaving it null is fine.
+     */
+    public DiscussionKey stubKey;
+
     public String text;
     public String langCode;
     public int inReplyToId;
