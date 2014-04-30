f69a9cf5bf1b7d23f7994e7b206bba8a3e7c4ae2
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-04-30 20:15:21 +0800

    Objectified StaffDTO

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/staff/StaffDTOFactory.java
@@ -0,0 +1,33 @@
+package com.tradehero.th.models.staff;
+
+import android.content.res.Resources;
+import com.tradehero.th.R;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+
+public class StaffDTOFactory
+{
+    public static final String SEPARATOR_NAME_TITLE = "\\|";
+
+    @Inject public StaffDTOFactory()
+    {
+        super();
+    }
+
+    public List<StaffDTO> getTradeHeroStaffers(Resources resources)
+    {
+        String[] staffs = resources.getStringArray(R.array.staffs);
+        List<StaffDTO> staffDTOs = new ArrayList<>(staffs.length);
+        for (String staff: staffs)
+        {
+            String[] staffInfo = staff.split(SEPARATOR_NAME_TITLE);
+            if (staffInfo.length < 2)
+            {
+                continue;
+            }
+            staffDTOs.add(new StaffDTO(staffInfo[0], staffInfo[1]));
+        }
+        return staffDTOs;
+    }
+}
--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/translation/ForTranslation.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/translation/ForTranslation.java
@@ -4,9 +4,6 @@ import javax.inject.Qualifier;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-/**
- * Created with IntelliJ IDEA. User: tho Date: 2/7/14 Time: 4:16 PM Copyright (c) TradeHero
- */
 @Qualifier @Retention(RetentionPolicy.RUNTIME)
 public @interface ForTranslation
 {
