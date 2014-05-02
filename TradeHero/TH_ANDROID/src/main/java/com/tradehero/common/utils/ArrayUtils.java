6b1d5a2708e2854f4732032a3e795e3329d5b639
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-05-02 13:48:05 +0800

    Cleanup

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/utils/ArrayUtils.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/utils/ArrayUtils.java
@@ -5,11 +5,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-/** Created with IntelliJ IDEA. User: xavier Date: 11/6/13 Time: 4:47 PM To change this template use File | Settings | File Templates. */
 public class ArrayUtils
 {
-    public static final String TAG = ArrayUtils.class.getSimpleName();
-
     public static <T> List<T> filter(Collection<T> target, Predicate<T> predicate)
     {
         List<T> result = new ArrayList<T>();
