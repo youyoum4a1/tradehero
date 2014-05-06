644bd477e9e06e2799ed93cf89daa1176f3359b1
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-05-06 18:49:25 +0800

    Added convenience graphic transformations. [#70249838]

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/graphics/RotateTransformation.java
@@ -0,0 +1,34 @@
+package com.tradehero.common.graphics;
+
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import com.squareup.picasso.Transformation;
+
+public class RotateTransformation implements Transformation
+{
+    private final int rotationDegree;
+
+    public RotateTransformation(int rotationDegree)
+    {
+        super();
+        this.rotationDegree = rotationDegree;
+    }
+
+    @Override public Bitmap transform(Bitmap source)
+    {
+        int width = source.getWidth();
+        int height = source.getHeight();
+
+        Matrix matrix = new Matrix();
+        matrix.preRotate(rotationDegree);
+
+        Bitmap dstBmp = Bitmap.createBitmap(source, 0, 0, width, height, matrix, false);
+        source.recycle();
+        return dstBmp;
+    }
+
+    @Override public String key()
+    {
+        return String.format("rotate-%d", rotationDegree);
+    }
+}
