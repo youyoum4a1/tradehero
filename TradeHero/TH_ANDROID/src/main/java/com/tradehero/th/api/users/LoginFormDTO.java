f84a2613585f3d3001b308e4bd7cecb634b003a3
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-05-06 17:38:18 +0800

    Use deviceType instead of Int

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/users/LoginFormDTO.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/users/LoginFormDTO.java
@@ -2,31 +2,19 @@ package com.tradehero.th.api.users;
 
 import com.tradehero.th.api.misc.DeviceType;
 
-
 public class LoginFormDTO
 {
-    public static final String TAG = LoginFormDTO.class.getSimpleName();
+    public final String deviceToken;
 
-    public String deviceToken;
-
-    @Deprecated
-    public boolean clientiOS;
-
-    public int clientType;
+    public final DeviceType clientType;
 
     // min. version support
-    public String clientVersion;
-
-    public LoginFormDTO(String deviceToken, boolean clientiOS, String clientVersion)
-    {
-        this.deviceToken = deviceToken;
-        this.clientiOS = clientiOS;
-        this.clientVersion = clientVersion;
-    }
+    public final String clientVersion;
 
     public LoginFormDTO(String deviceToken, DeviceType deviceType, String clientVersion)
     {
-        this(deviceToken, false, clientVersion);
-        this.clientType = deviceType.getServerValue();
+        this.deviceToken = deviceToken;
+        this.clientVersion = clientVersion;
+        this.clientType = deviceType;
     }
 }
