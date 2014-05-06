095834e8bac8affc7a3bb0d97df2ed73a914521b
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-05-06 11:40:05 +0800

    Cleanup deviceUtil

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/DeviceUtil.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/DeviceUtil.java
@@ -1,26 +1,3 @@
-/*
- ===========================================================================
- Copyright (c) 2012 Three Pillar Global Inc. http://threepillarglobal.com
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ===========================================================================
- */
 package com.tradehero.th.utils;
 
 import android.app.Activity;
@@ -28,8 +5,6 @@ import android.content.Context;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import java.lang.ref.WeakReference;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public final class DeviceUtil
 {
