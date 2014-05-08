10b7e0db5fd6fe292e54801eb1e8f016fde49dda
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-05-08 14:42:31 +0800

    Added factory for BitmapTypedOutput. #70249838

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/graphics/BitmapTypedOutputFactory.java
@@ -0,0 +1,35 @@
+package com.tradehero.th.models.graphics;
+
+import android.content.res.Resources;
+import com.tradehero.th.R;
+import com.tradehero.th.api.form.UserFormDTO;
+import com.tradehero.th.utils.BitmapForProfileFactory;
+import javax.inject.Inject;
+
+public class BitmapTypedOutputFactory
+{
+    @Inject public BitmapTypedOutputFactory()
+    {
+        super();
+    }
+
+    public BitmapTypedOutput createForProfilePhoto(
+            Resources resources,
+            BitmapForProfileFactory bitmapForProfileFactory,
+            UserFormDTO userFormDTO)
+    {
+        return createForProfilePhoto(resources, bitmapForProfileFactory, userFormDTO.profilePicturePath);
+    }
+
+    public BitmapTypedOutput createForProfilePhoto(
+            Resources resources,
+            BitmapForProfileFactory bitmapForProfileFactory,
+            String profilePicturePath)
+    {
+        return new BitmapTypedOutput(
+                BitmapTypedOutput.TYPE_JPEG,
+                bitmapForProfileFactory.decodeBitmapForProfile(resources, profilePicturePath),
+                profilePicturePath,
+                resources.getInteger(R.integer.user_profile_photo_compress_quality));
+    }
+}
