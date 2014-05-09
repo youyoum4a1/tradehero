81b573f8873fdafd86c413a157870a28a18383e2
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-05-09 15:00:06 +0800

    Adding more info when reporting to crashlytics.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/log/CrashReportingTree.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/log/CrashReportingTree.java
@@ -6,8 +6,18 @@ import timber.log.Timber;
 
 public class CrashReportingTree extends Timber.HollowTree
 {
-    @Override public void e(Throwable t, String message, Object... args)
+    @Override public void e(Throwable cause, String message, Object... args)
     {
-        Crashlytics.logException(t);
+        Crashlytics.logException(new Exception(
+                    getConcatMessage(cause, message, args),
+                    cause));
+    }
+
+    public String getConcatMessage(Throwable cause, String message, Object... args)
+    {
+        return String.format(
+                "Message: %s\nCause: %s",
+                String.format(message, args),
+                cause.getMessage());
     }
 }
