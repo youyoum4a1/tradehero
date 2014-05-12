5c58d4fcb844b86b5a5991468017ea1e6cf61b2e
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-05-12 16:49:24 +0800

    Navigator is now able to provide current active fragment

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discussion/MentionActionButtonsView.java
@@ -0,0 +1,27 @@
+package com.tradehero.th.fragments.discussion;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class MentionActionButtonsView extends LinearLayout
+{
+    //<editor-fold desc="Constructors">
+    public MentionActionButtonsView(Context context)
+    {
+        super(context);
+    }
+
+    public MentionActionButtonsView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public MentionActionButtonsView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+    }
+    //</editor-fold>
+
+
+}
