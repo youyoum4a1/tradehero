eb79c563a38fb279d2c21d8e2c667cb2fdba21c1
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-05-19 18:55:29 +0800

    Introduced convenience class.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/timeline/TimelineItemDTO.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/timeline/TimelineItemDTO.java
@@ -3,19 +3,18 @@ package com.tradehero.th.api.timeline;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.tradehero.th.api.discussion.AbstractDiscussionDTO;
 import com.tradehero.th.api.security.SecurityId;
+import com.tradehero.th.api.security.SecurityIdList;
 import com.tradehero.th.api.security.SecurityMediaDTO;
+import com.tradehero.th.api.security.SecurityMediaDTOList;
 import com.tradehero.th.api.timeline.key.TimelineItemDTOKey;
 import com.tradehero.th.api.users.UserProfileCompactDTO;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 
 public class TimelineItemDTO extends AbstractDiscussionDTO
 {
     public int type;
     public Date userViewedAtUtc;
-    private List<SecurityMediaDTO> medias;
+    private SecurityMediaDTOList medias;
     public Integer pushTypeId;
 
     public boolean useSysIcon;
@@ -24,66 +23,43 @@ public class TimelineItemDTO extends AbstractDiscussionDTO
 
     private UserProfileCompactDTO user;
 
+    //<editor-fold desc="Constructors">
     public TimelineItemDTO()
     {
     }
+    //</editor-fold>
 
-    public List<SecurityMediaDTO> getMedias()
-    {
-        return Collections.unmodifiableList(medias);
-    }
-
-    public void setMedias(List<SecurityMediaDTO> medias)
+    public void setMedias(SecurityMediaDTOList medias)
     {
         this.medias = medias;
     }
 
-    public List<SecurityId> getMediaSecurityIds()
+    public SecurityIdList getMediaSecurityIds()
     {
         if (medias == null)
         {
             return null;
         }
-        List<SecurityId> securityIds = new ArrayList<>();
-        for (SecurityMediaDTO securityMediaDTO : medias)
-        {
-            if (securityMediaDTO != null &&
-                    securityMediaDTO.hasValidSecurityId())
-            {
-                securityIds.add(securityMediaDTO.createSecurityId());
-            }
-        }
-        return securityIds;
+        return medias.getMediaSecurityIds();
     }
 
     @JsonIgnore
     public SecurityMediaDTO getFlavorSecurityForDisplay()
     {
-        SecurityMediaDTO securityMediaDTO = null;
-        for (SecurityMediaDTO m: medias)
+        if (medias == null)
         {
-            if (m.securityId != 0)
-            {
-                securityMediaDTO = m;
-            }
-
-            // we prefer the first security with photo
-            if (securityMediaDTO != null && securityMediaDTO.url != null)
-            {
-                return securityMediaDTO;
-            }
+            return null;
         }
-        return securityMediaDTO;
+        return medias.getFlavorSecurityForDisplay();
     }
 
     public SecurityId createFlavorSecurityIdForDisplay()
     {
-        SecurityMediaDTO securityMediaDTO = getFlavorSecurityForDisplay();
-        if (securityMediaDTO == null)
+        if (medias == null)
         {
             return null;
         }
-        return securityMediaDTO.createSecurityId();
+        return medias.createFlavorSecurityIdForDisplay();
     }
 
     public void setUser(UserProfileCompactDTO user)
