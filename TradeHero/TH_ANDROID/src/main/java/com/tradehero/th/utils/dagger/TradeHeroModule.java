8cb3345b18de023372f5cad2e88a2de6a043c936
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-05-20 19:11:06 +0800

    Prepared way to search among competition securities. Added specific eToro resources. #71638502

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/dagger/TradeHeroModule.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/dagger/TradeHeroModule.java
@@ -68,6 +68,7 @@ import com.tradehero.th.fragments.security.ChartFragment;
 import com.tradehero.th.fragments.security.SecurityItemView;
 import com.tradehero.th.fragments.security.SecurityItemViewAdapter;
 import com.tradehero.th.fragments.security.SecuritySearchFragment;
+import com.tradehero.th.fragments.security.SecuritySearchProviderFragment;
 import com.tradehero.th.fragments.security.SecuritySearchWatchlistFragment;
 import com.tradehero.th.fragments.security.StockInfoFragment;
 import com.tradehero.th.fragments.security.StockInfoValueFragment;
@@ -190,6 +191,7 @@ import javax.inject.Singleton;
                         SecurityListPagedLoader.class,
                         SecuritySearchFragment.class,
                         SecuritySearchWatchlistFragment.class,
+                        SecuritySearchProviderFragment.class,
                         MacquarieSecurityListPagedLoader.class,
                         SecurityItemViewAdapter.class,
                         MacquarieWarrantItemViewAdapter.class,
