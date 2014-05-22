d94158117e06d6e59028292a512d0dc180227bcc
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-05-22 19:20:27 +0800

    Attempt better reporting of null values in cache.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/persistence/PartialDTOCache.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/persistence/PartialDTOCache.java
@@ -111,7 +111,7 @@ abstract public class PartialDTOCache<DTOKeyType extends DTOKey, DTOType extends
                         {
                             if (value == null)
                             {
-                                Timber.e(new Exception(String.format("Null value returned for key %s, on cache %s", key, getClass())), null);
+                                Timber.e(new Exception(String.format("Null value returned for key %s, on cache %s", key, getCacheClass())), null);
                             }
                             currentListener.onDTOReceived(key, value, !forceUpdateCache);
                         }
@@ -247,5 +247,10 @@ abstract public class PartialDTOCache<DTOKeyType extends DTOKey, DTOType extends
         {
             autoFetch(key, force);
         }
+    }
+
+    private Class<?> getCacheClass()
+    {
+        return getClass();
     }
 }
