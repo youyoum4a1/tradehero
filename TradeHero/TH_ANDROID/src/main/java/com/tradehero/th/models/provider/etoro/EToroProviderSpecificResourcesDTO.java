a356f7949bd4c1787d32001a8e5c2f8e75f10b53
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-05-22 17:51:58 +0800

    Added eToro hard coded name. #71638502

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/provider/etoro/EToroProviderSpecificResourcesDTO.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/provider/etoro/EToroProviderSpecificResourcesDTO.java
@@ -14,7 +14,7 @@ public class EToroProviderSpecificResourcesDTO extends ProviderSpecificResources
         //helpVideoLinkBackgroundResId = R.drawable.btn_provider_3_macquarie;
         //helpVideoLinkTextColourResId = R.color.black;
         //timedHeaderLeaderboardTitleResId = R.string.competition_macquarie_warrant_hero_main_title;
-        //competitionPortfolioTitleResId = R.string.competition_macquarie_warrant_hero_main_title;
+        competitionPortfolioTitleResId = R.string.competition_e_toro_portfolio_title;
         //securityListFragmentTitleResId = R.string.competition_macquarie_warrant_hero_security_list_title;
     }
 }
