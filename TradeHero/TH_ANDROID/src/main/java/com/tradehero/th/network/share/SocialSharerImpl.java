df8e32c723b225c2ac672071afa1b0748ee0ae0f
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-05-23 17:29:59 +0800

    Make sure the context of social sharer is an activity one. #70146876

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/share/SocialSharerImpl.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/share/SocialSharerImpl.java
@@ -1,7 +1,7 @@
 package com.tradehero.th.network.share;
 
-import android.content.Context;
 import android.content.Intent;
+import com.tradehero.th.activities.CurrentActivityHolder;
 import com.tradehero.th.api.share.SocialShareFormDTO;
 import com.tradehero.th.wxapi.WXEntryActivity;
 import com.tradehero.th.api.share.wechat.WeChatDTO;
@@ -9,11 +9,11 @@ import javax.inject.Inject;
 
 public class SocialSharerImpl implements SocialSharer
 {
-    private final Context context;
+    private final CurrentActivityHolder currentActivityHolder;
 
-    @Inject public SocialSharerImpl(Context context)
+    @Inject public SocialSharerImpl(CurrentActivityHolder currentActivityHolder)
     {
-        this.context = context;
+        this.currentActivityHolder = currentActivityHolder;
     }
 
     @Override public void share(SocialShareFormDTO shareFormDTO)
@@ -30,12 +30,12 @@ public class SocialSharerImpl implements SocialSharer
 
     public void share(WeChatDTO weChatDTO)
     {
-        context.startActivity(createWeChatIntent(weChatDTO));
+        currentActivityHolder.getCurrentActivity().startActivity(createWeChatIntent(weChatDTO));
     }
 
     public Intent createWeChatIntent(WeChatDTO weChatDTO)
     {
-        Intent intent = new Intent(context, WXEntryActivity.class);
+        Intent intent = new Intent(currentActivityHolder.getCurrentContext(), WXEntryActivity.class);
         intent.putExtra(WXEntryActivity.WECHAT_MESSAGE_TYPE_KEY, weChatDTO.type);
         intent.putExtra(WXEntryActivity.WECHAT_MESSAGE_ID_KEY, weChatDTO.id);
         if (weChatDTO.title != null)
