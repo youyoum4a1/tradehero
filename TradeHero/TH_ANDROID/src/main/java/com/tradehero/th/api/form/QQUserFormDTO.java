12825d863f1282c89c80dbd895c69e1793e86ac5
haiping.hu <windy911@163.com>
2014-05-27 15:13:24 +0800

    [Social Network]QQ_login [#72006842]

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/form/QQUserFormDTO.java
@@ -0,0 +1,8 @@
+package com.tradehero.th.api.form;
+
+public class QQUserFormDTO extends UserFormDTO {
+
+   public String qq_openid;
+   public String qq_access_token;
+
+}
---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/QQUtils.java
@@ -0,0 +1,34 @@
+package com.tradehero.th.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import com.tradehero.th.auth.tencent_qq.QQAuthenticationProvider;
+import com.tradehero.th.base.THUser;
+import com.tradehero.th.misc.callback.LogInCallback;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class QQUtils {
+
+    private final QQAuthenticationProvider provider;
+
+    @Inject
+    public QQUtils(QQAuthenticationProvider provider)
+    {
+        this.provider = provider;
+        THUser.registerAuthenticationProvider(provider);
+    }
+
+    public void logIn(Context context, LogInCallback callback)
+    {
+        provider.with(context);
+        THUser.logInWithAsync(provider.getAuthType(), callback);
+    }
+
+    public void authorizeCallBack(int requestCode, int resultCode, Intent data){
+        provider.authorizeCallBack(requestCode,resultCode,data);
+    }
+
+}
