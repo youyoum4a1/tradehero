b0a77e0a2865d3f27da8ec51e26ada20d0827286
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-05-28 15:03:31 +0800

    Put Bundle handling of WeChatDTO. Formalised calling of WxEntryActivity. #72156240

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/share/SocialSharerImpl.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/share/SocialSharerImpl.java
@@ -164,16 +164,7 @@ public class SocialSharerImpl implements SocialSharer
     public Intent createWeChatIntent(WeChatDTO weChatDTO)
     {
         Intent intent = new Intent(currentActivityHolder.getCurrentContext(), WXEntryActivity.class);
-        intent.putExtra(WXEntryActivity.WECHAT_MESSAGE_TYPE_KEY, weChatDTO.type.getValue());
-        intent.putExtra(WXEntryActivity.WECHAT_MESSAGE_ID_KEY, weChatDTO.id);
-        if (weChatDTO.title != null)
-        {
-            intent.putExtra(WXEntryActivity.WECHAT_MESSAGE_TITLE_KEY, weChatDTO.title);
-        }
-        if (weChatDTO.imageURL != null && !weChatDTO.imageURL.isEmpty())
-        {
-            intent.putExtra(WXEntryActivity.WECHAT_MESSAGE_IMAGE_URL_KEY, weChatDTO.imageURL);
-        }
+        WXEntryActivity.putWeChatDTO(intent, weChatDTO);
         return intent;
     }
 
