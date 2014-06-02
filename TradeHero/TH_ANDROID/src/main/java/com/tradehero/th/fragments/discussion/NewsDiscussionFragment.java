5c9ef95d75a7488883e5587e7f4c659fdfec5eb8
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-06-02 22:36:58 +0800

    Cleanup

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discussion/NewsDiscussionFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discussion/NewsDiscussionFragment.java
@@ -5,26 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import butterknife.InjectView;
-import butterknife.OnClick;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
-import com.tradehero.common.persistence.DTOCache;
-import com.tradehero.common.utils.THToast;
 import com.tradehero.th.R;
 import com.tradehero.th.api.discussion.key.DiscussionKey;
-import com.tradehero.th.api.news.NewsItemDTO;
 import com.tradehero.th.api.news.key.NewsItemDTOKey;
-import com.tradehero.th.api.share.SocialShareFormDTO;
-import com.tradehero.th.api.translation.TranslationResult;
 import com.tradehero.th.fragments.news.NewsViewLinear;
-import com.tradehero.th.fragments.settings.SettingsFragment;
-import com.tradehero.th.misc.exception.THException;
-import com.tradehero.th.models.share.SocialShareTranslationHelper;
-import com.tradehero.th.persistence.news.NewsItemCache;
-import com.tradehero.th.persistence.translation.TranslationCache;
-import com.tradehero.th.persistence.translation.TranslationKey;
-import javax.inject.Inject;
 import timber.log.Timber;
 
 public class NewsDiscussionFragment extends AbstractDiscussionFragment
@@ -37,40 +24,15 @@ public class NewsDiscussionFragment extends AbstractDiscussionFragment
 
     private static final String BUNDLE_KEY_IS_RETURNING = NewsDiscussionFragment.class.getName() + ".isReturning";
 
-    private NewsItemDTO mDetailNewsItemDTO;
-
-    @Inject NewsItemCache newsItemCache;
-    @Inject TranslationCache translationCache;
-    @Inject SocialShareTranslationHelper socialShareTranslationHelper;
-
     @InjectView(R.id.discussion_view) NewsDiscussionView newsDiscussionView;
     @InjectView(R.id.news_view_linear) NewsViewLinear newsView;
 
-    @OnClick({
-            R.id.news_start_new_discussion,
-            R.id.discussion_action_button_comment_count,
-    })
-    void onStartNewDiscussion()
-    {
-        Bundle bundle = new Bundle();
-        if (newsItemDTOKey != null)
-        {
-            bundle.putBundle(DiscussionKey.BUNDLE_KEY_DISCUSSION_KEY_BUNDLE,
-                    newsItemDTOKey.getArgs());
-        }
-        getNavigator().pushFragment(DiscussionEditPostFragment.class, bundle);
-    }
-
     private NewsItemDTOKey newsItemDTOKey;
-
-    private DTOCache.GetOrFetchTask<NewsItemDTOKey, NewsItemDTO> newsFetchTask;
-    private DTOCache.GetOrFetchTask<TranslationKey, TranslationResult> translationTask;
 
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState)
     {
-        View view = inflater.inflate(R.layout.fragment_news_discussion, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_news_discussion, container, false);
     }
 
     @Override public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
@@ -78,7 +40,6 @@ public class NewsDiscussionFragment extends AbstractDiscussionFragment
         ActionBar actionBar = getSherlockActivity().getSupportActionBar();
         Bundle bundle = getArguments();
         String title = bundle.getString(NewsDiscussionFragment.BUNDLE_KEY_SECURITY_SYMBOL);
-        //bundle.putString(NewsDiscussionFragment.BUNDLE_KEY_SECURITY_SYMBOL, securityId.securitySymbol);
         actionBar.setTitle(title);
         Timber.d("onCreateOptionsMenu");
         super.onCreateOptionsMenu(menu, inflater);
@@ -104,8 +65,6 @@ public class NewsDiscussionFragment extends AbstractDiscussionFragment
 
     @Override public void onDestroyView()
     {
-        detachNewsFetchTask();
-        detachTranslationTask();
         Bundle args = getArguments();
         if (args != null)
         {
@@ -135,11 +94,6 @@ public class NewsDiscussionFragment extends AbstractDiscussionFragment
 
         if (newsItemDTOKey != null)
         {
-            NewsItemDTO cachedNews = newsItemCache.get(newsItemDTOKey);
-
-            //linkWith(cachedNews, andDisplay);
-
-            //fetchNewsDetail(true);
             newsView.display(newsItemDTOKey);
             setRandomBackground();
         }
@@ -164,71 +118,8 @@ public class NewsDiscussionFragment extends AbstractDiscussionFragment
         // TODO
     }
 
-    // TODO remove
-    private void fetchNewsDetail(boolean force)
-    {
-        detachNewsFetchTask();
-        newsFetchTask = newsItemCache.getOrFetch(newsItemDTOKey, force, createNewsFetchListener());
-        newsFetchTask.execute();
-    }
-
-    private void detachNewsFetchTask()
-    {
-        if (newsFetchTask != null)
-        {
-            newsFetchTask.setListener(null);
-        }
-        newsFetchTask = null;
-    }
-
-    private void detachTranslationTask()
-    {
-        if (translationTask != null)
-        {
-            translationTask.setListener(null);
-        }
-        translationTask = null;
-    }
-
-    private void linkWith(NewsItemDTO newsItemDTO, boolean andDisplay)
-    {
-        mDetailNewsItemDTO = newsItemDTO;
-
-        if (andDisplay)
-        {
-            newsView.linkWith(mDetailNewsItemDTO, true);
-        }
-    }
-
-    protected void pushSettingsForConnect(SocialShareFormDTO socialShareFormDTO)
-    {
-        Bundle args = new Bundle();
-        SettingsFragment.putSocialNetworkToConnect(args, socialShareFormDTO);
-        getDashboardNavigator().pushFragment(SettingsFragment.class, args);
-    }
-
-    @Override
-    public boolean isTabBarVisible()
+    @Override public boolean isTabBarVisible()
     {
         return false;
-    }
-
-    private DTOCache.Listener<NewsItemDTOKey, NewsItemDTO> createNewsFetchListener()
-    {
-        return new NewsFetchListener();
-    }
-
-    private class NewsFetchListener implements DTOCache.Listener<NewsItemDTOKey, NewsItemDTO>
-    {
-        @Override
-        public void onDTOReceived(NewsItemDTOKey key, NewsItemDTO value, boolean fromCache)
-        {
-            linkWith(value, true);
-        }
-
-        @Override public void onErrorThrown(NewsItemDTOKey key, Throwable error)
-        {
-            THToast.show(new THException(error));
-        }
     }
 }
