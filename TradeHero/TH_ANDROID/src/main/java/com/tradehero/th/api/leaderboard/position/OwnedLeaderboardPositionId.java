c41b28d0580d80bcad24a6fd2d015ae400003701
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-06-04 14:59:13 +0800

    Added an interface to describe a position key and getPositions key.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/leaderboard/position/OwnedLeaderboardPositionId.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/leaderboard/position/OwnedLeaderboardPositionId.java
@@ -2,9 +2,9 @@ package com.tradehero.th.api.leaderboard.position;
 
 import android.os.Bundle;
 import com.tradehero.common.persistence.DTO;
-import com.tradehero.common.persistence.DTOKey;
+import com.tradehero.th.api.position.PositionDTOKey;
 
-public class OwnedLeaderboardPositionId implements Comparable, DTOKey, DTO
+public class OwnedLeaderboardPositionId implements Comparable, PositionDTOKey, DTO
 {
     public final static String BUNDLE_KEY_LEADERBOARD_MARK_USER_ID = OwnedLeaderboardPositionId.class.getName() + ".leaderboardMarkUserId";
     public final static String BUNDLE_KEY_LEADERBOARD_MARK_USER_POSITION_ID = OwnedLeaderboardPositionId.class.getName() + ".leaderboardMarkUserPositionId";
--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/portfolio/OwnedPortfolioId.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/portfolio/OwnedPortfolioId.java
@@ -2,11 +2,11 @@ package com.tradehero.th.api.portfolio;
 
 import android.os.Bundle;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.tradehero.common.persistence.DTOKey;
+import com.tradehero.th.api.position.GetPositionsDTOKey;
 import com.tradehero.th.api.users.UserBaseDTO;
 import com.tradehero.th.api.users.UserBaseKey;
 
-public class OwnedPortfolioId  implements Comparable, DTOKey
+public class OwnedPortfolioId  implements Comparable, GetPositionsDTOKey
 {
     public final static String BUNDLE_KEY_USER_ID = OwnedPortfolioId.class.getName() + ".userId";
     public final static String BUNDLE_KEY_PORTFOLIO_ID = OwnedPortfolioId.class.getName() + ".portfolioId";
