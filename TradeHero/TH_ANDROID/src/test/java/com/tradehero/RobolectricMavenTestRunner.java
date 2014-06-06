fca3f1a6523ec814d8bef4f4d01893ac518e1352
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-06-06 15:53:56 +0800

    Trick to aggregate resources from library and the main project (for testing)

--- TradeHero/TH_ANDROID/src/test/java/com/tradehero/RobolectricMavenTestRunner.java
+++ TradeHero/TH_ANDROID/src/test/java/com/tradehero/RobolectricMavenTestRunner.java
@@ -1,11 +1,17 @@
 package com.tradehero;
 
 import com.tradehero.th.base.TestApplication;
+import java.io.File;
+import java.util.List;
 import org.junit.runners.model.InitializationError;
 import org.robolectric.AndroidManifest;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.res.Fs;
+import org.robolectric.res.FsFile;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 
 public class RobolectricMavenTestRunner extends RobolectricTestRunner
 {
@@ -18,25 +24,61 @@ public class RobolectricMavenTestRunner extends RobolectricTestRunner
     {
         if (config.manifest().equals(Config.DEFAULT))
         {
-            return new AndroidManifest(
-                    Fs.fileFromPath(TestConstants.MANIFEST_PATH),
-                    Fs.fileFromPath(TestConstants.RES_PATH),
-                    Fs.fileFromPath(TestConstants.ASSETS_PATH)) {
-                @Override public String getApplicationName()
-                {
-                    return TestApplication.class.getName();
-                }
-
-                /**
-                 * Note that robolectric does not support sdk ver 19 at this time
-                 * @return minSdkTarget support
-                 */
-                @Override public int getTargetSdkVersion()
-                {
-                    return 16;
-                }
-            };
+            return new MavenAndroidManifest(Fs.newFile(new File(TestConstants.BASE_APP_FOLDER)));
         }
         return super.getAppManifest(config);
+    }
+
+    /**
+     * This class do a nice trick to search for generated libraries, for each library, it will pick up corresponding resource folder
+     * and add it into resource pool, they are to be used for testing with reobolectric
+     */
+    public static class MavenAndroidManifest extends AndroidManifest
+    {
+        public MavenAndroidManifest(FsFile baseDir)
+        {
+            super(baseDir);
+        }
+
+        @Override public String getApplicationName()
+        {
+            return TestApplication.class.getName();
+        }
+
+        /**
+         * Note that robolectric does not support sdk ver 19 at this time
+         * @return minSdkTarget support
+         */
+        @Override public int getTargetSdkVersion()
+        {
+            return 16;
+        }
+
+        @Override protected List<FsFile> findLibraries()
+        {
+            // Try unpack folder from Maven/IntelliJ
+            FsFile unpack = getBaseDir().join(TestConstants.LIBRARIES_GENERATED_FOLDER);
+            if (unpack.exists())
+            {
+                FsFile[] libs = unpack.listFiles(new FsFile.Filter()
+                {
+                    @Override public boolean accept(FsFile fsFile)
+                    {
+                        return (fsFile != null) && (fsFile.isDirectory());
+                    }
+                });
+
+                if (libs != null)
+                {
+                    return asList(libs);
+                }
+            }
+            return emptyList();
+        }
+
+        @Override protected AndroidManifest createLibraryAndroidManifest(FsFile libraryBaseDir)
+        {
+            return new MavenAndroidManifest(libraryBaseDir);
+        }
     }
 }
