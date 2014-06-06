3fc01ff00441c84f064bf40b64ff61fa46a16ae6
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-06-06 10:56:26 +0800

    Test module for fragments

--- TradeHero/TH_ANDROID/src/test/java/com/tradehero/th/fragments/FragmentTestModule.java
+++ TradeHero/TH_ANDROID/src/test/java/com/tradehero/th/fragments/FragmentTestModule.java
@@ -1,5 +1,15 @@
 package com.tradehero.th.fragments;
 
+import com.tradehero.th.fragments.security.SecurityTestModule;
+import dagger.Module;
+
+@Module(
+        includes = {
+                SecurityTestModule.class
+        },
+        complete = false,
+        library = true
+)
 public class FragmentTestModule
 {
 }
--- TradeHero/TH_ANDROID/src/test/java/com/tradehero/th/fragments/security/WatchlistEditFragmentTest.java
+++ TradeHero/TH_ANDROID/src/test/java/com/tradehero/th/fragments/security/WatchlistEditFragmentTest.java
@@ -1,5 +1,9 @@
 package com.tradehero.th.fragments.security;
 
+import com.tradehero.RobolectricMavenTestRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(RobolectricMavenTestRunner.class)
 public class WatchlistEditFragmentTest
 {
 }
