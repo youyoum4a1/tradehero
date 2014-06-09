08367c8cbd1626b63f793fac9ff089bb9ffb5e1b
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-06-09 14:21:14 +0800

    A test for DashboardNavigator

--- TradeHero/TH_ANDROID/src/test/java/com/tradehero/th/fragments/DashboardNavigatorTest.java
+++ TradeHero/TH_ANDROID/src/test/java/com/tradehero/th/fragments/DashboardNavigatorTest.java
@@ -1,5 +1,41 @@
 package com.tradehero.th.fragments;
 
+import android.support.v4.app.Fragment;
+import com.tradehero.RobolectricMavenTestRunner;
+import com.tradehero.th.activities.DashboardActivity;
+import org.fest.util.VisibleForTesting;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+@RunWith(RobolectricMavenTestRunner.class)
 public class DashboardNavigatorTest
 {
+    private DashboardNavigator dashboardNavigator;
+
+    @Before public void setUp()
+    {
+        DashboardActivity dashboardActivity = Robolectric.buildActivity(DashboardActivity.class).create().visible().get();
+        dashboardNavigator = dashboardActivity.getDashboardNavigator();
+    }
+
+    @Test public void shouldAbleToNavigateBetweenFragments()
+    {
+        dashboardNavigator.pushFragment(TestFragment1.class);
+        assertThat(dashboardNavigator.getCurrentFragment()).isInstanceOf(TestFragment1.class);
+
+        dashboardNavigator.pushFragment(TestFragment2.class);
+        assertThat(dashboardNavigator.getCurrentFragment()).isInstanceOf(TestFragment2.class);
+
+        dashboardNavigator.popFragment();
+        assertThat(dashboardNavigator.getCurrentFragment()).isInstanceOf(TestFragment1.class);
+    }
+
+    @VisibleForTesting
+    public static class TestFragment1 extends Fragment {}
+    @VisibleForTesting
+    public static class TestFragment2 extends Fragment {}
 }
