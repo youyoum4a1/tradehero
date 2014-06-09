ccdefc74a817d3f7923e4749eaff93a2bd045054
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-06-09 12:50:28 +0800

    Fix bug with TabWidget number of child views
    This bug is found, thanks for unit test WatchlistEditFragmentTest

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/DashboardNavigator.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/DashboardNavigator.java
@@ -122,9 +122,9 @@ public class DashboardNavigator extends Navigator
         }
 
         // Hack to fix the issue with typing inside the competition webview
-        for (int i=0; i<DashboardTabType.usableValues().size(); ++i)
+        for (int i = 0; i < mTabHost.getTabWidget().getChildCount(); ++i)
         {
-            mTabHost.getTabWidget().getChildAt(i).setFocusable(false);
+            mTabHost.getTabWidget().getChildTabViewAt(i).setFocusable(false);
         }
 
         mTabHost.getTabWidget().setDividerDrawable(null);
