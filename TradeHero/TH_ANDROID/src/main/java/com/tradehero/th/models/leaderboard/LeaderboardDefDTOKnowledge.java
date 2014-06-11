12221eaad9610a061503669237f0f1d3772ada57
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-06-11 17:26:46 +0800

    Added possibility to show flag on exchange leaderboard.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/leaderboard/LeaderboardDefDTOKnowledge.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/leaderboard/LeaderboardDefDTOKnowledge.java
@@ -1,6 +1,7 @@
 package com.tradehero.th.models.leaderboard;
 
 import com.tradehero.th.api.leaderboard.def.LeaderboardDefDTO;
+import com.tradehero.th.api.market.Country;
 import com.tradehero.th.models.leaderboard.key.LeaderboardDefKeyKnowledge;
 import javax.inject.Inject;
 import org.jetbrains.annotations.NotNull;
@@ -19,7 +20,27 @@ public class LeaderboardDefDTOKnowledge
 
     public int getLeaderboardDefIcon(@NotNull LeaderboardDefDTO leaderboardDefDTO)
     {
-        return leaderboardDefKeyKnowledge.getLeaderboardDefIcon(leaderboardDefDTO.getLeaderboardDefKey());
-        // TODO check the exchanges in description
+        int byKey = leaderboardDefKeyKnowledge.getLeaderboardDefIcon(leaderboardDefDTO.getLeaderboardDefKey());
+        if (byKey != 0)
+        {
+            return byKey;
+        }
+        if (leaderboardDefDTO.countryCodes == null)
+        {
+            return 0;
+        }
+        Country fromCode;
+        for (String countryCode : leaderboardDefDTO.countryCodes)
+        {
+            try
+            {
+                return Country.valueOf(countryCode).logoId;
+            }
+            catch (IllegalArgumentException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return 0;
     }
 }
