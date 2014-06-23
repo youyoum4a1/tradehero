5248e2433ad7cd21c90873f8c9c08e248d6a6aa9
Malvin <malvinsutanto@gmail.com>
2014-06-23 12:38:07 +0800

    Cleanup functions and attributes names

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/trending/SearchPeopleItemView.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/trending/SearchPeopleItemView.java
@@ -34,7 +34,6 @@ public class SearchPeopleItemView extends FrameLayout implements DTOView<UserBas
     private TextView profitIndicator;
     private TextView stockPercentage;
     private TextView date;
-    //private CircularImageView userImage;
     private ImageView userPhoto;
     private ImageView peopleBgImage;
     private int defaultDrawable = R.drawable.superman_facebook;
@@ -45,7 +44,7 @@ public class SearchPeopleItemView extends FrameLayout implements DTOView<UserBas
     //<editor-fold desc="Constructors">
     public SearchPeopleItemView(Context context)
     {
-        super(context);    
+        super(context);
     }
 
     public SearchPeopleItemView(Context context, AttributeSet attrs)
@@ -72,7 +71,6 @@ public class SearchPeopleItemView extends FrameLayout implements DTOView<UserBas
         profitIndicator = (TextView) findViewById(R.id.profit_indicator);
         stockPercentage = (TextView) findViewById(R.id.stock_percentage);
         date = (TextView) findViewById(R.id.date);
-        //userImage = (CircularImageView) findViewById(R.id.user_image);
         userPhoto = (ImageView) findViewById(R.id.user_photo);
         if (userPhoto != null)
         {
