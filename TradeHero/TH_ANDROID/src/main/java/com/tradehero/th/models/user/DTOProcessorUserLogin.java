99a6076140347db40a80c9b43202c76089d45672
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-06-25 15:08:52 +0800

    Made DTOProcessor for UserLogin prefetch the initial trending screen.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/user/DTOProcessorUserLogin.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/user/DTOProcessorUserLogin.java
@@ -1,10 +1,18 @@
 package com.tradehero.th.models.user;
 
+import com.tradehero.th.api.market.ExchangeCompactDTO;
+import com.tradehero.th.api.market.ExchangeCompactDTOList;
+import com.tradehero.th.api.market.ExchangeListType;
+import com.tradehero.th.api.security.key.TrendingBasicSecurityListType;
 import com.tradehero.th.api.system.SystemStatusDTO;
+import com.tradehero.th.api.users.UserBaseDTOUtil;
 import com.tradehero.th.api.users.UserBaseKey;
 import com.tradehero.th.api.users.UserLoginDTO;
 import com.tradehero.th.api.users.UserProfileDTO;
+import com.tradehero.th.fragments.trending.TrendingFragment;
 import com.tradehero.th.models.DTOProcessor;
+import com.tradehero.th.persistence.market.ExchangeCompactListCache;
+import com.tradehero.th.persistence.security.SecurityCompactListCache;
 import com.tradehero.th.persistence.system.SystemStatusCache;
 import com.tradehero.th.persistence.user.UserProfileCache;
 import org.jetbrains.annotations.NotNull;
@@ -13,14 +21,23 @@ public class DTOProcessorUserLogin implements DTOProcessor<UserLoginDTO>
 {
     @NotNull private final UserProfileCache userProfileCache;
     @NotNull private final SystemStatusCache systemStatusCache;
+    @NotNull private final UserBaseDTOUtil userBaseDTOUtil;
+    @NotNull private final ExchangeCompactListCache exchangeCompactListCache;
+    @NotNull private final SecurityCompactListCache securityCompactListCache;
 
     //<editor-fold desc="Constructors">
     public DTOProcessorUserLogin(
             @NotNull UserProfileCache userProfileCache,
-            @NotNull SystemStatusCache systemStatusCache)
+            @NotNull SystemStatusCache systemStatusCache,
+            @NotNull UserBaseDTOUtil userBaseDTOUtil,
+            @NotNull ExchangeCompactListCache exchangeCompactListCache,
+            @NotNull SecurityCompactListCache securityCompactListCache)
     {
         this.userProfileCache = userProfileCache;
         this.systemStatusCache = systemStatusCache;
+        this.userBaseDTOUtil = userBaseDTOUtil;
+        this.exchangeCompactListCache = exchangeCompactListCache;
+        this.securityCompactListCache = securityCompactListCache;
     }
     //</editor-fold>
 
@@ -38,6 +55,20 @@ public class DTOProcessorUserLogin implements DTOProcessor<UserLoginDTO>
                     value.systemStatusDTO = new SystemStatusDTO();
                 }
                 systemStatusCache.put(userKey, value.systemStatusDTO);
+
+                ExchangeCompactDTOList exchangeCompacts = exchangeCompactListCache.get(new ExchangeListType());
+                if (exchangeCompacts != null)
+                {
+                    ExchangeCompactDTO initialExchange = userBaseDTOUtil.getInitialExchange(profile, exchangeCompacts);
+                    if (initialExchange != null)
+                    {
+                        securityCompactListCache.getOrFetchAsync(
+                                new TrendingBasicSecurityListType(
+                                        initialExchange.name,
+                                        1,
+                                        TrendingFragment.DEFAULT_PER_PAGE));
+                    }
+                }
             }
         }
         return value;
