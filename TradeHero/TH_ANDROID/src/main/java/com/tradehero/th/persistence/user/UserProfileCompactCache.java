9cca378b4a332e855f175310c7decfb7b5d04aa1
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-06-25 21:55:14 +0800

    Moved UserProfileCompact cache to new model.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/persistence/user/UserProfileCompactCache.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/persistence/user/UserProfileCompactCache.java
@@ -1,17 +1,13 @@
 package com.tradehero.th.persistence.user;
 
-import com.tradehero.common.persistence.StraightDTOCache;
+import com.tradehero.common.persistence.StraightDTOCacheNew;
 import com.tradehero.th.api.users.UserBaseKey;
 import com.tradehero.th.api.users.UserProfileCompactDTO;
-import java.util.ArrayList;
-import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-@Singleton public class UserProfileCompactCache extends StraightDTOCache<UserBaseKey, UserProfileCompactDTO>
+@Singleton public class UserProfileCompactCache extends StraightDTOCacheNew<UserBaseKey, UserProfileCompactDTO>
 {
     public static final int DEFAULT_MAX_SIZE = 1000;
 
@@ -25,24 +21,8 @@ import org.jetbrains.annotations.Nullable;
     }
     //</editor-fold>
 
-    @Override protected UserProfileCompactDTO fetch(@NotNull UserBaseKey key) throws Throwable
+    @Override public UserProfileCompactDTO fetch(@NotNull UserBaseKey key) throws Throwable
     {
-        return userProfileCache.fetch(key);
-    }
-
-    @Contract("null -> null; !null -> !null") @Nullable
-    public List<UserProfileCompactDTO> get(@Nullable List<UserBaseKey> baseKeys)
-    {
-        if (baseKeys == null)
-        {
-            return null;
-        }
-
-        List<UserProfileCompactDTO> userProfileDTOs = new ArrayList<>();
-        for (@NotNull UserBaseKey baseKey: baseKeys)
-        {
-            userProfileDTOs.add(get(baseKey));
-        }
-        return userProfileDTOs;
+        return userProfileCache.getOrFetchSync(key);
     }
 }
