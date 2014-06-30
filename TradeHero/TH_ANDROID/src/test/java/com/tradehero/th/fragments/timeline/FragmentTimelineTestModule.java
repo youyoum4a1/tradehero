c5794eb50b2ce09441517c3d27f41a3a7747d7f8
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-06-30 18:50:57 +0800

    Fixed crash on MeTimelineFragment

---
+++ TradeHero/TH_ANDROID/src/test/java/com/tradehero/th/fragments/timeline/FragmentTimelineTestModule.java
@@ -0,0 +1,14 @@
+package com.tradehero.th.fragments.timeline;
+
+import dagger.Module;
+
+@Module(
+        injects = {
+                MeTimelineFragmentTest.class
+        },
+        complete = false,
+        library = true
+)
+public class FragmentTimelineTestModule
+{
+}
