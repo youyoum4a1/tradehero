d7c58b6d16e894b68fbfb6aed5944dd94546c123
alex <alex@tradehero.mobi>
2014-07-08 21:08:41 +0800

    fix test fail

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/activities/SplashActivity.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/activities/SplashActivity.java
@@ -128,6 +128,7 @@ public class SplashActivity extends SherlockActivity
         {
             ActivityHelper.launchGuide(SplashActivity.this);
             firstLaunchPreference.set(true);
+            finish();
         }
         else
         {
