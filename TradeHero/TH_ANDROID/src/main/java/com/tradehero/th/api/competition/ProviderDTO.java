5500a70a195cdb4654a05340f8ffa41b8893d228
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-07-25 13:39:48 +0800

    Added Transaction cost in provider as per the server.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/competition/ProviderDTO.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/competition/ProviderDTO.java
@@ -10,6 +10,7 @@ public class ProviderDTO extends ProviderCompactDTO
     public String durationType;
     public String totalPrize;
     @Nullable public Boolean vip;
+    @Nullable public Double txnCostUsd;
 
     @Override public String toString()
     {
@@ -20,6 +21,7 @@ public class ProviderDTO extends ProviderCompactDTO
                 ", durationType=" + durationType +
                 ", totalPrize=" + totalPrize +
                 ", vip=" + vip +
+                ", txnCostUsd=" + txnCostUsd +
         '}';
     }
 }
