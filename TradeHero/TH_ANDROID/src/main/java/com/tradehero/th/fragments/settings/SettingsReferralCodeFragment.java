c244fdaa5eaa308b11bd785b0ab37c40b7d923a7
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-07-29 19:15:30 +0800

    Fix, cleanup SettingsReferralCodeFragment.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/settings/SettingsReferralCodeFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/settings/SettingsReferralCodeFragment.java
@@ -11,11 +11,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
+import com.tradehero.common.persistence.DTOCacheNew;
 import com.tradehero.common.utils.THToast;
 import com.tradehero.th.R;
 import com.tradehero.th.activities.CurrentActivityHolder;
 import com.tradehero.th.api.users.CurrentUserId;
 import com.tradehero.th.api.users.UpdateReferralCodeDTO;
+import com.tradehero.th.api.users.UserBaseKey;
 import com.tradehero.th.api.users.UserProfileDTO;
 import com.tradehero.th.fragments.base.DashboardFragment;
 import com.tradehero.th.misc.exception.THException;
@@ -25,12 +28,14 @@ import com.tradehero.th.persistence.user.UserProfileCache;
 import com.tradehero.th.utils.ProgressDialogUtil;
 import dagger.Lazy;
 import javax.inject.Inject;
+import org.jetbrains.annotations.NotNull;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
+import timber.log.Timber;
 
-public class SettingsReferralCodeFragment extends DashboardFragment implements View.OnClickListener
+public class SettingsReferralCodeFragment extends DashboardFragment
 {
-    private MiddleCallback<Response> middleCallback;
+    private MiddleCallback<Response> updateMiddleCallback;
     private ProgressDialog progressDialog;
 
     @Inject CurrentUserId currentUserId;
@@ -46,13 +51,79 @@ public class SettingsReferralCodeFragment extends DashboardFragment implements V
     @InjectView(R.id.referral_code_dialog_key) LinearLayout mReferralCodeLayout;
     @InjectView(R.id.already_done_key) LinearLayout mAlreadyDoneLayout;
 
+    private DTOCacheNew.Listener<UserBaseKey, UserProfileDTO> userProfileCacheListener;
+
+    @Override public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        userProfileCacheListener = createProfileCacheListener();
+    }
+
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_settings_referral_code_layout, container, false);
         ButterKnife.inject(this, view);
-        mOKButton.setOnClickListener(this);
-        mCancelButton.setOnClickListener(this);
-        UserProfileDTO userProfileDTO = userProfileCache.get(currentUserId.toUserBaseKey());
+        return view;
+    }
+
+    @Override public void onResume()
+    {
+        super.onResume();
+        fetchProfile();
+    }
+
+    @Override public void onDestroyView()
+    {
+        detachProfileCache();
+        detachUpdateMiddleCallback();
+        if (progressDialog != null)
+        {
+            progressDialog.dismiss();
+        }
+        progressDialog = null;
+        ButterKnife.reset(this);
+        super.onDestroyView();
+    }
+
+    @Override public void onDestroy()
+    {
+        userProfileCacheListener = null;
+        super.onDestroy();
+    }
+
+    protected void fetchProfile()
+    {
+        detachProfileCache();
+        userProfileCache.register(currentUserId.toUserBaseKey(), userProfileCacheListener);
+        userProfileCache.getOrFetchAsync(currentUserId.toUserBaseKey());
+    }
+
+    protected void detachProfileCache()
+    {
+        userProfileCache.unregister(userProfileCacheListener);
+    }
+
+    protected DTOCacheNew.Listener<UserBaseKey, UserProfileDTO> createProfileCacheListener()
+    {
+        return new SettingsReferralUserProfileListener();
+    }
+
+    protected class SettingsReferralUserProfileListener implements DTOCacheNew.Listener<UserBaseKey, UserProfileDTO>
+    {
+        @Override public void onDTOReceived(@NotNull UserBaseKey key, @NotNull UserProfileDTO value)
+        {
+            linkWith(value, true);
+        }
+
+        @Override public void onErrorThrown(@NotNull UserBaseKey key, @NotNull Throwable error)
+        {
+            THToast.show(R.string.error_fetch_your_user_profile);
+            Timber.e("Failed to fetch profile info", error);
+        }
+    }
+
+    private void linkWith(UserProfileDTO userProfileDTO, boolean andDisplay)
+    {
         if (userProfileDTO != null)
         {
             mReferralCode.setText(userProfileDTO.referralCode);
@@ -63,43 +134,24 @@ public class SettingsReferralCodeFragment extends DashboardFragment implements V
                 mAlreadyDoneLayout.setVisibility(View.VISIBLE);
             }
         }
-        return view;
     }
 
-    @Override public void onDestroyView()
+    @OnClick(R.id.btn_cancel)
+    protected void popFragment(View view)
     {
-        ButterKnife.reset(this);
-        detachMiddleCallback();
-        progressDialog = null;
-        super.onDestroyView();
+        getDashboardNavigator().popFragment();
     }
 
-    @Override public void onDestroy()
+    @OnClick(R.id.btn_ok)
+    protected void submitReferralCode(View view)
     {
-        detachMiddleCallback();
-        getProgressDialog().dismiss();
-        mOKButton.setOnClickListener(null);
-        mCancelButton.setOnClickListener(null);
-        super.onDestroy();
-    }
-
-    @Override public void onClick(View view)
-    {
-        switch (view.getId())
+        if (mInviteCode.getText().toString().length() > 5)
         {
-            case R.id.btn_ok:
-                if (mInviteCode.getText().toString().length() > 5)
-                {
-                    detachMiddleCallback();
-                    getProgressDialog().show();
-                    UpdateReferralCodeDTO updateReferralCodeDTO = new UpdateReferralCodeDTO(
-                            mInviteCode.getText().toString());
-                    middleCallback = userServiceWrapperLazy.get().updateReferralCode(currentUserId.toUserBaseKey(), updateReferralCodeDTO, new TrackCallback());
-                }
-                break;
-            case R.id.btn_cancel:
-                getDashboardNavigator().popFragment();
-                break;
+            detachUpdateMiddleCallback();
+            getProgressDialog().show();
+            UpdateReferralCodeDTO updateReferralCodeDTO = new UpdateReferralCodeDTO(
+                    mInviteCode.getText().toString());
+            updateMiddleCallback = userServiceWrapperLazy.get().updateReferralCode(currentUserId.toUserBaseKey(), updateReferralCodeDTO, new TrackCallback());
         }
     }
 
@@ -120,13 +172,13 @@ public class SettingsReferralCodeFragment extends DashboardFragment implements V
         }
     }
 
-    private void detachMiddleCallback()
+    private void detachUpdateMiddleCallback()
     {
-        if (middleCallback != null)
+        if (updateMiddleCallback != null)
         {
-            middleCallback.setPrimaryCallback(null);
+            updateMiddleCallback.setPrimaryCallback(null);
         }
-        middleCallback = null;
+        updateMiddleCallback = null;
     }
 
     private ProgressDialog getProgressDialog()
