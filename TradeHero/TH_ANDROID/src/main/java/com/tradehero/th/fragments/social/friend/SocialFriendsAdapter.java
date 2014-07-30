4818c9e06f21adda13512dd0d9b7d1adadccb0e3
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-07-30 13:26:04 +0800

    Split SocialFriendUserView into 2, for header and user. Used DTOAdapter. Used intermediate listener for items.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/social/friend/SocialFriendsAdapter.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/social/friend/SocialFriendsAdapter.java
@@ -1,55 +1,40 @@
 package com.tradehero.th.fragments.social.friend;
 
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.tradehero.th.adapters.AbstractArrayAdapter;
+import com.tradehero.th.adapters.ArrayDTOAdapterNew;
 import com.tradehero.th.api.social.UserFriendsDTO;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import timber.log.Timber;
 
-public class SocialFriendsAdapter extends AbstractArrayAdapter<SocialFriendListItemDTO>
+public class SocialFriendsAdapter extends ArrayDTOAdapterNew<SocialFriendListItemDTO, SocialFriendItemView>
 {
-    private Context mContext;
-    private int mLayoutResourceId;
-    private SocialFriendItemView.OnElementClickListener elementClickedListener;
+    private int mLayoutItemResId;
+    private int mLayoutHeaderResId;
+    @Nullable private SocialFriendUserView.OnElementClickListener elementClickedListener;
 
     //<editor-fold desc="Constructors">
-    public SocialFriendsAdapter(Context context, List<SocialFriendListItemDTO> objects, int layoutResourceId)
+    public SocialFriendsAdapter(Context context, List<SocialFriendListItemDTO> objects, int layoutItemResId, int layoutHeaderResId)
     {
-        super(context, 0, objects);
-        this.mContext = context;
-        this.mLayoutResourceId = layoutResourceId;
+        super(context, 0);
+        addAll(objects);
+        this.mLayoutItemResId = layoutItemResId;
+        this.mLayoutHeaderResId = layoutHeaderResId;
     }
     //</editor-fold>
 
     @Override
     public SocialFriendItemView getView(int position, View convertView, ViewGroup parent)
     {
-        if (convertView == null)
+        SocialFriendItemView itemView = super.getView(position, convertView, parent);
+        if (convertView instanceof SocialFriendUserView)
         {
-            convertView = LayoutInflater.from(mContext).inflate(getViewResId(position), parent, false);
+            ((SocialFriendUserView) convertView).setOnElementClickedListener(createUserClickedListener());
         }
-        SocialFriendItemView dtoView = (SocialFriendItemView) convertView;
-        SocialFriendListItemDTO item = getItem(position);
-        dtoView.display(item);
-        dtoView.setOnElementClickedListener(elementClickedListener);
-        return dtoView;
-    }
-
-    @Override
-    protected String getItemNameForFilter(SocialFriendListItemDTO item)
-    {
-        if (item instanceof SocialFriendListItemUserDTO)
-        {
-            return ((SocialFriendListItemUserDTO) item).userFriendsDTO.name;
-        }
-        else if(item instanceof SocialFriendListItemHeaderDTO)
-        {
-            return ((SocialFriendListItemHeaderDTO) item).name;
-        }
-        throw new IllegalArgumentException("Unhandled element type");
+        return itemView;
     }
 
     @Override public boolean areAllItemsEnabled()
@@ -62,46 +47,65 @@ public class SocialFriendsAdapter extends AbstractArrayAdapter<SocialFriendListI
         return getItem(position) instanceof SocialFriendListItemUserDTO;
     }
 
-    protected int getViewResId(int position)
+    @Override public int getViewResId(int position)
     {
-        return mLayoutResourceId;
+        SocialFriendListItemDTO item = getItem(position);
+        if (item instanceof SocialFriendListItemHeaderDTO)
+        {
+            return mLayoutHeaderResId;
+        }
+        else if (item instanceof SocialFriendListItemUserDTO)
+        {
+            return mLayoutItemResId;
+        }
+        throw new IllegalArgumentException("Unhandled item type " + item.getClass());
     }
 
     public void setOnElementClickedListener(
-            SocialFriendItemView.OnElementClickListener elementClickedListener)
+            @Nullable SocialFriendUserView.OnElementClickListener elementClickedListener)
     {
         this.elementClickedListener = elementClickedListener;
     }
 
-    protected void handleFollowEvent(UserFriendsDTO userFriendsDTO)
+    protected void handleFollowEvent(@NotNull UserFriendsDTO userFriendsDTO)
     {
+        SocialFriendUserView.OnElementClickListener listenerCopy = elementClickedListener;
+        if (listenerCopy != null)
+        {
+            listenerCopy.onFollowButtonClick(userFriendsDTO);
+        }
     }
 
-    protected void handleInviteEvent(UserFriendsDTO userFriendsDTO)
+    protected void handleInviteEvent(@NotNull UserFriendsDTO userFriendsDTO)
     {
+        SocialFriendUserView.OnElementClickListener listenerCopy = elementClickedListener;
+        if (listenerCopy != null)
+        {
+            listenerCopy.onInviteButtonClick(userFriendsDTO);
+        }
     }
 
-    protected SocialFriendItemView.OnElementClickListener createUserClickedListener()
+    protected SocialFriendUserView.OnElementClickListener createUserClickedListener()
     {
         return new SocialElementClickListener();
     }
 
-    protected class SocialElementClickListener implements SocialFriendItemView.OnElementClickListener
+    protected class SocialElementClickListener implements SocialFriendUserView.OnElementClickListener
     {
         @Override
-        public void onFollowButtonClick(UserFriendsDTO userFriendsDTO)
+        public void onFollowButtonClick(@NotNull UserFriendsDTO userFriendsDTO)
         {
             handleFollowEvent(userFriendsDTO);
         }
 
         @Override
-        public void onInviteButtonClick(UserFriendsDTO userFriendsDTO)
+        public void onInviteButtonClick(@NotNull UserFriendsDTO userFriendsDTO)
         {
             handleInviteEvent(userFriendsDTO);
         }
 
         @Override
-        public void onCheckBoxClick(UserFriendsDTO userFriendsDTO)
+        public void onCheckBoxClick(@NotNull UserFriendsDTO userFriendsDTO)
         {
             Timber.d("onCheckBoxClicked " + userFriendsDTO);
         }
