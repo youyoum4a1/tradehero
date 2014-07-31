328d73668088ddced786acb15453aa2e8ba8a16a
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-07-31 13:11:11 +0800

    Add missing return statement on handling expired facebook access token

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/auth/FacebookAuthenticationProvider.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/auth/FacebookAuthenticationProvider.java
@@ -150,6 +150,7 @@ public class FacebookAuthenticationProvider implements THAuthenticationProvider
                     if (facebook.isSessionValid())
                     {
                         extendAccessToken(activity, currentOperationCallback);
+                        return;
                     }
                     createAndExecuteMeRequest(session);
                 }
