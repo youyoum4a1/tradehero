42527c1ec21b56469338a6dc9645036b7df58d7d
Malvin <malvinsutanto@gmail.com>
2014-08-05 17:43:35 +0800

    Implement Touch Listener which dismisses the dialog

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/achievement/AchievementDialogFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/achievement/AchievementDialogFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,10 +16,13 @@ import butterknife.InjectView;
 import butterknife.OnClick;
 import com.tradehero.th.R;
 import com.tradehero.th.fragments.base.BaseDialogFragment;
+import com.tradehero.th.models.level.LevelDTO;
 import com.tradehero.th.widget.UserLevelProgressBar;
 
 public class AchievementDialogFragment extends BaseDialogFragment
 {
+    @InjectView(R.id.achievement_content_container) ViewGroup contentContainer;
+
     @InjectView(R.id.achievement_header) TextView header;
     @InjectView(R.id.achievement_title) TextView title;
     @InjectView(R.id.achievement_description) TextView description;
@@ -32,12 +36,16 @@ public class AchievementDialogFragment extends BaseDialogFragment
     @InjectView(R.id.btn_achievement_dismiss) Button btnDismiss;
     @InjectView(R.id.btn_achievement_share) Button btnShare;
 
+    private boolean mShouldDismissOnOutsideClicked;
+
     @Override public Dialog onCreateDialog(Bundle savedInstanceState)
     {
         Dialog d = super.onCreateDialog(savedInstanceState);
-        setStyle(DialogFragment.STYLE_NO_TITLE, getTheme());
-        //d.getWindow().setWindowAnimations(R.style.detailDialogAnimation); //TODO
+        setStyle(DialogFragment.STYLE_NO_TITLE, R.style.TH_Achievement_Dialog);
+        d.getWindow().setWindowAnimations(R.style.TH_Achievement_Dialog_Animation);
         d.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        d.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+        d.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         return d;
     }
 
@@ -50,6 +58,43 @@ public class AchievementDialogFragment extends BaseDialogFragment
     @Override public void onViewCreated(View view, Bundle savedInstanceState)
     {
         super.onViewCreated(view, savedInstanceState);
+        mShouldDismissOnOutsideClicked = true; //TODO
+    }
+
+    @Override public void onResume()
+    {
+        super.onResume();
+        contentContainer.setOnTouchListener(
+                new AchievementDialogSwipeDismissTouchListener(contentContainer, null,
+                        new AchievementDialogSwipeDismissTouchListener.DismissCallbacks()
+                        {
+                            @Override public boolean canDismiss(Object token)
+                            {
+                                return true;
+                            }
+
+                            @Override public void onDismiss(View view, Object token)
+                            {
+                                removeDialogAnimation();
+                                getDialog().dismiss();
+                            }
+                        }));
+    }
+
+    private void removeDialogAnimation()
+    {
+        getDialog().getWindow().setWindowAnimations(R.style.TH_Achievement_Dialog_NoAnimation);
+    }
+
+    @Override public void onPause()
+    {
+        contentContainer.setOnTouchListener(null);
+        super.onPause();
+    }
+
+    public void setShouldDismissOnOutsideClicked(boolean mShouldDismissOnOutsideClicked)
+    {
+        this.mShouldDismissOnOutsideClicked = mShouldDismissOnOutsideClicked;
     }
 
     @OnClick(R.id.btn_achievement_share)
@@ -62,5 +107,27 @@ public class AchievementDialogFragment extends BaseDialogFragment
     public void onDismissBtnClicked()
     {
         getDialog().dismiss();
+    }
+
+    @OnClick(R.id.achievement_dummy_container)
+    public void onOutsideContentClicked()
+    {
+        if (mShouldDismissOnOutsideClicked)
+        {
+            getDialog().dismiss();
+        }
+    }
+
+    private UserLevelProgressBar.UserLevelProgressBarListener createUserLevelProgressBarListener()
+    {
+        return new AchievementUserLevelProgressBarListener();
+    }
+
+    protected class AchievementUserLevelProgressBarListener implements UserLevelProgressBar.UserLevelProgressBarListener
+    {
+        @Override public void onLevelUp(LevelDTO fromLevel, LevelDTO toLevel)
+        {
+
+        }
     }
 }
