669ab2d10e91dfacc87f9dc31d53c75b41da5a0c
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-08-06 11:25:10 +0800

    Unused injection

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/leaderboard/LeaderboardMarkUserListAdapter.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/leaderboard/LeaderboardMarkUserListAdapter.java
@@ -11,8 +11,6 @@ import com.tradehero.th.api.leaderboard.LeaderboardUserDTO;
 import com.tradehero.th.api.portfolio.OwnedPortfolioId;
 import com.tradehero.th.api.users.UserBaseDTO;
 import com.tradehero.th.api.users.UserProfileDTO;
-import com.tradehero.th.billing.THBillingInteractor;
-import javax.inject.Inject;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,7 +19,6 @@ public class LeaderboardMarkUserListAdapter extends
                 LeaderboardUserDTO, LeaderboardMarkUserItemView, LeaderboardMarkUserLoader>
     implements PullToRefreshBase.OnRefreshListener<ListView>
 {
-    @Inject protected THBillingInteractor userInteractor;
     protected UserProfileDTO currentUserProfileDTO;
     @Nullable protected OwnedPortfolioId applicablePortfolioId;
     protected LeaderboardMarkUserItemView.OnFollowRequestedListener followRequestedListener;
