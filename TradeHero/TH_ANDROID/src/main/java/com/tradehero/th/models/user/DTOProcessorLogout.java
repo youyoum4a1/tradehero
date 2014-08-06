63e54dadb95f3477450868f9e5727308246ad885
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-08-06 17:51:24 +0800

    Made UserProfileCache clear message and notification caches if there is a change in the count.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/user/DTOProcessorLogout.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/user/DTOProcessorLogout.java
@@ -4,18 +4,23 @@ import android.app.NotificationManager;
 import com.tradehero.th.api.users.UserProfileDTO;
 import com.tradehero.th.models.DTOProcessor;
 import com.tradehero.th.persistence.DTOCacheUtil;
+import org.jetbrains.annotations.NotNull;
 
-public class DTOProcessorLogout  implements DTOProcessor<UserProfileDTO>
+public class DTOProcessorLogout implements DTOProcessor<UserProfileDTO>
 {
-    private final DTOCacheUtil dtoCacheUtil;
-    private final NotificationManager notificationManager;
+    @NotNull private final DTOCacheUtil dtoCacheUtil;
+    @NotNull private final NotificationManager notificationManager;
 
-    public DTOProcessorLogout(DTOCacheUtil dtoCacheUtil, NotificationManager notificationManager)
+    //<editor-fold desc="Constructors">
+    public DTOProcessorLogout(
+            @NotNull DTOCacheUtil dtoCacheUtil,
+            @NotNull NotificationManager notificationManager)
     {
         super();
         this.dtoCacheUtil = dtoCacheUtil;
         this.notificationManager = notificationManager;
     }
+    //</editor-fold>
 
     @Override public UserProfileDTO process(UserProfileDTO userProfileDTO)
     {
--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/persistence/user/UserProfileCache.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/persistence/user/UserProfileCache.java
@@ -5,6 +5,8 @@ import com.tradehero.th.api.users.UserBaseKey;
 import com.tradehero.th.api.users.UserProfileDTO;
 import com.tradehero.th.network.service.UserServiceWrapper;
 import com.tradehero.th.persistence.leaderboard.LeaderboardCache;
+import com.tradehero.th.persistence.message.MessageHeaderListCache;
+import com.tradehero.th.persistence.notification.NotificationListCache;
 import com.tradehero.th.persistence.social.HeroListCache;
 import com.tradehero.th.persistence.social.VisitedFriendListPrefs;
 import dagger.Lazy;
@@ -20,19 +22,25 @@ import org.jetbrains.annotations.NotNull;
     @NotNull private final Lazy<UserProfileCompactCache> userProfileCompactCache;
     @NotNull private final Lazy<HeroListCache> heroListCache;
     @NotNull private final Lazy<LeaderboardCache> leaderboardCache;
+    @NotNull private final Lazy<MessageHeaderListCache> messageHeaderListCache;
+    @NotNull private final Lazy<NotificationListCache> notificationListCache;
 
     //<editor-fold desc="Constructors">
     @Inject public UserProfileCache(
             @NotNull Lazy<UserServiceWrapper> userServiceWrapper,
             @NotNull Lazy<UserProfileCompactCache> userProfileCompactCache,
             @NotNull Lazy<HeroListCache> heroListCache,
-            @NotNull Lazy<LeaderboardCache> leaderboardCache)
+            @NotNull Lazy<LeaderboardCache> leaderboardCache,
+            @NotNull Lazy<MessageHeaderListCache> messageHeaderListCache,
+            @NotNull Lazy<NotificationListCache> notificationListCache)
     {
         super(DEFAULT_MAX_SIZE);
         this.userServiceWrapper = userServiceWrapper;
         this.userProfileCompactCache = userProfileCompactCache;
         this.heroListCache = heroListCache;
         this.leaderboardCache = leaderboardCache;
+        this.messageHeaderListCache = messageHeaderListCache;
+        this.notificationListCache = notificationListCache;
     }
     //</editor-fold>
 
@@ -50,6 +58,18 @@ import org.jetbrains.annotations.NotNull;
             leaderboardCache.get().put(userProfileDTO.getMostSkilledLbmuKey(), userProfileDTO.mostSkilledLbmu);
         }
         userProfileCompactCache.get().put(userBaseKey, userProfileDTO);
-        return super.put(userBaseKey, userProfileDTO);
+        UserProfileDTO previous = super.put(userBaseKey, userProfileDTO);
+        if (previous != null)
+        {
+            if (previous.unreadMessageThreadsCount != userProfileDTO.unreadMessageThreadsCount)
+            {
+                messageHeaderListCache.get().invalidateAll();
+            }
+            if (previous.unreadNotificationsCount != userProfileDTO.unreadNotificationsCount)
+            {
+                notificationListCache.get().invalidateAll();
+            }
+        }
+        return previous;
     }
 }
