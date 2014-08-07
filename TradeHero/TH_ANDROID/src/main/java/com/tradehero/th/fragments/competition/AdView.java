c2b0155b739aa2a2a46ccf82ff1be7a3a2be13ec
alex <alex@tradehero.mobi>
2014-08-07 16:51:39 +0800

    P1:Unable to track SGX StockWhiz competition banner click-through [#76417418]

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/competition/AdView.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/competition/AdView.java
@@ -9,8 +9,11 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import com.squareup.picasso.Picasso;
+import com.tradehero.common.utils.THToast;
 import com.tradehero.th.R;
 import com.tradehero.th.api.DTOView;
+import com.tradehero.th.api.analytics.AnalyticsEventForm;
+import com.tradehero.th.api.analytics.BatchAnalyticsEventForm;
 import com.tradehero.th.api.competition.AdDTO;
 import com.tradehero.th.api.users.CurrentUserId;
 import com.tradehero.th.base.Navigator;
@@ -18,15 +21,22 @@ import com.tradehero.th.base.NavigatorActivity;
 import com.tradehero.th.fragments.competition.zone.dto.CompetitionZoneAdvertisementDTO;
 import com.tradehero.th.fragments.competition.zone.dto.CompetitionZoneDTO;
 import com.tradehero.th.fragments.web.WebViewFragment;
+import com.tradehero.th.network.service.UserServiceWrapper;
 import com.tradehero.th.utils.DaggerUtils;
+import com.tradehero.th.utils.DateUtils;
 import dagger.Lazy;
+import java.util.ArrayList;
+import java.util.Date;
 import javax.inject.Inject;
 
 public class AdView extends RelativeLayout
         implements DTOView<CompetitionZoneDTO>
 {
     @InjectView(R.id.banner) ImageView banner;
+    @Inject UserServiceWrapper userServiceWrapper;
+    @Inject Context context;
     private AdDTO adDTO;
+    private int providerId;
 
     @OnClick(R.id.banner) void onBannerClicked(ImageView banner)
     {
@@ -36,6 +46,7 @@ public class AdView extends RelativeLayout
             String url = adDTO.redirectUrl + String.format("&userId=%d", currentUserId.get());
             WebViewFragment.putUrl(bundle, url);
             getNavigator().pushFragment(WebViewFragment.class, bundle);
+            sendAnalytics(adDTO, "proceed");
         }
     }
 
@@ -120,12 +131,41 @@ public class AdView extends RelativeLayout
                     picasso.get().load(adDTO.bannerImageUrl)
                             .into(banner);
                 }
+
+                sendAnalytics(adDTO, "served");
             }
             else
             {
                 banner.setImageDrawable(null);
             }
         }
+    }
+
+    private void sendAnalytics(final AdDTO adDTO, final String event)
+    {
+        Thread thread = new Thread(new Runnable()
+        {
+            @Override public void run()
+            {
+                try
+                {
+                    AnalyticsEventForm analyticsEventForm = new AnalyticsEventForm(event,
+                            DateUtils.getFormattedUtcDateFromDate(context.getResources(),
+                                    new Date(System.currentTimeMillis())), adDTO.id, providerId,
+                            currentUserId.toUserBaseKey().getUserId());
+                    BatchAnalyticsEventForm batchAnalyticsEventForm = new BatchAnalyticsEventForm();
+                    batchAnalyticsEventForm.events = new ArrayList<>();
+                    batchAnalyticsEventForm.events.add(analyticsEventForm);
+                    userServiceWrapper.sendAnalytics(batchAnalyticsEventForm);
+                }
+                catch (Exception e)
+                {
+                    THToast.show(e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
     }
 
     private String getResourceFileName(String url)
@@ -142,5 +182,10 @@ public class AdView extends RelativeLayout
             return fileName.replace('-', '_').toLowerCase();
         }
         return null;
+    }
+
+    public void setProviderId(int providerId)
+    {
+        this.providerId = providerId;
     }
 }
