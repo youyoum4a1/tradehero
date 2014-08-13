86a6389c6e621dba7b0a1f3fe2ee848712f20139
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-08-13 19:34:39 +0800

    Behold the shrunk discussion dtos put in cache! Ensures that Timeline item views show the latest user profile picture. #76856310

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/persistence/discussion/DiscussionCache.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/persistence/discussion/DiscussionCache.java
@@ -1,6 +1,6 @@
 package com.tradehero.th.persistence.discussion;
 
-import com.tradehero.common.persistence.StraightDTOCacheNew;
+import com.tradehero.common.persistence.StraightCutDTOCacheNew;
 import com.tradehero.common.persistence.prefs.IntPreference;
 import com.tradehero.th.api.discussion.AbstractDiscussionCompactDTO;
 import com.tradehero.th.api.discussion.DiscussionDTOList;
@@ -15,26 +15,30 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 @Singleton
-public class DiscussionCache extends StraightDTOCacheNew<DiscussionKey, AbstractDiscussionCompactDTO>
+public class DiscussionCache extends StraightCutDTOCacheNew<DiscussionKey, AbstractDiscussionCompactDTO, AbstractDiscussionCompactCutDTO>
 {
     @NotNull private final DiscussionServiceWrapper discussionServiceWrapper;
     @NotNull private final NewsServiceWrapper newsServiceWrapper;
     @NotNull private final UserTimelineServiceWrapper timelineServiceWrapper;
+    @NotNull private final AbstractDiscussionCompactCutDTOFactory cutDTOFactory;
 
     //<editor-fold desc="Constructors">
     @Inject public DiscussionCache(
             @SingleCacheMaxSize IntPreference maxSize,
             @NotNull NewsServiceWrapper newsServiceWrapper,
             @NotNull UserTimelineServiceWrapper userTimelineServiceWrapper,
-            @NotNull DiscussionServiceWrapper discussionServiceWrapper)
+            @NotNull DiscussionServiceWrapper discussionServiceWrapper,
+            @NotNull AbstractDiscussionCompactCutDTOFactory cutDTOFactory)
     {
         super(maxSize.get());
 
         this.discussionServiceWrapper = discussionServiceWrapper;
         this.newsServiceWrapper = newsServiceWrapper;
         this.timelineServiceWrapper = userTimelineServiceWrapper;
+        this.cutDTOFactory = cutDTOFactory;
     }
     //</editor-fold>
 
@@ -49,6 +53,20 @@ public class DiscussionCache extends StraightDTOCacheNew<DiscussionKey, Abstract
             return newsServiceWrapper.getSecurityNewsDetail(discussionKey.id);
         }
         return discussionServiceWrapper.getComment(discussionKey);
+    }
+
+    @NotNull @Override protected AbstractDiscussionCompactCutDTO cutValue(
+            @NotNull DiscussionKey key,
+            @NotNull AbstractDiscussionCompactDTO value)
+    {
+        return cutDTOFactory.shrinkValue(value);
+    }
+
+    @Nullable @Override protected AbstractDiscussionCompactDTO inflateValue(
+            @NotNull DiscussionKey key,
+            @Nullable AbstractDiscussionCompactCutDTO cutValue)
+    {
+        return cutDTOFactory.inflate(cutValue);
     }
 
     public DiscussionDTOList put(List<? extends AbstractDiscussionCompactDTO> discussionList)
