a1493afe93124d22b240fd2c4b1b570332156fe7
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-08-14 15:31:04 +0800

    Robolectric gradle test runner

---
+++ TradeHero/TH_ANDROID/src/test/java/com/tradehero/RobolectricGradleTestRunner.java
@@ -0,0 +1,29 @@
+package com.tradehero;
+
+import org.junit.runners.model.InitializationError;
+import org.robolectric.AndroidManifest;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.res.Fs;
+
+/**
+ * Created by tho on 8/14/2014.
+ */
+public class RobolectricGradleTestRunner extends RobolectricTestRunner {
+    public RobolectricGradleTestRunner(Class<?> testClass) throws InitializationError {
+        super(testClass);
+    }
+
+    @Override
+    protected AndroidManifest getAppManifest(Config config) {
+        String manifestProperty = System.getProperty("android.manifest");
+        if (config.manifest().equals(Config.DEFAULT) && manifestProperty != null) {
+            String resProperty = System.getProperty("android.resources");
+            String assetsProperty = System.getProperty("android.assets");
+            return new AndroidManifest(Fs.fileFromPath(manifestProperty),
+                    Fs.fileFromPath(resProperty),
+                    Fs.fileFromPath(assetsProperty));
+        }
+        return super.getAppManifest(config);
+    }
+}
