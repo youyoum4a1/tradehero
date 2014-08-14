c43425502cc45f72c57c2c6c3448d317ecf38ddc
alex <alex@tradehero.mobi>
2014-08-14 17:47:57 +0800

    fix bug of num is wrong when share buy trade by wechat

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/trade/BuySellFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/trade/BuySellFragment.java
@@ -1230,13 +1230,13 @@ public class BuySellFragment extends AbstractBuySellFragment
             if (isTransactionTypeBuy)
             {
                 weChatDTO.title = getString(R.string.buy_sell_switch_buy) + " "
-                        + securityCompactDTO.name + " " + mQuantity + getString(
+                        + securityCompactDTO.name + " " + abstractTransactionDialogFragment.getQuantityString() + getString(
                         R.string.buy_sell_share_count) + " @" + quoteDTO.ask;
             }
             else
             {
                 weChatDTO.title = getString(R.string.buy_sell_switch_sell) + " "
-                        + securityCompactDTO.name + " " + mQuantity + getString(
+                        + securityCompactDTO.name + " " + abstractTransactionDialogFragment.getQuantityString() + getString(
                         R.string.buy_sell_share_count) + " @" + quoteDTO.bid;
             }
             socialSharerLazy.get().share(weChatDTO); // TODO proper callback?
