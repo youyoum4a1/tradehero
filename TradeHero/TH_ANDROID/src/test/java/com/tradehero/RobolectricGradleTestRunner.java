c6eaf486a5c40a3f81609d9884e013f3ed385e83
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-08-15 19:14:07 +0800

    Gradle configuration to help tests work with IntelliJ/AndroidStudio

--- TradeHero/TH_ANDROID/src/test/java/com/tradehero/RobolectricGradleTestRunner.java
+++ TradeHero/TH_ANDROID/src/test/java/com/tradehero/RobolectricGradleTestRunner.java
@@ -1,5 +1,7 @@
 package com.tradehero;
 
+import com.tradehero.th.BuildConfig;
+import java.io.File;
 import org.junit.runners.model.InitializationError;
 import org.robolectric.AndroidManifest;
 import org.robolectric.RobolectricTestRunner;
@@ -19,15 +21,51 @@ public class RobolectricGradleTestRunner extends RobolectricTestRunner
     @Override
     protected AndroidManifest getAppManifest(Config config)
     {
-        String manifestProperty = System.getProperty("android.manifest");
         if (config.manifest().equals(Config.DEFAULT))
         {
-            String resProperty = System.getProperty("android.resources");
-            String assetsProperty = System.getProperty("android.assets");
-            return new GradleAndroidManifest(Fs.fileFromPath(manifestProperty),
-                    Fs.fileFromPath(resProperty),
-                    Fs.fileFromPath(assetsProperty));
+            if (BuildConfig.IS_INTELLIJ)
+            {
+                String manifestProperty = getConfigPath("manifests") + "AndroidManifest.xml";
+                String resProperty = getConfigPath("res");
+                String assetsProperty = getConfigPath("assets");
+                return new GradleAndroidManifest(
+                        Fs.fileFromPath(manifestProperty),
+                        Fs.fileFromPath(resProperty),
+                        Fs.fileFromPath(assetsProperty)
+                );
+            }
+            else
+            {
+                String manifestProperty = System.getProperty("android.manifest");
+                String resProperty = System.getProperty("android.resources");
+                String assetsProperty = System.getProperty("android.assets");
+                return new GradleAndroidManifest(
+                        Fs.fileFromPath(manifestProperty),
+                        Fs.fileFromPath(resProperty),
+                        Fs.fileFromPath(assetsProperty)
+                );
+            }
         }
         return super.getAppManifest(config);
+    }
+
+    private String getConfigPath(String configType)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("TradeHero").append(File.separator).append("TH_ANDROID").append(File.separator)
+                .append("build")
+                .append(File.separator)
+                .append("intermediates")
+                .append(File.separator)
+                .append(configType)
+                .append(File.separator);
+        if (BuildConfig.FLAVOR.length() > 0)
+        {
+            sb.append(BuildConfig.FLAVOR)
+                    .append(File.separator);
+        }
+        sb.append(BuildConfig.BUILD_TYPE)
+                .append(File.separator);
+        return sb.toString();
     }
 }
