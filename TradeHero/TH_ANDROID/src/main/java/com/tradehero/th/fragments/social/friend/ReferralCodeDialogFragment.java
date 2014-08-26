360f2b72b28cc84494d1acb4767f373ab7b5600d
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-08-26 18:20:58 +0800

    Objectified Referral code window, so as to avoid duplication of code.

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/social/friend/ReferralCodeDialogFragment.java
@@ -0,0 +1,56 @@
+package com.tradehero.th.fragments.social.friend;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import com.tradehero.th.R;
+import com.tradehero.th.fragments.base.BaseDialogFragment;
+
+public class ReferralCodeDialogFragment extends BaseDialogFragment
+{
+    public static void showReferralCodeDialog(FragmentManager fragmentManager)
+    {
+        new ReferralCodeDialogFragment().show(fragmentManager, ReferralCodeDialogFragment.class.getName());
+    }
+
+    @Override public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setStyle(BaseDialogFragment.STYLE_NO_TITLE, getTheme());
+    }
+
+    @Override public Dialog onCreateDialog(Bundle savedInstanceState)
+    {
+        Dialog d = super.onCreateDialog(savedInstanceState);
+        d.getWindow().setWindowAnimations(R.style.TH_BuySellDialogAnimation);
+        return d;
+    }
+
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.referral_code_dialog_layout, container, false);
+    }
+
+    @Override public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.inject(this, view);
+    }
+
+    @Override public void onDestroyView()
+    {
+        ButterKnife.reset(this);
+        super.onDestroyView();
+    }
+
+    @OnClick({R.id.btn_cancel, R.id.btn_cancel_submit, R.id.btn_done})
+    @Override public void dismiss()
+    {
+        super.dismiss();
+    }
+}
