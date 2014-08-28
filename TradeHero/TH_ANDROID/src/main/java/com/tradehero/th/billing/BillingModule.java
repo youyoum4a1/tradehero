0cad5fba32369ce17b897c69ea463a51c5c018ed
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-08-28 13:45:11 +0800

    First shot at Amazon billing.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/billing/BillingModule.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/billing/BillingModule.java
@@ -2,6 +2,7 @@ package com.tradehero.th.billing;
 
 import com.tradehero.common.billing.BillingInteractor;
 import com.tradehero.common.billing.BillingLogicHolder;
+import com.tradehero.th.billing.amazon.AmazonBillingModule;
 import com.tradehero.th.billing.googleplay.GooglePlayBillingModule;
 import com.tradehero.th.billing.samsung.SamsungBillingModule;
 import dagger.Module;
@@ -10,8 +11,9 @@ import javax.inject.Singleton;
 
 @Module(
         includes = {
+                //AmazonBillingModule.class,
                 GooglePlayBillingModule.class,
-                //SamsungBillingModule.class
+                //SamsungBillingModule.class,
         },
         injects = {
         },
