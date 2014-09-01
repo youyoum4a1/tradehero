5b5475deef2c41760da956dac9bb8b4c051bd291
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-09-01 17:35:12 +0800

    Disable swipe on discovery tab, also rename activity screen -> discussions (new specs)

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discovery/DiscoveryDiscussionFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discovery/DiscoveryDiscussionFragment.java
@@ -38,7 +38,7 @@ public class DiscoveryDiscussionFragment extends SherlockFragment
 
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View view = inflater.inflate(R.layout.discovery_activity, container, false);
+        View view = inflater.inflate(R.layout.discovery_discussion, container, false);
         initView(view);
         return view;
     }
