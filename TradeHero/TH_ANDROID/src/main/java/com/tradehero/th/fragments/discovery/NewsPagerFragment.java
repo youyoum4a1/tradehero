14b74958cb521027fcba107d7320f5a23b64faf8
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-09-03 16:22:11 +0800

    Implement filter for the discovery news (on top)
    - Use 2 viewpagers
    - Cache all the pages
    - Bottom viewpager will listen to page change event of the top viewpager
    (the filter)

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discovery/NewsPagerFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discovery/NewsPagerFragment.java
@@ -11,15 +11,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 import com.actionbarsherlock.app.SherlockFragment;
 import com.tradehero.th.R;
 import com.tradehero.th.utils.DaggerUtils;
-import com.viewpagerindicator.TitlePageIndicator;
 
 public class NewsPagerFragment extends SherlockFragment
 {
     @InjectView(R.id.news_pager) ViewPager mViewPager;
-    @InjectView(R.id.news_indicator) TitlePageIndicator mPageIndicator;
+    @InjectView(R.id.news_carousel) ViewPager mNewsCarousel;
+    @OnClick(R.id.previous_filter) void handlePreviousFilterClick()
+    {
+        int currentItem = mNewsCarousel.getCurrentItem();
+        int size = mNewsCarousel.getAdapter().getCount();
+        mNewsCarousel.setCurrentItem((currentItem + size - 1) % size);
+    }
+
+    @OnClick(R.id.next_filter) void handleNextFilterClick()
+    {
+        int currentItem = mNewsCarousel.getCurrentItem();
+        int size = mNewsCarousel.getAdapter().getCount();
+        mNewsCarousel.setCurrentItem((currentItem + 1) % size);
+    }
 
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
@@ -32,8 +45,22 @@ public class NewsPagerFragment extends SherlockFragment
     {
         ButterKnife.inject(this, view);
 
+        int totalPage = NewsType.values().length;
         mViewPager.setAdapter(new DiscoveryNewsFragmentAdapter(((Fragment) this).getChildFragmentManager()));
-        mPageIndicator.setViewPager(mViewPager);
+        mNewsCarousel.setAdapter(new DiscoveryNewsCarouselFragmentAdapter(((Fragment) this).getChildFragmentManager()));
+
+        // all pages need to be cached, coz they are both circle pagers
+        mViewPager.setOffscreenPageLimit(totalPage);
+        mNewsCarousel.setOffscreenPageLimit(totalPage);
+
+        mNewsCarousel.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener()
+        {
+            @Override public void onPageSelected(int position)
+            {
+                mViewPager.setCurrentItem(position);
+            }
+        });
+        //mPageIndicator.setViewPager(mViewPager);
     }
 
     @Override public void onAttach(Activity activity)
@@ -42,7 +69,7 @@ public class NewsPagerFragment extends SherlockFragment
         DaggerUtils.inject(this);
     }
 
-    private class DiscoveryNewsFragmentAdapter extends FragmentPagerAdapter
+    private class DiscoveryNewsFragmentAdapter extends DiscoveryNewsAdapter
     {
         public DiscoveryNewsFragmentAdapter(FragmentManager fm)
         {
@@ -52,6 +79,27 @@ public class NewsPagerFragment extends SherlockFragment
         @Override public Fragment getItem(int i)
         {
             return FeaturedNewsHeadlineFragment.newInstance(NewsType.values()[i]);
+        }
+    }
+
+    private class DiscoveryNewsCarouselFragmentAdapter extends DiscoveryNewsAdapter
+    {
+        public DiscoveryNewsCarouselFragmentAdapter(FragmentManager fm)
+        {
+            super(fm);
+        }
+
+        @Override public Fragment getItem(int i)
+        {
+            return NewsCarouselFragment.newInstance(NewsType.values()[i]);
+        }
+    }
+
+    private abstract class DiscoveryNewsAdapter extends FragmentPagerAdapter
+    {
+        public DiscoveryNewsAdapter(FragmentManager fm)
+        {
+            super(fm);
         }
 
         @Override public int getCount()
--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discovery/NewsType.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discovery/NewsType.java
@@ -4,14 +4,16 @@ import com.tradehero.th.R;
 
 enum NewsType
 {
-    Region(R.string.regional_news),
-    MotleyFool(R.string.motley_fool),
-    Global(R.string.global_news);
+    Region(R.string.regional_news, R.layout.news_carousel_regional),
+    MotleyFool(R.string.motley_fool, R.layout.news_carousel_motleyfool),
+    Global(R.string.global_news, R.layout.news_carousel_global);
 
     public final int titleResourceId;
+    public final int titleViewResourceId;
 
-    NewsType(int titleResourceId)
+    NewsType(int titleResourceId, int titleViewResourceId)
     {
         this.titleResourceId = titleResourceId;
+        this.titleViewResourceId = titleViewResourceId;
     }
 }
