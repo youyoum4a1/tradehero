0840e20012a55cdffd53c7fa7bc35d51815540a3
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-09-09 18:03:43 +0800

    Separated proguard definitions for Amazon.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/utils/IOUtils.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/utils/IOUtils.java
@@ -1,5 +1,7 @@
 package com.tradehero.common.utils;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import retrofit.RetrofitError;
 
 import java.io.ByteArrayOutputStream;
@@ -15,7 +17,7 @@ public class IOUtils
      * Be careful to use this method since the buffer size has only 0x1000 bytes
      * @throws IOException
      */
-    public static byte[] streamToBytes(InputStream stream) throws IOException
+    public static byte[] streamToBytes(@Nullable InputStream stream) throws IOException
     {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         if (stream != null)
@@ -30,7 +32,7 @@ public class IOUtils
         return baos.toByteArray();
     }
 
-    public static byte[] streamToBytes(InputStream stream, int length) throws IOException
+    public static byte[] streamToBytes(@Nullable InputStream stream, int length) throws IOException
     {
         if (stream != null)
         {
@@ -41,12 +43,12 @@ public class IOUtils
         return null;
     }
 
-    public static String streamToString(InputStream stream) throws IOException
+    public static String streamToString(@Nullable InputStream stream) throws IOException
     {
         return new String(streamToBytes(stream), "UTF-8");
     }
 
-    public static String errorToBodyString(RetrofitError error) throws IOException
+    public static String errorToBodyString(@NotNull RetrofitError error) throws IOException
     {
         return streamToString(error.getResponse().getBody().in());
     }
