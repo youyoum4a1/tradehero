6f37d515d2550c5062570581f0f775f3440b43f6
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-09-10 17:44:04 +0800

    Fix crash on StockChartActivity

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/activities/StockChartActivity.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/activities/StockChartActivity.java
@@ -5,14 +5,24 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.tradehero.th.R;
+import com.tradehero.th.UIModule;
+import com.tradehero.th.base.THApp;
 import com.tradehero.th.fragments.security.ChartFragment;
+import com.tradehero.th.inject.Injector;
+import dagger.Module;
 
 public class StockChartActivity extends SherlockFragmentActivity
+    implements Injector
 {
+    private Injector newInjector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        THApp app = THApp.get(this);
+        newInjector = app.plus(new StockChartActivityModule());
+
         setContentView(R.layout.activity_stock_chart);
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -20,5 +30,21 @@ public class StockChartActivity extends SherlockFragmentActivity
         fragment.setArguments(getIntent().getExtras().getBundle(ChartFragment.BUNDLE_KEY_ARGUMENTS));
         fragmentTransaction.add(R.id.stock_chart, fragment);
         fragmentTransaction.commit();
+    }
+
+    @Override public void inject(Object o)
+    {
+        newInjector.inject(o);
+    }
+
+    @Module(
+            includes = {
+                    UIModule.class
+            },
+            library = true,
+            complete = false
+    )
+    public class StockChartActivityModule
+    {
     }
 }
