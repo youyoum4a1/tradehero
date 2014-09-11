1ca8d56d8e3515b5b3275dac9a41f60a6ed2e1fa
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-09-11 16:28:20 +0800

    Removed ask for review in buy sell.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/trade/BuySellFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/trade/BuySellFragment.java
@@ -23,7 +23,6 @@ import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
-import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.special.residemenu.ResideMenu;
@@ -32,7 +31,6 @@ import com.squareup.picasso.Picasso;
 import com.squareup.picasso.RequestCreator;
 import com.squareup.picasso.Transformation;
 import com.tradehero.common.persistence.DTOCacheNew;
-import com.tradehero.common.persistence.prefs.LongPreference;
 import com.tradehero.common.utils.THToast;
 import com.tradehero.route.Routable;
 import com.tradehero.th.R;
@@ -61,8 +59,6 @@ import com.tradehero.th.fragments.position.PositionListFragment;
 import com.tradehero.th.fragments.security.BuySellBottomStockPagerAdapter;
 import com.tradehero.th.fragments.security.StockInfoFragment;
 import com.tradehero.th.fragments.security.WatchlistEditFragment;
-import com.tradehero.th.fragments.settings.AskForInviteDialogFragment;
-import com.tradehero.th.fragments.settings.AskForReviewDialogFragment;
 import com.tradehero.th.fragments.social.SocialLinkHelper;
 import com.tradehero.th.fragments.social.SocialLinkHelperFactory;
 import com.tradehero.th.fragments.tutorial.WithTutorial;
@@ -81,8 +77,6 @@ import com.tradehero.th.network.service.SocialServiceWrapper;
 import com.tradehero.th.network.share.SocialSharer;
 import com.tradehero.th.persistence.portfolio.PortfolioCache;
 import com.tradehero.th.persistence.portfolio.PortfolioCompactCache;
-import com.tradehero.th.persistence.prefs.ShowAskForInviteDialog;
-import com.tradehero.th.persistence.prefs.ShowAskForReviewDialog;
 import com.tradehero.th.persistence.watchlist.UserWatchlistPositionCache;
 import com.tradehero.th.utils.AlertDialogUtil;
 import com.tradehero.th.utils.DateUtils;
@@ -124,8 +118,6 @@ public class BuySellFragment extends AbstractBuySellFragment
 
     @Inject ResideMenu resideMenu;
     @Inject Lazy<CurrentActivityHolder> currentActivityHolderLazy;
-    @Inject @ShowAskForReviewDialog LongPreference mShowAskForReviewDialogPreference;
-    @Inject @ShowAskForInviteDialog LongPreference mShowAskForInviteDialogPreference;
 
     //for dialog
     private PushPortfolioFragmentRunnable pushPortfolioFragmentRunnable = null;
@@ -1208,8 +1200,6 @@ public class BuySellFragment extends AbstractBuySellFragment
                         setActionBarSubtitle(null);
                         pushPortfolioFragmentRunnable.pushPortfolioFragment(securityPositionDetailDTO);
                     }
-
-                    showPrettyReviewAndInvite(isBuy);
                 }
 
                 @Override public void onTransactionFailed(boolean isBuy, THException error)
@@ -1225,32 +1215,6 @@ public class BuySellFragment extends AbstractBuySellFragment
                     R.string.buy_sell_no_quote_title,
                     R.string.buy_sell_no_quote_message,
                     R.string.buy_sell_no_quote_cancel);
-        }
-    }
-
-    private void showPrettyReviewAndInvite(boolean isBuy)
-    {
-        if (!isBuy)//only check after sell
-        {
-            Double profit = abstractTransactionDialogFragment.getProfitOrLossUsd();
-            if (profit != null)
-            {
-                SherlockFragmentActivity activity = (SherlockFragmentActivity) currentActivityHolderLazy.get().getCurrentActivity();
-                if (profit > 0 && activity != null)
-                {
-                    long lastReviewLimitTime = mShowAskForReviewDialogPreference.get();
-                    if (System.currentTimeMillis() > lastReviewLimitTime)
-                    {
-                        AskForReviewDialogFragment.showReviewDialog(activity.getSupportFragmentManager());
-                        return;
-                    }
-                    long lastInviteLimitTime = mShowAskForInviteDialogPreference.get();
-                    if (System.currentTimeMillis() > lastInviteLimitTime)
-                    {
-                        AskForInviteDialogFragment.showInviteDialog(activity.getSupportFragmentManager());
-                    }
-                }
-            }
         }
     }
 
