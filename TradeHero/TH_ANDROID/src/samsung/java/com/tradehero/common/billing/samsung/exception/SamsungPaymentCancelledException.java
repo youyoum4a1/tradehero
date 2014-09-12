b98dd53a703861fd8d1dc68fc4ad8a5e20cd0632
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-09-12 11:03:49 +0800

    Cleanup Samsung exceptions.

--- TradeHero/TH_ANDROID/src/samsung/java/com/tradehero/common/billing/samsung/exception/SamsungPaymentCancelledException.java
+++ TradeHero/TH_ANDROID/src/samsung/java/com/tradehero/common/billing/samsung/exception/SamsungPaymentCancelledException.java
@@ -7,24 +7,9 @@ public class SamsungPaymentCancelledException extends SamsungOneCodeException
     public static final int VALID_ERROR_CODE = SamsungIapHelper.IAP_PAYMENT_IS_CANCELED;
 
     //<editor-fold desc="Constructors">
-    public SamsungPaymentCancelledException()
-    {
-        super(VALID_ERROR_CODE);
-    }
-
     public SamsungPaymentCancelledException(String message)
     {
         super(VALID_ERROR_CODE, message);
-    }
-
-    public SamsungPaymentCancelledException(String message, Exception cause)
-    {
-        super(VALID_ERROR_CODE, message, cause);
-    }
-
-    public SamsungPaymentCancelledException(Exception cause)
-    {
-        super(VALID_ERROR_CODE, cause);
     }
     //</editor-fold>
 
