13576d811e7d46e1dd8b12fb94af23ad78977184
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-09-19 13:10:34 +0800

    Added example dex script call.

--- scripts/apktool/example.sh
+++ scripts/apktool/example.sh
@@ -2,4 +2,7 @@
 ./baksmali /Users/xavier/Documents/Projects/TH_ANDROID2/TradeHero/TH_ANDROID/build/outputs/apk/app-amazon-debug.apk -o /Users/xavier/Documents/Temp/apktool/amazon2
 
 # Example to count methods 
-find /Users/xavier/Documents/Temp/apktool/amazon2/  -type d -print -exec sh -c "./smali {} -o {}/classes.dex && sh -c \"/Users/xavier/Documents/Projects/adt-bundle-mac-x86_64-20131030/sdk/build-tools/20.0.0/dexdump -f {}/classes.dex | grep method_ids_size\"" \; > method_count_amazon_2.txt 
+find /Users/xavier/Documents/Temp/apktool/amazon2/ -type d -print -exec sh -c "./smali {} -o {}/classes.dex && sh -c \"/Users/xavier/Documents/Projects/adt-bundle-mac-x86_64-20131030/sdk/build-tools/20.0.0/dexdump -f {}/classes.dex | grep method_ids_size\"" \; > method_count_amazon_2.txt 
+
+# Example to dex classes per folder, 3 folders at a time
+find /Users/xavier/Documents/Temp/apktool/samsungcut/ -type d -print | xargs -P 3 -I {} ./smali {} -o {}/classes.dex
