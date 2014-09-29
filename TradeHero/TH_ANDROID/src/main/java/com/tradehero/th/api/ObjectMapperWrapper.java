41e336156d9dc95d7eacf65bc6a67400bd008975
Malvin <malvinsutanto@gmail.com>
2014-09-29 16:14:59 +0800

    Remove cache validation since it's not needed

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/ObjectMapperWrapper.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/ObjectMapperWrapper.java
@@ -119,10 +119,6 @@ public class ObjectMapperWrapper extends ObjectMapper
             userAchievementCacheLazy.get().putNonDefDuplicates(userAchievementDTOs);
             UserBaseKey userBaseKey = currentUserIdLazy.get().toUserBaseKey();
             achievementCategoryListCacheLazy.get().invalidate(userBaseKey);
-            for (UserAchievementDTO userAchievementDTO : userAchievementDTOs)
-            {
-                achievementCategoryCacheLazy.get().invalidate(new AchievementCategoryId(userBaseKey, userAchievementDTO.achievementDef.categoryId));
-            }
             userProfileCacheLazy.get().updateXPIfNecessary(userBaseKey, userAchievementDTOs.findBiggestXPTotal());
             userProfileCacheLazy.get().addAchievements(userBaseKey, userAchievementDTOs.size());
         }
