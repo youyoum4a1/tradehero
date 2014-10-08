c6e4c1e8c24a4b95edc696b2a173ac6f47894089
haiping.hu <windy911@163.com>
2014-10-08 17:45:12 +0800

    fixed bugs

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/chinabuild/fragment/trade/TradeOfRisePercentFragment.java
@@ -0,0 +1,12 @@
+package com.tradehero.th.fragments.chinabuild.fragment.trade;
+
+import com.tradehero.th.api.security.key.TrendingAllSecurityListType;
+
+public class TradeOfRisePercentFragment extends TradeOfTypeBaseFragment
+{
+
+    public int getTradeType()
+    {
+        return TrendingAllSecurityListType.ALL_SECURITY_LIST_TYPE_RISE_PERCENT;
+    }
+}
