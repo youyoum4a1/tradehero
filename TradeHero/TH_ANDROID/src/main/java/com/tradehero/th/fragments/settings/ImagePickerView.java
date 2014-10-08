bf30b0a23e48ce6a079dfd197ffe53c36567e204
Malvin <malvinsutanto@gmail.com>
2014-10-08 17:33:41 +0800

    Prevent crash when device has no camera

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/settings/ImagePickerView.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/settings/ImagePickerView.java
@@ -3,13 +3,17 @@ package com.tradehero.th.fragments.settings;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.tradehero.common.utils.THToast;
 import com.tradehero.th.R;
 import com.tradehero.th.inject.HierarchyInjector;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 import javax.inject.Inject;
 
@@ -35,9 +39,21 @@ public class ImagePickerView extends LinearLayout
 
     @OnClick(R.id.request_image_from_camera) void onImageFromCameraRequested()
     {
+        PackageManager pm = activity
+                .getApplicationContext()
+                .getPackageManager();
         Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        //cameraIntent.setType("image/jpeg");
-        activity.startActivityForResult(cameraIntent, REQUEST_CAMERA);
+
+        List<ResolveInfo> handlerActivities = pm.queryIntentActivities(cameraIntent, 0);
+        if (handlerActivities.size() > 0)
+        {
+            //cameraIntent.setType("image/jpeg");
+            activity.startActivityForResult(cameraIntent, REQUEST_CAMERA);
+        }
+        else
+        {
+            THToast.show(R.string.device_no_camera);
+        }
     }
 
     @OnClick(R.id.request_image_from_library) void onImageFromLibraryRequested()
