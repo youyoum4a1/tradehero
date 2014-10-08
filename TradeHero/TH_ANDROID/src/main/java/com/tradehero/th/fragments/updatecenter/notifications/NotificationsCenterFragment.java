83ae1544e442990bf65cc6ffe357ff0dfdadcc47
Malvin <malvinsutanto@gmail.com>
2014-10-08 15:46:39 +0800

    Fix crash when going into notifications lists

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/updatecenter/notifications/NotificationsCenterFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/updatecenter/notifications/NotificationsCenterFragment.java
@@ -1,5 +1,6 @@
 package com.tradehero.th.fragments.updatecenter.notifications;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,10 +8,13 @@ import android.view.ViewGroup;
 import com.tradehero.route.Routable;
 import com.tradehero.th.R;
 import com.tradehero.th.fragments.base.DashboardFragment;
+import javax.inject.Inject;
 
 @Routable("notifications")
 public class NotificationsCenterFragment extends DashboardFragment
 {
+    @Inject Context doNotRemoveOrFails;
+
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         return inflater.inflate(R.layout.notifications_center, container, false);
