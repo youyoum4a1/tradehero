9449fa12c012e50ce36bb29a672b7cc85441b4cb
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-10-09 19:06:14 +0800

    Moved shortcut creation into China build.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/activities/GuideActivity.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/activities/GuideActivity.java
@@ -1,13 +1,7 @@
 package com.tradehero.th.activities;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -16,9 +10,9 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import com.tradehero.th.BuildConfig;
 import com.tradehero.th.R;
 import com.tradehero.th.utils.DaggerUtils;
+import com.tradehero.th.utils.ShortcutUtil;
 import com.tradehero.th.utils.metrics.Analytics;
 import com.tradehero.th.utils.metrics.AnalyticsConstants;
 import com.tradehero.th.utils.metrics.events.MethodEvent;
@@ -47,24 +41,11 @@ public class GuideActivity extends Activity
         list.add(R.drawable.guide1);
         list.add(R.drawable.guide2);
         list.add(R.drawable.guide3);
-        //list.add(R.drawable.guide4);
-        //list.add(R.drawable.guide5);
 
         viewpager.setAdapter(new ListViewPagerAdapter(list));
         viewpager.setOnPageChangeListener(this);
 
-        try
-        {
-            if (isInstallShortcut())
-            {
-                removeShortcut();
-            }
-            createShortcut();
-        }
-        catch (SecurityException e)
-        {
-            Timber.e(e, null);
-        }
+        ShortcutUtil.recreateShortcut(this);
 
         analytics.openSession();
         analytics.tagScreen(AnalyticsConstants.Splash);
@@ -76,111 +57,11 @@ public class GuideActivity extends Activity
         super.onPause();
     }
 
-    private void createShortcut()
-    {
-        Intent intent = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
-        intent.putExtra("duplicate", false);
-        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.app_name));
-        Parcelable icon = Intent.ShortcutIconResource.fromContext(getApplicationContext(), R.drawable.launcher);
-        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
-        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, createLaunchIntent());
-        sendBroadcast(intent);
-    }
-
-    private Intent createLaunchIntent()
-    {
-        Intent launchIntent = new Intent(Intent.ACTION_MAIN);
-        launchIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        launchIntent.setComponent(new ComponentName(getPackageName(), AuthenticationActivity.class.getName()));
-        launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-        return launchIntent;
-    }
-
-    private void printShortcutName(Cursor c)
-    {
-        if (!BuildConfig.DEBUG)
-        {
-            return;
-        }
-
-        if (c == null || c.getCount() <= 0)
-        {
-            return;
-        }
-
-        String name = "title";
-        c.moveToFirst();
-        int index = c.getColumnIndex(name);
-        if (index == -1)
-        {
-            return;
-        }
-        c.moveToPrevious();
-        while (c.moveToNext())
-        {
-            String title = c.getString(index);
-            Timber.d("Shortcut title:%s",title);
-        }
-    }
-
-    private boolean isInstallShortcut()
-    {
-        String name = getString(R.string.app_name);
-        boolean  isInstallShortcut = false;
-        final ContentResolver cr = getContentResolver();
-        String AUTHORITY = "com.android.launcher.settings";
-        Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/favorites?notify=true");
-
-        Cursor c = cr.query(CONTENT_URI, new String[] { "title", "iconResource" },
-                "title=?", new String[]{name}, null);
-
-        if (c != null && c.getCount() > 0)
-        {
-            isInstallShortcut = true;
-        }
-        printShortcutName(c);
-
-        if (c != null)
-        {
-            c.close();
-        }
-
-        if (isInstallShortcut)
-        {
-            return isInstallShortcut;
-        }
-
-        AUTHORITY = "com.android.launcher2.settings";
-        CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/favorites?notify=true");
-        c = cr.query(CONTENT_URI, new String[] { "title", "iconResource" },
-                "title=?", new String[]{name}, null);
-
-        if (c != null && c.getCount() > 0) {
-            isInstallShortcut = true;
-        }
-        printShortcutName(c);
-
-        return isInstallShortcut;
-    }
-
-    private void removeShortcut()
-    {
-
-        Intent shortcutIntent = new Intent(getApplicationContext(), AuthenticationActivity.class);
-        shortcutIntent.setAction(Intent.ACTION_MAIN);
-
-        Intent addIntent = new Intent();
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-        addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.app_name));
-
-        addIntent.setAction("com.android.launcher.action.UNINSTALL_SHORTCUT");
-        getApplicationContext().sendBroadcast(addIntent);
-    }
-
     @Override public void onClick(View v)
     {
         analytics.addEvent(new SimpleEvent(AnalyticsConstants.SplashScreenCancel));
         ActivityHelper.launchAuthentication(this);
+        finish();
     }
 
     class ListViewPagerAdapter extends PagerAdapter
