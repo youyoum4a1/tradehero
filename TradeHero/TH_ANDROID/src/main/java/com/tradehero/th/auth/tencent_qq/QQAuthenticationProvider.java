494ed9691e6873fbac9a74f90797b0b08316f452
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-10-10 18:35:03 +0800

    Added implementation to connect with QQ.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/auth/tencent_qq/QQAuthenticationProvider.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/auth/tencent_qq/QQAuthenticationProvider.java
@@ -2,108 +2,56 @@ package com.tradehero.th.auth.tencent_qq;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import com.tencent.tauth.IUiListener;
 import com.tencent.tauth.Tencent;
-import com.tencent.tauth.UiError;
+import com.tradehero.th.api.social.SocialNetworkEnum;
 import com.tradehero.th.auth.AuthData;
 import com.tradehero.th.auth.SocialAuthenticationProvider;
 import com.tradehero.th.network.service.SocialLinker;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONException;
-import org.json.JSONObject;
 import rx.Observable;
-import timber.log.Timber;
+import rx.functions.Func1;
 
 @Singleton
 public class QQAuthenticationProvider extends SocialAuthenticationProvider
 {
     private static final String SCOPE = "all";
-    private final Tencent mTencent;
-    private QQAppAuthData mAccessToken;
-
-    public static final String KEY_ACCESS_TOKEN = "qq_access_token";
-    public static final String KEY_OPEN_ID = "qq_openid";
+    @NotNull private final Context context;
+    @NotNull private final Tencent tencent;
 
     //<editor-fold desc="Constructors">
-    @Inject public QQAuthenticationProvider(@NotNull SocialLinker socialLinker, Tencent tencent)
+    @Inject public QQAuthenticationProvider(
+            @NotNull SocialLinker socialLinker,
+            @NotNull Context context,
+            @NotNull Tencent tencent)
     {
         super(socialLinker);
-        this.mTencent = tencent;
+        this.context = context;
+        this.tencent = tencent;
     }
     //</editor-fold>
 
-    private boolean checkContext()
-    {
-        if (baseContext != null && baseContext.get() != null)
-        {
-            Context ctx = baseContext.get();
-            if (ctx instanceof Activity)
-            {
-                return !((Activity) ctx).isFinishing();
-            }
-        }
-        return false;
-    }
-
-    private void onAuthorizeCancel()
-    {
-        handleCancel(currentOperationCallback);
-        currentOperationCallback = null;
-    }
-
-    public void authorizeCallBack(int requestCode, int resultCode, Intent data)
-    {
-        Timber.d("windy requestCode = " + requestCode + "resultCode = " + resultCode + "data = " + data.toString());
-    }
-
     @Override public void logout()
     {
-        throw new RuntimeException("Not implemented");
-    }
-
-    private class BaseUiListener implements IUiListener
-    {
-
-        protected void doComplete(JSONObject values)
-        {
-        }
-
-        @Override
-        public void onComplete(Object response)
-        {
-            Timber.d("windy BaseUiListener on Complete:" + response.toString());
-            JSONObject jsonObject;
-            try
-            {
-                jsonObject = new JSONObject(response.toString());
-                doComplete(jsonObject);
-            } catch (JSONException e)
-            {
-                Timber.e("QQ BaseUiListener " + e.toString());
-            }
-        }
-
-        @Override
-        public void onError(UiError e)
-        {
-
-            Timber.e("windy onError:" + "code:" + e.errorCode + ", msg:"
-                    + e.errorMessage + ", detail:" + e.errorDetail);
-            onAuthorizeCancel();
-        }
-
-        @Override
-        public void onCancel()
-        {
-            onAuthorizeCancel();
-        }
+        tencent.logout(context);
     }
 
     @Override protected Observable<AuthData> createAuthDataObservable(Activity activity)
     {
-        throw new RuntimeException("Not implemented");
+        tencent.logout(activity);
+
+        return Observable.create(new OperatorTencent(tencent, activity, SCOPE))
+            .map(new Func1<QQAppAuthData, AuthData>()
+            {
+                @Override public AuthData call(@NotNull QQAppAuthData qqAppAuthData)
+                {
+                    return new AuthData(
+                            SocialNetworkEnum.QQ,
+                            null, // FIXME TODO expiration
+                            qqAppAuthData.accessToken,
+                            qqAppAuthData.openId);
+                }
+            });
     }
 }
---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/auth/tencent_qq/QQCancelException.java
@@ -0,0 +1,5 @@
+package com.tradehero.th.auth.tencent_qq;
+
+public class QQCancelException extends RuntimeException
+{
+}
