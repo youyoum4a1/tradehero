8c5273449e5c71b0924c0f52c9397d325aa3f101
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-10-10 15:48:40 +0800

    Prevent crash Dagger / Proguard bug.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/social/friend/SocialFriendsFragmentTwitter.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/social/friend/SocialFriendsFragmentTwitter.java
@@ -1,10 +1,14 @@
 package com.tradehero.th.fragments.social.friend;
 
+import android.content.Context;
 import com.tradehero.th.R;
 import com.tradehero.th.api.social.SocialNetworkEnum;
+import javax.inject.Inject;
 
 public class SocialFriendsFragmentTwitter extends SocialFriendsFragment
 {
+    @SuppressWarnings("UnusedDeclaration") @Inject Context doNotRemoveOrItFails;
+
     @Override
     protected SocialNetworkEnum getSocialNetwork()
     {
