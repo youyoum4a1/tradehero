75b1c6d611a66ebbbaaedb7152091cca758658d1
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-10-16 16:58:50 +0800

    Remove some unused constants

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/Constants.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/Constants.java
@@ -20,14 +20,6 @@ public class Constants
 
     public static final int LEADERBOARD_MARK_USER_ITEM_PER_PAGE = COMMON_ITEM_PER_PAGE;
 
-    public static final String FACEBOOK_PROFILE_PICTURE = "http://graph.facebook.com/%s/picture?type=large";
-
-    // TestFlightApp
-    public static final boolean TEST_FLIGHT_ENABLED = true;
-    public static final String TEST_FLIGHT_TOKEN = "a8a266bb-500c-4cdf-a5b0-f9c5bd6ad995";
-    public static final boolean TEST_FLIGHT_REPORT_CHECKPOINT = true;
-    public static final boolean TEST_FLIGHT_REPORT_LOG = true;
-
     // this constant is dedicated for static content page (html, image, cdn that
     // may be needed later, for Api endpoint, refer to retrofit module, we want to make it
     // generic and easy to switch between endpoint (prod, dev, test server) as much as possible.
