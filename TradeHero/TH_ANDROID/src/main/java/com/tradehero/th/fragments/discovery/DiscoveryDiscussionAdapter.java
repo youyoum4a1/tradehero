6daee8809d6f1174223c3c2090f2213e97953b94
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-10-21 11:16:31 +0800

    Use the new ArrayDTOAdapter

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discovery/DiscoveryDiscussionAdapter.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discovery/DiscoveryDiscussionAdapter.java
@@ -2,19 +2,19 @@ package com.tradehero.th.fragments.discovery;
 
 import android.content.Context;
 import android.support.annotation.LayoutRes;
-import com.tradehero.th.adapters.DTOAdapterNew;
+import com.tradehero.th.adapters.ArrayDTOAdapter;
 import com.tradehero.th.api.timeline.key.TimelineItemDTOKey;
+import com.tradehero.th.fragments.timeline.TimelineItemViewLinear;
 import org.jetbrains.annotations.NotNull;
 
-public class DiscoveryDiscussionAdapter extends DTOAdapterNew<TimelineItemDTOKey>
+public class DiscoveryDiscussionAdapter extends ArrayDTOAdapter<TimelineItemDTOKey, TimelineItemViewLinear>
 {
     public DiscoveryDiscussionAdapter(@NotNull Context context, @LayoutRes int layoutResourceId)
     {
         super(context, layoutResourceId);
     }
 
-    public void replaceWith(TimelineItemDTOKey timelineItemDTOKey)
+    @Override protected void fineTune(int position, TimelineItemDTOKey dto, TimelineItemViewLinear dtoView)
     {
-        clear();
     }
 }
