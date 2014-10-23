7e6492d3143eac15fee1f46de7427c8525fd587c
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-10-23 17:57:33 +0800

    Introducing cacheRx.

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/common/persistence/BaseFetchDTOCacheRx.java
@@ -0,0 +1,83 @@
+package com.tradehero.common.persistence;
+
+import android.util.Pair;
+import java.util.Collection;
+import org.jetbrains.annotations.NotNull;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
+import rx.functions.Func1;
+import rx.subjects.BehaviorSubject;
+
+abstract public class BaseFetchDTOCacheRx<DTOKeyType extends DTOKey, DTOType extends DTO>
+        extends BaseDTOCacheRx<DTOKeyType, DTOType>
+{
+    @NotNull final private THLruCache<DTOKeyType, Subscription> cachedFetcherSubscriptions;
+
+    //<editor-fold desc="Constructors">
+    protected BaseFetchDTOCacheRx(int valueSize, int subjectSize, int fetcherSize)
+    {
+        super(valueSize, subjectSize);
+        this.cachedFetcherSubscriptions = new THLruCache<>(fetcherSize);
+    }
+    //</editor-fold>
+
+    @NotNull abstract protected Observable<DTOType> fetch(@NotNull DTOKeyType key);
+
+    @NotNull @Override
+    protected BehaviorSubject<Pair<DTOKeyType, DTOType>> getOrCreateBehavior(@NotNull final DTOKeyType key)
+    {
+        BehaviorSubject<Pair<DTOKeyType, DTOType>> cachedSubject = super.getOrCreateBehavior(key);
+        if (cachedFetcherSubscriptions.get(key) == null)
+        {
+            fetch(key)
+                    .map(new Func1<DTOType, Pair<DTOKeyType, DTOType>>()
+                    {
+                        @Override public Pair<DTOKeyType, DTOType> call(DTOType dtoType)
+                        {
+                            return Pair.create(key, dtoType);
+                        }
+                    })
+                    .subscribe(new Observer<Pair<DTOKeyType,DTOType>>()
+                    {
+                        @Override public void onNext(Pair<DTOKeyType, DTOType> pair)
+                        {
+                            BaseFetchDTOCacheRx.this.onNext(pair.first, pair.second);
+                        }
+
+                        @Override public void onCompleted()
+                        {
+                            cachedFetcherSubscriptions.remove(key);
+                        }
+
+                        @Override public void onError(Throwable error)
+                        {
+                            cachedFetcherSubscriptions.remove(key);
+                            BaseFetchDTOCacheRx.this.onError(key, error);
+                        }
+                    });
+        }
+        return cachedSubject;
+    }
+
+    @Override public void invalidate(@NotNull DTOKeyType key)
+    {
+        super.invalidate(key);
+        Subscription subscription = cachedFetcherSubscriptions.remove(key);
+        if (subscription != null)
+        {
+            subscription.unsubscribe();
+        }
+    }
+
+    @Override public void invalidateAll()
+    {
+        super.invalidateAll();
+        Collection<Subscription> fetcherSubscriptions = cachedFetcherSubscriptions.snapshot().values();
+        cachedFetcherSubscriptions.evictAll();
+        for (Subscription subscription : fetcherSubscriptions)
+        {
+            subscription.unsubscribe();
+        }
+    }
+}
