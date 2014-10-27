5e7815f688e90d90302ebce5924a1868d0095a1d
Tho Nguyen <nguyentruongtho.sg@gmail.com>
2014-10-27 13:13:15 +0800

    Display user name and profile picture on timeline item view

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/timeline/TimelineDTOProcessor.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/models/timeline/TimelineDTOProcessor.java
@@ -1,7 +1,10 @@
 package com.tradehero.th.models.timeline;
 
+import com.android.internal.util.Predicate;
+import com.tradehero.common.utils.CollectionUtils;
 import com.tradehero.th.api.timeline.TimelineDTO;
 import com.tradehero.th.api.timeline.TimelineItemDTO;
+import com.tradehero.th.api.users.UserProfileCompactDTO;
 import com.tradehero.th.persistence.discussion.DiscussionCache;
 import java.util.List;
 import javax.inject.Inject;
@@ -25,8 +28,15 @@ public class TimelineDTOProcessor implements Action1<TimelineDTO>
         List<TimelineItemDTO> timelineItemList = timelineDTO.getEnhancedItems();
         if (timelineItemList != null)
         {
-            for (TimelineItemDTO timelineItemDTO: timelineItemList)
+            for (final TimelineItemDTO timelineItemDTO: timelineItemList)
             {
+                timelineItemDTO.setUser(CollectionUtils.first(timelineDTO.getUsers(), new Predicate<UserProfileCompactDTO>()
+                {
+                    @Override public boolean apply(UserProfileCompactDTO userProfileCompactDTO)
+                    {
+                        return timelineItemDTO.userId == userProfileCompactDTO.id;
+                    }
+                }));
                 discussionCache.put(timelineItemDTO.getDiscussionKey(), timelineItemDTO);
             }
         }
