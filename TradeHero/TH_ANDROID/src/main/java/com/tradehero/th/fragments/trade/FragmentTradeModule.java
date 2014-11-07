45b0cdea5801d7293abec41dd6848f7951675c21
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-11-07 18:40:01 +0800

    Simplified FreshQuote holder. Used animation for progress bar.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/trade/FragmentTradeModule.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/trade/FragmentTradeModule.java
@@ -1,13 +1,9 @@
 package com.tradehero.th.fragments.trade;
 
-import com.tradehero.th.fragments.trade.quote.FragmentTradeQuoteModule;
 import com.tradehero.th.fragments.trade.view.TradeListItemView;
 import dagger.Module;
 
 @Module(
-        includes = {
-                FragmentTradeQuoteModule.class
-        },
         injects = {
                 BuySellFragment.class,
                 AbstractTransactionDialogFragment.class,
