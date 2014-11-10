29a65ded256f2d05aa15c11c0fdcde457ef0f3e9
Malvin <malvinsutanto@gmail.com>
2014-11-10 18:32:08 +0800

    Fix dialog being stretched in 7" tablets

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/competition/CompetitionPreseasonDialogFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/competition/CompetitionPreseasonDialogFragment.java
@@ -1,26 +1,25 @@
 package com.tradehero.th.fragments.competition;
 
-import android.content.Context;
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ViewFlipper;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import com.squareup.picasso.Picasso;
 import com.tradehero.th.R;
 import com.tradehero.th.api.competition.ProviderDTO;
 import com.tradehero.th.api.competition.ProviderId;
-import com.tradehero.th.api.users.CurrentUserId;
 import com.tradehero.th.fragments.base.BaseShareableDialogFragment;
 import com.tradehero.th.persistence.competition.ProviderCacheRx;
 import com.tradehero.th.widget.MarkdownTextView;
 import javax.inject.Inject;
-import rx.Observer;
+import rx.Subscriber;
 import rx.android.observables.AndroidObservable;
 import timber.log.Timber;
 
@@ -29,17 +28,19 @@ public class CompetitionPreseasonDialogFragment extends BaseShareableDialogFragm
     public static final String TAG = CompetitionPreseasonDialogFragment.class.getName();
 
     private static final String KEY_PROVIDER_ID = CompetitionPreseasonDialogFragment.class.getName() + ".providerId";
+    private static final int CONTENT_VIEW_INDEX = 0;
+    private static final int LOADING_VIEW_INDEX = 1;
 
     @Inject Picasso picasso;
     @Inject ProviderCacheRx providerCacheRx;
 
-    @InjectView(R.id.preseason_share) Button btnShare;
+    @InjectView(R.id.preseason_viewflipper) ViewFlipper viewFlipper;
     @InjectView(R.id.preseason_title_image) ImageView imgTitle;
     @InjectView(R.id.preseason_prize_image) ImageView imgPrize;
     @InjectView(R.id.preseason_prize_description) MarkdownTextView textDescription;
-    @InjectView(R.id.preseason_prize_tncs) Button btnTncs;
 
     private ProviderId providerId;
+    private ProviderDTO providerDTO;
 
     public static CompetitionPreseasonDialogFragment newInstance(ProviderId providerId)
     {
@@ -52,6 +53,7 @@ public class CompetitionPreseasonDialogFragment extends BaseShareableDialogFragm
 
     @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+        setStyle(DialogFragment.STYLE_NO_FRAME, R.style.TH_Dialog);
         return inflater.inflate(R.layout.competition_preseason_dialog, container, false);
     }
 
@@ -59,6 +61,34 @@ public class CompetitionPreseasonDialogFragment extends BaseShareableDialogFragm
     public void onCloseClicked()
     {
         getDialog().dismiss();
+    }
+
+    @OnClick(R.id.preseason_share)
+    public void onShareClicked()
+    {
+        if (getEnabledSharePreferences().isEmpty())
+        {
+            alertDialogUtil.popWithNegativeButton(
+                    getActivity(),
+                    R.string.link_select_one_social,
+                    R.string.link_select_one_social_description,
+                    R.string.ok);
+        }
+        else if (providerDTO == null)
+        {
+            share();
+        }
+    }
+
+    private void share()
+    {
+        //TODO
+    }
+
+    @OnClick(R.id.preseason_prize_tncs)
+    public void onTncsClicked()
+    {
+        //TODO
     }
 
     private void getProviderIdFromArgs()
@@ -76,8 +106,14 @@ public class CompetitionPreseasonDialogFragment extends BaseShareableDialogFragm
     {
         super.onViewCreated(view, savedInstanceState);
         AndroidObservable.bindFragment(this, providerCacheRx.get(providerId))
-                .subscribe(new Observer<Pair<ProviderId, ProviderDTO>>()
+                .subscribe(new Subscriber<Pair<ProviderId, ProviderDTO>>()
                 {
+                    @Override public void onStart()
+                    {
+                        super.onStart();
+                        showLoadingDialog();
+                    }
+
                     @Override public void onCompleted()
                     {
 
@@ -90,13 +126,25 @@ public class CompetitionPreseasonDialogFragment extends BaseShareableDialogFragm
 
                     @Override public void onNext(Pair<ProviderId, ProviderDTO> providerIdProviderDTOPair)
                     {
-                        initView(providerIdProviderDTOPair.second);
+                        linkAndInitViews(providerIdProviderDTOPair.second);
                     }
                 });
     }
 
-    private void initView(ProviderDTO providerDTO)
+    private void showLoadingDialog()
     {
+        viewFlipper.setDisplayedChild(LOADING_VIEW_INDEX);
+    }
+
+    private void showContent()
+    {
+        viewFlipper.setDisplayedChild(CONTENT_VIEW_INDEX);
+    }
+
+    private void linkAndInitViews(ProviderDTO providerDTO)
+    {
+        this.providerDTO = providerDTO;
+        showContent();
         if (providerDTO.navigationLogoUrl != null)
         {
             picasso.load(providerDTO.navigationLogoUrl).into(imgTitle);
