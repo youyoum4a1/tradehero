e66fdd9da91a6911471f14601c0da4bb45904a88
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-11-19 10:50:06 +0800

    Added options to AlertDialog operator.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/rx/dialog/AlertDialogOnSubscribe.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/rx/dialog/AlertDialogOnSubscribe.java
@@ -2,9 +2,11 @@ package com.tradehero.th.rx.dialog;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.util.Pair;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.util.Pair;
+import android.widget.ListAdapter;
 import rx.Observable;
 import rx.Subscriber;
 
@@ -21,33 +23,60 @@ public class AlertDialogOnSubscribe implements Observable.OnSubscribe<Pair<Dialo
 
     @Override public void call(final Subscriber<? super Pair<DialogInterface, Integer>> subscriber)
     {
-        builder.alertDialogBuilder.setPositiveButton(
-                builder.positiveButton,
-                new DialogInterface.OnClickListener()
-                {
-                    @Override public void onClick(DialogInterface dialogInterface, int i)
-                    {
-                        subscriber.onNext(Pair.create(dialogInterface, i));
-                    }
-                });
-        builder.alertDialogBuilder.setNegativeButton(
-                builder.negativeButton,
-                new DialogInterface.OnClickListener()
-                {
-                    @Override public void onClick(DialogInterface dialogInterface, int i)
-                    {
-                        subscriber.onNext(Pair.create(dialogInterface, i));
-                    }
-                });
-        builder.alertDialogBuilder.setNeutralButton(
-                builder.neutralButton,
-                new DialogInterface.OnClickListener()
-                {
-                    @Override public void onClick(DialogInterface dialogInterface, int i)
-                    {
-                        subscriber.onNext(Pair.create(dialogInterface, i));
-                    }
-                });
+        DialogInterface.OnClickListener passingOnListener = new DialogInterface.OnClickListener()
+        {
+            @Override public void onClick(DialogInterface dialogInterface, int i)
+            {
+                subscriber.onNext(Pair.create(dialogInterface, i));
+            }
+        };
+        if (builder.positiveButtonRes != null)
+        {
+            builder.alertDialogBuilder.setPositiveButton(
+                    builder.positiveButtonRes,
+                    passingOnListener);
+        }
+        else
+        {
+            builder.alertDialogBuilder.setPositiveButton(
+                    builder.positiveButton,
+                    passingOnListener);
+        }
+
+        if (builder.negativeButtonRes != null)
+        {
+            builder.alertDialogBuilder.setNegativeButton(
+                    builder.negativeButtonRes,
+                    passingOnListener);
+        }
+        else
+        {
+            builder.alertDialogBuilder.setNegativeButton(
+                    builder.negativeButton,
+                    passingOnListener);
+        }
+
+        if (builder.neutralButtonRes != null)
+        {
+            builder.alertDialogBuilder.setNeutralButton(
+                    builder.neutralButtonRes,
+                    passingOnListener);
+        }
+        else
+        {
+            builder.alertDialogBuilder.setNeutralButton(
+                    builder.neutralButton,
+                    passingOnListener);
+        }
+
+        if (builder.singleChoicedapter != null)
+        {
+            builder.alertDialogBuilder.setSingleChoiceItems(
+                    builder.singleChoicedapter,
+                    builder.singleChoiceCheckedItem,
+                    passingOnListener);
+        }
+
         AlertDialog dialog = builder.alertDialogBuilder.create();
         dialog.setOnDismissListener(new DialogInterface.OnDismissListener()
         {
@@ -56,7 +85,14 @@ public class AlertDialogOnSubscribe implements Observable.OnSubscribe<Pair<Dialo
                 subscriber.onCompleted();
             }
         });
-        dialog.show();
+        try
+        {
+            dialog.show();
+            dialog.setCanceledOnTouchOutside(builder.canceledOnTouchOutside);
+        } catch (Throwable e)
+        {
+            subscriber.onError(e);
+        }
     }
 
     public static Builder builder(@NonNull AlertDialog.Builder alertDialogBuilder)
@@ -67,8 +103,15 @@ public class AlertDialogOnSubscribe implements Observable.OnSubscribe<Pair<Dialo
     public static class Builder
     {
         @Nullable private CharSequence positiveButton;
+        @Nullable @StringRes private Integer positiveButtonRes;
         @Nullable private CharSequence negativeButton;
+        @Nullable @StringRes private Integer negativeButtonRes;
         @Nullable private CharSequence neutralButton;
+        @Nullable @StringRes private Integer neutralButtonRes;
+        private boolean canceledOnTouchOutside;
+        @Nullable private ListAdapter singleChoicedapter;
+        private int singleChoiceCheckedItem;
+
         @NonNull private final AlertDialog.Builder alertDialogBuilder;
 
         public Builder(@NonNull AlertDialog.Builder alertDialogBuilder)
@@ -82,15 +125,46 @@ public class AlertDialogOnSubscribe implements Observable.OnSubscribe<Pair<Dialo
             return this;
         }
 
+        public Builder setPositiveButton(int positiveButton)
+        {
+            this.positiveButtonRes = positiveButton;
+            return this;
+        }
+
         public Builder setNegativeButton(@Nullable CharSequence negativeButton)
         {
             this.negativeButton = negativeButton;
             return this;
         }
 
+        public Builder setNegativeButton(int negativeButton)
+        {
+            this.negativeButtonRes = negativeButton;
+            return this;
+        }
+
         public Builder setNeutralButton(@Nullable CharSequence neutralButton)
         {
             this.neutralButton = neutralButton;
+            return this;
+        }
+
+        public Builder setNeutralButton(int neutralButton)
+        {
+            this.neutralButtonRes = neutralButton;
+            return this;
+        }
+
+        public Builder setCanceledOnTouchOutside(boolean canceledOnTouchOutside)
+        {
+            this.canceledOnTouchOutside = canceledOnTouchOutside;
+            return this;
+        }
+
+        public Builder setSingleChoiceItems(@NonNull ListAdapter adapter, int checkedItem)
+        {
+            this.singleChoicedapter = adapter;
+            this.singleChoiceCheckedItem = checkedItem;
             return this;
         }
 
