454aecf9345a75875398c0adad8a25739b79f737
Malvin <malvinsutanto@gmail.com>
2014-12-01 17:46:19 +0800

    Remove missing method reference

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/BasePagedListRxFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/BasePagedListRxFragment.java
@@ -89,7 +89,6 @@ abstract public class BasePagedListRxFragment<
         listView.setOnScrollListener(createListViewScrollListener());
         listView.setEmptyView(emptyContainer);
         listView.setAdapter(itemViewAdapter);
-        initViews(view);
     }
 
     @NonNull protected AbsListView.OnScrollListener createListViewScrollListener()
