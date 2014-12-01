6beb2cb8ed4202406482515898d2aaf98f86fac6
Malvin <malvinsutanto@gmail.com>
2014-12-01 14:08:14 +0800

    Fix code styles for new method

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/StringUtils.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/StringUtils.java
@@ -61,13 +61,16 @@ public class StringUtils
         return str == null || str.trim().isEmpty();
     }
 
-    public static String removeImageSpanObjects(@NonNull String inStr) {
+    public static String removeImageSpanObjects(@NonNull String inStr)
+    {
         SpannableStringBuilder spannedStr = (SpannableStringBuilder) Html
                 .fromHtml(inStr.trim());
         Object[] spannedObjects = spannedStr.getSpans(0, spannedStr.length(),
                 Object.class);
-        for (int i = 0; i < spannedObjects.length; i++) {
-            if (spannedObjects[i] instanceof ImageSpan) {
+        for (int i = 0; i < spannedObjects.length; i++)
+        {
+            if (spannedObjects[i] instanceof ImageSpan)
+            {
                 ImageSpan imageSpan = (ImageSpan) spannedObjects[i];
                 spannedStr.replace(spannedStr.getSpanStart(imageSpan),
                         spannedStr.getSpanEnd(imageSpan), "");
