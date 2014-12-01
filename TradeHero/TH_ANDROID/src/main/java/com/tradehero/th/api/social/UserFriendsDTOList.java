72099382dcaa901c569d8b385dfbdbcf99160846
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-12-01 11:54:57 +0800

    Convenience methods. Cleanup

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/social/UserFriendsDTOList.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/social/UserFriendsDTOList.java
@@ -1,8 +1,11 @@
 package com.tradehero.th.api.social;
 
+import android.support.annotation.NonNull;
 import com.tradehero.common.persistence.BaseArrayListHasExpiration;
 import com.tradehero.common.persistence.DTO;
+import com.tradehero.common.utils.CollectionUtils;
 import java.util.Collection;
+import java.util.List;
 
 public class UserFriendsDTOList extends BaseArrayListHasExpiration<UserFriendsDTO>
         implements DTO
@@ -20,4 +23,14 @@ public class UserFriendsDTOList extends BaseArrayListHasExpiration<UserFriendsDT
         super(c, DEFAULT_LIFE_EXPECTANCY_SECONDS);
     }
     //</editor-fold>
+
+    @NonNull public List<UserFriendsDTO> getTradeHeroUsers()
+    {
+        return CollectionUtils.filter(this, UserFriendsDTO::isTradeHeroUser);
+    }
+
+    @NonNull public List<UserFriendsDTO> getNonTradeHeroUsers()
+    {
+        return CollectionUtils.filter(this, UserFriendsDTO::isNonTradeHeroUser);
+    }
 }
