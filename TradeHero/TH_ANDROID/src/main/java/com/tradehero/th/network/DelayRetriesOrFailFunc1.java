da5ccc6381f757de1ac507222c8675a957615c03
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-12-04 17:54:03 +0800

    Let the sent message be retried 3 times in case of error. #70148936

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/DelayRetriesOrFailFunc1.java
@@ -0,0 +1,43 @@
+package com.tradehero.th.network;
+
+import com.tradehero.common.utils.THToast;
+import com.tradehero.th.R;
+import com.tradehero.th.base.THApp;
+import com.tradehero.th.misc.exception.THException;
+import java.util.concurrent.TimeUnit;
+import rx.Notification;
+import rx.Observable;
+import rx.functions.Func1;
+
+public class DelayRetriesOrFailFunc1 implements Func1<Observable<? extends Notification<?>>, Observable<?>>
+{
+    private int countDown;
+    private long delayMillis;
+
+    //<editor-fold desc="Constructors">
+    public DelayRetriesOrFailFunc1(int retries, long delayMillis)
+    {
+        this.countDown = retries;
+        this.delayMillis = delayMillis;
+    }
+    //</editor-fold>
+
+    @Override public Observable<?> call(Observable<? extends Notification<?>> attempts)
+    {
+        return attempts.flatMap(attempt ->
+        {
+            Throwable error = ((Notification) attempt).getThrowable();
+            if (countDown <= 0)
+            {
+                return Observable.error(error);
+            }
+            THToast.show(String.format(
+                    THApp.getResourceString(R.string.service_retry),
+                    countDown,
+                    new THException(error).getMessage()));
+            countDown--;
+            return Observable.just(attempt)
+                    .delay(delayMillis, TimeUnit.MILLISECONDS);
+        });
+    }
+}
