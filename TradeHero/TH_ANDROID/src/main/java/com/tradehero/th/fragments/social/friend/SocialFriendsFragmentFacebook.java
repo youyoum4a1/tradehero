72c4f0de67f6dee6c52dda05f682dcce2ca34534
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-12-04 11:24:07 +0800

    First shot at retrieving invitable friends.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/social/friend/SocialFriendsFragmentFacebook.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/social/friend/SocialFriendsFragmentFacebook.java
@@ -1,14 +1,35 @@
 package com.tradehero.th.fragments.social.friend;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import com.facebook.HttpMethod;
+import com.facebook.Response;
+import com.tradehero.common.utils.THToast;
 import com.tradehero.th.R;
 import com.tradehero.th.api.social.SocialNetworkEnum;
+import com.tradehero.th.api.social.UserFriendsDTOList;
+import com.tradehero.common.social.facebook.FacebookRequestOperator;
 import javax.inject.Inject;
 import javax.inject.Provider;
+import rx.Observable;
+import rx.Subscription;
+import rx.android.observables.AndroidObservable;
+import timber.log.Timber;
 
 public class SocialFriendsFragmentFacebook extends SocialFriendsFragment
 {
+    private static final String API_INVITABLE_FRIENDS = "/me/invitable_friends";
+
     @Inject Provider<SocialFriendHandlerFacebook> facebookSocialFriendHandlerProvider;
+
+    @Nullable Subscription invitableFriendsSubscription;
+
+    @Override public void onDestroyView()
+    {
+        unsubscribe(invitableFriendsSubscription);
+        invitableFriendsSubscription = null;
+        super.onDestroyView();
+    }
 
     @Override
     protected SocialNetworkEnum getSocialNetwork()
@@ -26,5 +47,43 @@ public class SocialFriendsFragmentFacebook extends SocialFriendsFragment
     protected SocialFriendHandlerFacebook createFriendHandler()
     {
         return facebookSocialFriendHandlerProvider.get();
+    }
+
+    @Override protected void linkWith(@NonNull UserFriendsDTOList value)
+    {
+        super.linkWith(value);
+        if (this.invitableFriends.isEmpty())
+        {
+            //fetchInvitableFriends();
+        }
+    }
+
+    protected void fetchInvitableFriends()
+    {
+        unsubscribe(invitableFriendsSubscription);
+        invitableFriendsSubscription = AndroidObservable.bindFragment(
+                this,
+                facebookSocialFriendHandlerProvider.get().createProfileSessionObservable()
+                        .take(1)
+                        .flatMap(pair -> Observable.create(
+                                FacebookRequestOperator.builder(pair.second)
+                                        .setGraphPath(API_INVITABLE_FRIENDS)
+                                        .setHttpMethod(HttpMethod.GET)
+                                        .setVersion("v2.2")
+                                        .build())))
+                .subscribe(
+                        this::onInvitableFriendsReceived,
+                        this::onInvitableFriendsFailed);
+    }
+
+    protected void onInvitableFriendsReceived(Response response)
+    {
+        Timber.d("InvitableFriends response %s", response);
+        THToast.show("Parse invitable friends");
+    }
+
+    protected void onInvitableFriendsFailed(Throwable e)
+    {
+        Timber.e(e, "InvitableFriends error");
     }
 }
