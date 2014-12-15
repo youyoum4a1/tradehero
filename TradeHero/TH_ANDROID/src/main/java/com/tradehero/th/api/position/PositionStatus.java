663a71ce7bee8792857b046b712dc9a4e4f0e5a7
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-12-15 17:14:21 +0800

    Had omitted position status.

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/position/PositionStatus.java
@@ -0,0 +1,36 @@
+package com.tradehero.th.api.position;
+
+import android.support.annotation.Nullable;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import timber.log.Timber;
+
+public enum PositionStatus
+{
+    SHORT(-1),
+    CLOSED(0),
+    LONG(1),
+    ;
+
+    public final int value;
+
+    //<editor-fold desc="Constructors">
+    PositionStatus(int value)
+    {
+        this.value = value;
+    }
+    //</editor-fold>
+
+    @JsonCreator @Nullable
+    public static PositionStatus create(int value)
+    {
+        for (PositionStatus positionStatus : values())
+        {
+            if (positionStatus.value == value)
+            {
+                return positionStatus;
+            }
+        }
+        Timber.e(new IllegalArgumentException(), "Unknown PositionStatus value %d", value);
+        return null;
+    }
+}
