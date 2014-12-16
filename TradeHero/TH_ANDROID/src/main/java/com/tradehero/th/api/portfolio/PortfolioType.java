deda6e8889b978e74f674e96b1b03c03514bec01
Malvin <malvinsutanto@gmail.com>
2014-12-16 16:08:42 +0800

    Pull PortfolioType from develop branch

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/portfolio/PortfolioType.java
@@ -0,0 +1,35 @@
+package com.tradehero.th.api.portfolio;
+
+import android.support.annotation.Nullable;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import timber.log.Timber;
+
+public enum PortfolioType
+{
+    STOCKS(1),
+    FX(2),
+    ;
+
+    private int value;
+
+    //<editor-fold desc="Constructors">
+    PortfolioType(int value)
+    {
+        this.value = value;
+    }
+    //</editor-fold>
+
+    @JsonCreator @Nullable
+    public static PortfolioType create(int value)
+    {
+        for (PortfolioType portfolioType : values())
+        {
+            if (portfolioType.value == value)
+            {
+                return portfolioType;
+            }
+        }
+        Timber.e(new IllegalArgumentException(), "Unknown PortfolioType value: %d", value);
+        return null;
+    }
+}
