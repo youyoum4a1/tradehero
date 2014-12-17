42c7dfd28543af72caebbc4f1316b1d548ca65ca
Malvin <malvinsutanto@gmail.com>
2014-12-17 11:03:16 +0800

    Revert LeaderboardUserDTO

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/leaderboard/LeaderboardUserDTO.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/leaderboard/LeaderboardUserDTO.java
@@ -4,15 +4,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.tradehero.th.adapters.ExpandableItem;
 import com.tradehero.th.api.leaderboard.key.LeaderboardUserId;
 import com.tradehero.th.api.leaderboard.position.LeaderboardMarkUserId;
 import com.tradehero.th.api.portfolio.OwnedPortfolioId;
-import com.tradehero.th.api.portfolio.PortfolioCompactDTO;
 import com.tradehero.th.api.position.GetPositionsDTOKey;
 import com.tradehero.th.api.users.UserBaseDTO;
+import com.tradehero.th.models.number.THSignedNumber;
 import com.tradehero.th.utils.SecurityUtils;
 import java.util.Date;
 import java.util.List;
@@ -20,39 +18,35 @@ import java.util.List;
 public class LeaderboardUserDTO extends UserBaseDTO
         implements ExpandableItem
 {
-    public static final Double MIN_CONSISTENCY = 0.004;
-
     private static final String LEADERBOARD_USER_POSITION = "LEADERBOARD_USER_POSITION";
     private static final String LEADERBOARD_ID = "LEADERBOARD_ID";
     private static final String LEADERBOARD_INCLUDE_FOF = "LEADERBOARD_INCLUDE_FOF";
+    public static final Double MIN_CONSISTENCY = 0.004;
 
     public long lbmuId;    // leaderboardMarkUser.id ..., will be null if user not ranked
     public int portfolioId;    // ...OR portfolioId --> messy
-
-    public double roiInPeriod;
-    public double roiAnnualizedInPeriod;
-    public int ordinalPosition; // OK
-    public String currencyDisplay;
-    public String currencyISO;
 
     @JsonProperty("friendOf_UserIds")
     public List<Integer> friendOfUserIds;    // client expects userIds here to be present in LeaderboardDTO.users collection!
     @JsonProperty("friendOf_markupString")
     public String friendOfMarkupString;
 
-    public Date periodStartUtc;
-    public Date periodEndUtc;
-
+    public double roiInPeriod;
     public double PLinPeriodRefCcy;
+    public double roiAnnualizedInPeriod;
     public double investedAmountRefCcy;
 
     public int numberOfTradesInPeriod;
     public int numberOfPositionsInPeriod;
     @Nullable public Double avgNumberOfTradesPerMonth;
 
+    public int ordinalPosition; // OK
+
     public int avgHoldingPeriodMins;
 
     // additional fields for most skilled
+    public Date periodStartUtc;
+    public Date periodEndUtc;
     @JsonProperty("stddev_positionRoiInPeriod")
     @Nullable public Double stdDevPositionRoiInPeriod;
     @JsonProperty("sharpeRatioInPeriod_vsSP500")
@@ -67,10 +61,12 @@ public class LeaderboardUserDTO extends UserBaseDTO
     public Integer followerCountFree;
     public Integer followerCountPaid;
     public Integer commentCount;
+    public String currencyDisplay;
+    public String currencyISO;
 
-    @NonNull public LeaderboardUserId getLeaderboardUserId()
+    public LeaderboardUserDTO()
     {
-        return new LeaderboardUserId(id, lbmuId);
+        super();
     }
 
     @Nullable public GetPositionsDTOKey getGetPositionsDTOKey()
@@ -101,6 +97,21 @@ public class LeaderboardUserDTO extends UserBaseDTO
         return null;
     }
 
+    @NonNull public LeaderboardUserId getLeaderboardUserId()
+    {
+        return new LeaderboardUserId(id, lbmuId);
+    }
+
+    public String getHeroQuotientFormatted()
+    {
+        if (starRating == null)
+        {
+            return "0";
+        }
+
+        return THSignedNumber.builder(starRating).withOutSign().build().toString();
+    }
+
     public int getCommentsCount()
     {
         return commentCount == null ? 0 : commentCount;
@@ -116,6 +127,11 @@ public class LeaderboardUserDTO extends UserBaseDTO
         return winRatio != null ? winRatio : 0;
     }
 
+    @Nullable public Double getVolatility()
+    {
+        return stdDevPositionRoiInPeriod;
+    }
+
     public int getNumberOfTrades()
     {
         return numberOfTradesInPeriod;
@@ -126,15 +142,36 @@ public class LeaderboardUserDTO extends UserBaseDTO
         return benchmarkRoiInPeriod != null ? benchmarkRoiInPeriod : 0;
     }
 
-    //<editor-fold desc="ExtendedDTO">
-    @JsonIgnore
-    public Double getConsistency()
+    public double normalizePerformance()
     {
-        if (stdDevPositionRoiInPeriod != null && stdDevPositionRoiInPeriod != 0)
+        try
         {
-            return 1 / stdDevPositionRoiInPeriod;
+            if (sharpeRatioInPeriodVsSP500 == null)
+            {
+                return 0;
+            }
+            Double v = sharpeRatioInPeriodVsSP500;
+            Double min = (double) -2;
+            Double max = (double) 2;
+
+            if (v > max)
+            {
+                v = max;
+            }
+            else if (v < min)
+            {
+                v = min;
+            }
+            double r = 100 * (v - min) / (max - min);
+            //Timber.d("normalizePerformance sharpeRatioInPeriodVsSP500 %s result %s",
+            //        sharpeRatioInPeriodVsSP500, r);
+
+            return r;
+        } catch (Exception e)
+        {
+            //Timber.e("normalizePerformance", e);
         }
-        return null;
+        return 0;
     }
 
     @JsonIgnore
@@ -153,6 +190,7 @@ public class LeaderboardUserDTO extends UserBaseDTO
         return currencyDisplay != null && !currencyDisplay.isEmpty();
     }
 
+    //<editor-fold desc="ExtendedDTO">
     @JsonIgnore
     public void setPosition(Integer position)
     {
@@ -163,18 +201,6 @@ public class LeaderboardUserDTO extends UserBaseDTO
     public Integer getPosition()
     {
         return (Integer) get(LEADERBOARD_USER_POSITION);
-    }
-
-    @JsonIgnore
-    @Override public boolean isExpanded()
-    {
-        return (Boolean) get(ExpandableItem.class.getName(), false);
-    }
-
-    @JsonIgnore
-    @Override public void setExpanded(boolean expanded)
-    {
-        this.put(ExpandableItem.class.getName(), expanded);
     }
 
     @JsonIgnore
@@ -200,4 +226,29 @@ public class LeaderboardUserDTO extends UserBaseDTO
     {
         this.put(LEADERBOARD_INCLUDE_FOF, includeFoF);
     }
+
+    @JsonIgnore
+    @Override public boolean isExpanded()
+    {
+        return (Boolean) get(ExpandableItem.class.getName(), false);
+    }
+
+    @JsonIgnore
+    @Override public void setExpanded(boolean expanded)
+    {
+        this.put(ExpandableItem.class.getName(), expanded);
+    }
+
+    @JsonIgnore
+    public Double getConsistency()
+    {
+        if (stdDevPositionRoiInPeriod != null && stdDevPositionRoiInPeriod != 0)
+        {
+            return 1 / stdDevPositionRoiInPeriod;
+        }
+        return null;
+    }
+
+    //</editor-fold>
 }
+
