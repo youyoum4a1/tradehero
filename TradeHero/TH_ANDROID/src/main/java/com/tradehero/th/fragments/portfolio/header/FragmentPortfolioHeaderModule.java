36390e24b527189d26e624e7a2218dbff197854b
Xavier Lepretre <xavier.lepretre@gmail.com>
2014-12-18 12:14:52 +0800

    Had omitted injection.

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/portfolio/header/FragmentPortfolioHeaderModule.java
@@ -0,0 +1,14 @@
+package com.tradehero.th.fragments.portfolio.header;
+
+import dagger.Module;
+
+@Module(
+        injects = {
+                CurrentUserFxPortfolioHeaderView.class,
+        },
+        library = true,
+        complete = false
+)
+public class FragmentPortfolioHeaderModule
+{
+}
