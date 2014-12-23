1dad0a7019e156915882de2f9c41377e3a6c3e32
Malvin <malvinsutanto@gmail.com>
2014-12-23 15:29:14 +0800

    Make sure we display fx items on fx competitions

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/competition/ProviderFxListFragment.java
@@ -0,0 +1,50 @@
+package com.tradehero.th.fragments.competition;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import com.tradehero.th.R;
+import com.tradehero.th.api.portfolio.OwnedPortfolioId;
+import com.tradehero.th.api.security.SecurityCompactDTO;
+import com.tradehero.th.fragments.security.SimpleSecurityItemViewAdapter;
+import com.tradehero.th.fragments.trade.BuySellFXFragment;
+import javax.inject.Inject;
+
+public class ProviderFxListFragment extends ProviderSecurityListFragment
+{
+    @Inject Context dummyContext;
+
+    @Override protected ListAdapter createSecurityItemViewAdapter()
+    {
+        return new SimpleSecurityItemViewAdapter(
+                getActivity(),
+                R.layout.trending_fx_item);
+    }
+
+    @Override protected AdapterView.OnItemClickListener createOnItemClickListener()
+    {
+        return new OnFxViewClickListener();
+    }
+
+    private class OnFxViewClickListener implements AdapterView.OnItemClickListener
+    {
+        @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+        {
+            SecurityCompactDTO securityCompactDTO = (SecurityCompactDTO) parent.getItemAtPosition(position);
+
+            Bundle args = new Bundle();
+            BuySellFXFragment.putSecurityId(args, securityCompactDTO.getSecurityId());
+
+            OwnedPortfolioId ownedPortfolioId = getApplicablePortfolioId();
+
+            if (ownedPortfolioId != null)
+            {
+                BuySellFXFragment.putApplicablePortfolioId(args, ownedPortfolioId);
+            }
+
+            navigator.get().pushFragment(BuySellFXFragment.class, args);
+        }
+    }
+}
