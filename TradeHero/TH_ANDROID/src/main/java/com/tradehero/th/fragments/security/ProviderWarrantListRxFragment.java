992d5e93cd0df11aced92bf765492e56e2f4c1c4
Malvin <malvinsutanto@gmail.com>
2014-12-30 15:18:26 +0800

    Put search icon on DW warrant list

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/security/ProviderWarrantListRxFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/security/ProviderWarrantListRxFragment.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import com.tradehero.common.persistence.DTOCacheRx;
 import com.tradehero.th.R;
 import com.tradehero.th.adapters.PagedArrayDTOAdapterNew;
 import com.tradehero.th.api.competition.key.WarrantProviderSecurityListType;
+import com.tradehero.th.api.portfolio.OwnedPortfolioId;
 import com.tradehero.th.api.security.SecurityCompactDTO;
 import com.tradehero.th.api.security.SecurityCompactDTOList;
 import com.tradehero.th.api.security.WarrantType;
@@ -46,6 +50,35 @@ public class ProviderWarrantListRxFragment extends ProviderSecurityListRxFragmen
             Bundle savedInstanceState)
     {
         return inflater.inflate(R.layout.fragment_provider_warrant_list, container, false);
+    }
+
+    @Override public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.search_menu, menu);
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.btn_search:
+                pushSearchFragment();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void pushSearchFragment()
+    {
+        Bundle args = new Bundle();
+        SecuritySearchProviderFragment.putProviderId(args, providerId);
+        OwnedPortfolioId applicablePortfolioId = getApplicablePortfolioId();
+        if (applicablePortfolioId != null)
+        {
+            SecuritySearchProviderFragment.putApplicablePortfolioId(args, applicablePortfolioId);
+        }
+        navigator.get().pushFragment(SecuritySearchProviderFragment.class, args);
     }
 
     @NonNull @Override protected PagedArrayDTOAdapterNew<SecurityCompactDTO, SecurityItemView> createItemViewAdapter()
