dd17b05154a15fdbc703dfc68a659271b25a6d62
Malvin <malvinsutanto@gmail.com>
2015-01-06 17:46:25 +0800

    Create new service to get FXVideos

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/service/VideoServiceRx.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/service/VideoServiceRx.java
@@ -2,6 +2,8 @@ package com.tradehero.th.network.service;
 
 import com.tradehero.th.api.education.PaginatedVideoCategoryDTO;
 import com.tradehero.th.api.education.PaginatedVideoDTO;
+import com.tradehero.th.api.education.VideoDTO;
+import java.util.List;
 import retrofit.http.GET;
 import retrofit.http.Query;
 import rx.Observable;
@@ -18,4 +20,7 @@ interface VideoServiceRx
             @Query("videoCategoryId") int videoCategoryId,
             @Query("page") Integer page,
             @Query("perPage") Integer perPage);
+
+    @GET("/fxvideos")
+    Observable<List<VideoDTO>> getFXVideos();
 }
