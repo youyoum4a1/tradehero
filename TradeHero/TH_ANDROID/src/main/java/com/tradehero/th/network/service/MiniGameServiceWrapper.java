5c884170cadce61813521abdad1601061a31da8b
Malvin <malvinsutanto@gmail.com>
2015-01-15 18:56:45 +0800

    Handle viral minigame popup

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/service/MiniGameServiceWrapper.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/service/MiniGameServiceWrapper.java
@@ -6,6 +6,9 @@ import com.tradehero.th.api.games.MiniGameDefDTO;
 import com.tradehero.th.api.games.MiniGameDefDTOList;
 import com.tradehero.th.api.games.MiniGameDefKey;
 import com.tradehero.th.api.games.MiniGameScoreResponseDTO;
+import com.tradehero.th.api.games.ViralMiniGameDefDTO;
+import com.tradehero.th.api.games.ViralMiniGameDefDTOList;
+import com.tradehero.th.api.games.ViralMiniGameDefKey;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import rx.Observable;
@@ -35,5 +38,15 @@ import rx.Observable;
     @NonNull public Observable<MiniGameScoreResponseDTO> recordScore(@NonNull MiniGameDefKey gameId, @NonNull GameScore score)
     {
         return miniGameServiceRx.recordScore(gameId.key, score.score, score.level);
+    }
+
+    @NonNull public Observable<ViralMiniGameDefDTOList> getAllViralGame()
+    {
+        return miniGameServiceRx.getViralGameList();
+    }
+
+    @NonNull public Observable<ViralMiniGameDefDTO> getViralGame(@NonNull ViralMiniGameDefKey gameId)
+    {
+        return miniGameServiceRx.getViralGameDetail(gameId.key);
     }
 }
---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/persistence/games/ViralMiniGameDefCache.java
@@ -0,0 +1,47 @@
+package com.tradehero.th.persistence.games;
+
+import android.support.annotation.NonNull;
+import com.tradehero.common.persistence.BaseFetchDTOCacheRx;
+import com.tradehero.common.persistence.DTOCacheUtilRx;
+import com.tradehero.common.persistence.UserCache;
+import com.tradehero.th.api.games.MiniGameDefDTO;
+import com.tradehero.th.api.games.MiniGameDefKey;
+import com.tradehero.th.api.games.ViralMiniGameDefDTO;
+import com.tradehero.th.api.games.ViralMiniGameDefKey;
+import com.tradehero.th.network.service.MiniGameServiceWrapper;
+import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import rx.Observable;
+
+@Singleton @UserCache
+public class ViralMiniGameDefCache extends BaseFetchDTOCacheRx<ViralMiniGameDefKey, ViralMiniGameDefDTO>
+{
+    private static final int DEFAULT_MAX_VALUE = 200;
+    private static final int DEFAULT_MAX_SUBJECT = 10;
+
+    @NonNull protected final MiniGameServiceWrapper miniGameServiceWrapper;
+
+    //<editor-fold desc="Constructors">
+    @Inject public ViralMiniGameDefCache(
+            @NonNull DTOCacheUtilRx dtoCacheUtilRx,
+            @NonNull MiniGameServiceWrapper miniGameServiceWrapper)
+    {
+        super(DEFAULT_MAX_VALUE, DEFAULT_MAX_SUBJECT, DEFAULT_MAX_SUBJECT, dtoCacheUtilRx);
+        this.miniGameServiceWrapper = miniGameServiceWrapper;
+    }
+    //</editor-fold>
+
+    @NonNull @Override protected Observable<ViralMiniGameDefDTO> fetch(@NonNull ViralMiniGameDefKey key)
+    {
+        return miniGameServiceWrapper.getViralGame(key);
+    }
+
+    public void onNext(@NonNull List<? extends ViralMiniGameDefDTO> miniGames)
+    {
+        for (ViralMiniGameDefDTO miniGame : miniGames)
+        {
+            onNext(miniGame.getDTOKey(), miniGame);
+        }
+    }
+}
