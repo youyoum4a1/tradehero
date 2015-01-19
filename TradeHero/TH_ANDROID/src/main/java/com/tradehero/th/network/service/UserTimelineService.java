6e5fe25b890e1d2b6c32262ba13a862b5eabdae3
Xavier Lepretre <xavier.lepretre@gmail.com>
2015-01-19 18:00:01 +0800

    Remove unused methods.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/service/UserTimelineService.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/service/UserTimelineService.java
@@ -1,30 +1,13 @@
 package com.tradehero.th.network.service;
 
-import com.tradehero.th.api.BaseResponseDTO;
 import com.tradehero.th.api.timeline.TimelineDTO;
-import com.tradehero.th.api.timeline.TimelineItemDTO;
-import com.tradehero.th.api.timeline.TimelineItemShareRequestDTO;
 import com.tradehero.th.api.timeline.TimelineSection;
-import retrofit.http.Body;
-import retrofit.http.DELETE;
 import retrofit.http.GET;
-import retrofit.http.POST;
 import retrofit.http.Path;
 import retrofit.http.Query;
 
 public interface UserTimelineService
 {
-    //<editor-fold desc="Get Global Timeline">
-    @GET("/timeline")
-    TimelineDTO getGlobalTimeline(
-            @Query("maxCount") Integer maxCount,
-            @Query("maxId") Integer maxId,
-            @Query("minId") Integer minId);
-
-    @GET("/timeline/{timelineId}") TimelineItemDTO getTimelineDetail(
-            @Path("timelineId") int timelineId);
-    //</editor-fold>
-
     //<editor-fold desc="Get User Timeline">
     @GET("/users/{userId}/{section}")
     TimelineDTO getTimeline(
@@ -33,20 +16,5 @@ public interface UserTimelineService
             @Query("maxCount") Integer maxCount,
             @Query("maxId") Integer maxId,
             @Query("minId") Integer minId);
-    //</editor-fold>
-
-    //<editor-fold desc="Share Timeline Item">
-    @POST("/users/{userId}/timeline/{timelineItemId}/share")
-    BaseResponseDTO shareTimelineItem( // Appears to return a boolean
-            @Path("userId") int userId,
-            @Path("timelineItemId") int timelineItemId,
-            @Body TimelineItemShareRequestDTO timelineItemShareRequestDTO);
-    //</editor-fold>
-
-    //<editor-fold desc="Delete Timeline Item">
-    @DELETE("/users/{userId}/timeline/{timelineItemId}")
-    BaseResponseDTO deleteTimelineItem(
-            @Path("userId") int userId,
-            @Path("timelineItemId") int timelineItemId);
     //</editor-fold>
 }
