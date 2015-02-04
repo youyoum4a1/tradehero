f19ec80304a1f6b4965f23eaac000477c6976a14
Xavier Lepretre <xavier.lepretre@gmail.com>
2015-02-04 17:27:31 +0800

    Removed obsolete element #87391738

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/portfolio/PortfolioDTO.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/api/portfolio/PortfolioDTO.java
@@ -2,9 +2,7 @@ package com.tradehero.th.api.portfolio;
 
 import android.support.annotation.NonNull;
 import com.tradehero.common.persistence.DTO;
-import com.tradehero.th.api.quote.UpdatePricesQuoteDTO;
 import java.util.Date;
-import java.util.List;
 
 public class PortfolioDTO extends PortfolioCompactDTO implements DTO
 {
@@ -28,9 +26,6 @@ public class PortfolioDTO extends PortfolioCompactDTO implements DTO
     public Double roiY2DAnnualized;
     public double plY2D;
 
-    @Deprecated
-    public List<UpdatePricesQuoteDTO> yahooSymbols;
-
     public int countTrades;
     public int countExchanges;
 
@@ -52,7 +47,6 @@ public class PortfolioDTO extends PortfolioCompactDTO implements DTO
                 ", roiY2D=" + roiY2D +
                 ", roiY2DAnnualized=" + roiY2DAnnualized +
                 ", plY2D=" + plY2D +
-                ", yahooSymbols=" + yahooSymbols +
                 ", countTrades=" + countTrades +
                 ']';
     }
