f67c237ce8be3cd3b23bed31586c44c92d3a1afc
Xavier Lepretre <xavier.lepretre@gmail.com>
2015-02-12 12:43:14 +0800

    Made use of Bing's Ajax.svc to remove the XML deserialiser. Should gain more than 2,500 methods...

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/service/TranslationServiceBingWrapper.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/service/TranslationServiceBingWrapper.java
@@ -6,6 +6,7 @@ import com.tradehero.th.api.translation.bing.BingTranslationToken;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import rx.Observable;
+import rx.functions.Func1;
 
 @Singleton public class TranslationServiceBingWrapper
 {
@@ -25,6 +26,13 @@ import rx.Observable;
     {
         return translationServiceBingRx.requestForTranslation(
                 token.getPrefixedAccessToken(),
-                from, to, PREFERRED_CONTENT_TYPE, text);
+                from, to, PREFERRED_CONTENT_TYPE, text)
+                .map(new Func1<String, BingTranslationResult>()
+                {
+                    @Override public BingTranslationResult call(String translated)
+                    {
+                        return new BingTranslationResult(from, to, translated);
+                    }
+                });
     }
 }
