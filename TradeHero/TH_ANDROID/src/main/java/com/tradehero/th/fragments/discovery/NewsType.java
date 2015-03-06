613f3bf586434e1dc0b653d3dfb43d4b660ddee1
Richard Liang <richard.liangyx@gmail.com>
2015-03-06 10:22:43 +0800

    P1:Order of news sources should be changed

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discovery/NewsType.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/discovery/NewsType.java
@@ -8,10 +8,10 @@ import com.tradehero.th.utils.metrics.AnalyticsConstants;
 
 enum NewsType
 {
+    SeekingAlpha(R.string.seeking_alpha, R.layout.news_carousel_seekingalpha, AnalyticsConstants.NewsSeekingAlpha),
     MotleyFool(R.string.motley_fool, R.layout.news_carousel_motleyfool, AnalyticsConstants.NewsMotleyFool),
     Region(R.string.regional_news, R.layout.news_carousel_regional, AnalyticsConstants.NewsRegional),
-    Global(R.string.global_news, R.layout.news_carousel_global, AnalyticsConstants.NewsGlobal),
-    SeekingAlpha(R.string.seeking_alpha, R.layout.news_carousel_seekingalpha, AnalyticsConstants.NewsSeekingAlpha);
+    Global(R.string.global_news, R.layout.news_carousel_global, AnalyticsConstants.NewsGlobal);
 
     @StringRes public final int titleResourceId;
     @LayoutRes public final int titleViewResourceId;
