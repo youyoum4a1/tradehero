7c1daa91fdd43e2c0c43b769cd4a5bfb63a91042
Malvin <malvinsutanto@gmail.com>
2015-04-15 16:41:50 +0800

    Add close button on top of the popquiz activity

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/activities/PopQuizActivity.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/activities/PopQuizActivity.java
@@ -3,6 +3,7 @@ package com.tradehero.th.activities;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.view.Menu;
 import com.tradehero.common.utils.THToast;
 import com.tradehero.th.R;
 import com.tradehero.th.api.social.SocialNetworkEnum;
@@ -20,6 +21,12 @@ public class PopQuizActivity extends OneFragmentActivity
     @NonNull @Override protected Class<? extends Fragment> getInitialFragment()
     {
         return XWalkWebViewFragment.class;
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.popquiz_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @NonNull @Override protected Bundle getInitialBundle()
