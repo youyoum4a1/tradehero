1ddd1b22caa701b8ab69faac4c5b4498cbce38cd
PalmerMa <min_99@126.com>
2015-06-09 11:48:38 +0800

    new feature security detail

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/chinabuild/fragment/security/SecurityUserOptFragment.java
@@ -0,0 +1,10 @@
+package com.tradehero.chinabuild.fragment.security;
+
+import com.tradehero.th.fragments.base.DashboardFragment;
+
+/**
+ * Created by palmer on 15/6/9.
+ */
+public class SecurityUserOptFragment extends DashboardFragment{
+
+}
