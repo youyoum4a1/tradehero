5c92d23696a64472294734949d9b2bb2e113489e
Richard Liang <richard.liangyx@gmail.com>
2015-06-10 17:00:01 +0800

    Meaningfully field names for QuoteTick

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/chinabuild/data/QuoteTick.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/chinabuild/data/QuoteTick.java
@@ -5,12 +5,36 @@ package com.tradehero.chinabuild.data;
  */
 public class QuoteTick {
     public Integer id;
-    public Double p;
-    public Long v;
-    public Double a;
+    public Double price;
+    public Long volume;
+    public Double avgPrice;
+
+    /**
+     * set Price
+     * @param p
+     */
+    public void setP(Double p) {
+        price = p;
+    }
+
+    /**
+     * set volume
+     * @param v
+     */
+    public void setV(Long v) {
+        volume = v;
+    }
+
+    /**
+     * set average price
+     * @param a
+     */
+    public void setA(Double a) {
+        avgPrice = a;
+    }
 
     public String toString() {
-        return String.format("id[%d], price[%f], volume[%d], avg[%f]", id, p, v, a);
+        return String.format("id[%d], price[%f], volume[%d], avg[%f]", id, price, volume, avgPrice);
     }
 
 }
