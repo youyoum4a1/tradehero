cc5e08a668823719927d100266d9f0a802346771
Xavier Lepretre <xavier.lepretre@gmail.com>
2015-06-25 16:51:10 +0800

    Added live broker DTOs.

---
+++ TradeHero/TH_ANDROID/src/live/java/com/tradehero/th/api/live/LiveBrokerConstants.java
@@ -0,0 +1,6 @@
+package com.tradehero.th.api.live;
+
+public class LiveBrokerConstants
+{
+    public static final int AYONDO_ID = 1;
+}
---
+++ TradeHero/TH_ANDROID/src/live/java/com/tradehero/th/api/live/LiveBrokerId.java
@@ -0,0 +1,26 @@
+package com.tradehero.th.api.live;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.tradehero.common.persistence.DTOKey;
+
+public class LiveBrokerId implements DTOKey
+{
+    @NonNull public final Integer key;
+
+    @JsonCreator public LiveBrokerId(int key)
+    {
+        this.key = key;
+    }
+
+    @Override public int hashCode()
+    {
+        return key.hashCode();
+    }
+
+    @Override public boolean equals(@Nullable Object other)
+    {
+        return other instanceof LiveBrokerId && ((LiveBrokerId) other).key.equals(key);
+    }
+}
