3b12459d00b747aab189cd60d1e92d569f3e29d1
Malvin <malvinsutanto@gmail.com>
2015-06-26 14:11:18 +0800

    view injections for step 5

--- TradeHero/TH_ANDROID/src/live/java/com/tradehero/th/fragments/live/LiveSignUpStep5Fragment.java
+++ TradeHero/TH_ANDROID/src/live/java/com/tradehero/th/fragments/live/LiveSignUpStep5Fragment.java
@@ -5,13 +5,26 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import com.tradehero.th.R;
 import com.tradehero.th.fragments.base.BaseFragment;
+import com.tradehero.th.widget.DocumentActionWidget;
 
 public class LiveSignUpStep5Fragment extends LiveSignUpStepBaseFragment
 {
+    @InjectView(R.id.document_action_identity) DocumentActionWidget documentActionIdentity;
+    @InjectView(R.id.document_action_residence) DocumentActionWidget documentActionResidence;
+    @InjectView(R.id.document_action_signature) DocumentActionWidget documentActionSignature;
+
     @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         return inflater.inflate(R.layout.fragment_sign_up_live_step_5, container, false);
+    }
+
+    @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.inject(this, view);
     }
 }
