96454901b94028306439520efb976eb231a7e936
Xavier Lepretre <xavier.lepretre@gmail.com>
2015-07-01 11:10:18 +0800

    Pushed Samsung build 566 to Samsung store.

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/security/SecuritySearchFragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/fragments/security/SecuritySearchFragment.java
@@ -54,7 +54,7 @@ public class SecuritySearchFragment extends BaseSearchRxFragment<
         return retrieved;
     }
 
-    private AssetClass getAssetClass()
+    @NonNull private AssetClass getAssetClass()
     {
         if (assetClass == null)
         {
