d543384940f06faa9adf2d562ec2b9737124db91
Xavier Lepretre <xavier.lepretre@gmail.com>
2015-07-09 17:00:09 +0800

    Added elements in KYC Options. Added dummy KYC Options.

--- TradeHero/TH_ANDROID/src/live/java/com/tradehero/th/api/live/ayondo/KYCAyondoFormOptionsDTO.java
+++ TradeHero/TH_ANDROID/src/live/java/com/tradehero/th/api/live/ayondo/KYCAyondoFormOptionsDTO.java
@@ -1,8 +1,50 @@
 package com.tradehero.th.api.live.ayondo;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tradehero.th.api.live.KYCFormOptionsDTO;
+import com.tradehero.th.api.market.Country;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class KYCAyondoFormOptionsDTO implements KYCFormOptionsDTO
 {
     public static final String KEY_AYONDO_TYPE = "Ayondo";
+
+    @NonNull public final List<Country> allowedMobilePhoneCountries;
+    @NonNull public final List<Country> allowedNationalityCountries;
+    @NonNull public final List<Country> allowedResidencyCountries;
+
+    public KYCAyondoFormOptionsDTO(
+            @JsonProperty("allowedMobilePhoneCountries") @Nullable List<Country> allowedMobilePhoneCountries,
+            @JsonProperty("allowedNationalityCountries") @Nullable List<Country> allowedNationalityCountries,
+            @JsonProperty("allowedResidencyCountries") @Nullable List<Country> allowedResidencyCountries)
+    {
+        if (allowedMobilePhoneCountries == null)
+        {
+            this.allowedMobilePhoneCountries = Collections.unmodifiableList(Arrays.asList(Country.values()));
+        }
+        else
+        {
+            this.allowedMobilePhoneCountries = Collections.unmodifiableList(allowedMobilePhoneCountries);
+        }
+        if (allowedNationalityCountries == null)
+        {
+            this.allowedNationalityCountries = Collections.unmodifiableList(Arrays.asList(Country.values()));
+        }
+        else
+        {
+            this.allowedNationalityCountries = Collections.unmodifiableList(allowedNationalityCountries);
+        }
+        if (allowedResidencyCountries == null)
+        {
+            this.allowedResidencyCountries = Collections.unmodifiableList(Arrays.asList(Country.values()));
+        }
+        else
+        {
+            this.allowedResidencyCountries = Collections.unmodifiableList(allowedResidencyCountries);
+        }
+    }
 }
--- TradeHero/TH_ANDROID/src/liveDebug/java/com/tradehero/th/network/service/DummyLiveServiceWrapper.java
+++ TradeHero/TH_ANDROID/src/liveDebug/java/com/tradehero/th/network/service/DummyLiveServiceWrapper.java
@@ -3,16 +3,21 @@ package com.tradehero.th.network.service;
 import android.support.annotation.NonNull;
 import com.tradehero.th.api.live.IdentityPromptInfoDTO;
 import com.tradehero.th.api.live.IdentityPromptInfoKey;
+import com.tradehero.th.api.live.KYCFormOptionsDTO;
 import com.tradehero.th.api.live.LiveBrokerDTO;
 import com.tradehero.th.api.live.LiveBrokerId;
 import com.tradehero.th.api.live.LiveBrokerSituationDTO;
 import com.tradehero.th.api.live.LiveCountryDTO;
 import com.tradehero.th.api.live.LiveCountryDTOList;
 import com.tradehero.th.api.live.LiveTradingSituationDTO;
+import com.tradehero.th.api.live.ayondo.KYCAyondoFormOptionsDTO;
 import com.tradehero.th.api.market.Country;
 import com.tradehero.th.models.kyc.ayondo.KYCAyondoForm;
 import com.tradehero.th.persistence.prefs.LiveBrokerSituationPreference;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import javax.inject.Inject;
 import rx.Observable;
 
@@ -66,5 +71,58 @@ public class DummyLiveServiceWrapper extends LiveServiceWrapper
             liveCountryDTOs.add(new LiveCountryDTO(country));
         }
         return Observable.just(liveCountryDTOs);
+    }
+
+    @NonNull @Override public Observable<KYCFormOptionsDTO> getKYCFormOptions(@NonNull LiveBrokerId liveBrokerId)
+    {
+        List<Country> nationalities = new ArrayList<>(Arrays.asList(Country.values()));
+        nationalities.remove(Country.NONE);
+        nationalities.removeAll(getNoBusinessNationalities());
+        KYCFormOptionsDTO options = new KYCAyondoFormOptionsDTO(
+                Arrays.asList(Country.SG, Country.AU, Country.GB),
+                nationalities,
+                Arrays.asList(Country.SG, Country.AU, Country.GB));
+        return Observable.just(options);
+    }
+
+    @NonNull public List<Country> getNoBusinessNationalities()
+    {
+        return Collections.unmodifiableList(Arrays.asList(
+                Country.IR,
+                Country.KP,
+                Country.CU,
+                Country.EC,
+                Country.ET,
+                Country.KE,
+                Country.MM,
+                Country.NG,
+                Country.PK,
+                Country.ST,
+                Country.SY,
+                Country.TZ,
+                Country.TR,
+                Country.VN,
+                Country.YE,
+                Country.BD,
+                Country.IQ,
+                Country.KG,
+                Country.LY,
+                Country.TJ,
+                Country.ZW,
+                Country.SD,
+                Country.AF,
+                Country.LA,
+                Country.DZ,
+                Country.AL,
+                Country.AO,
+                Country.AG,
+                Country.AR,
+                Country.KH,
+                Country.KW,
+                Country.MN,
+                Country.NA,
+                Country.SO,
+                Country.US
+        ));
     }
 }
