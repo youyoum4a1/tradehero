85deb0c0205454e67c670c046cc6f0aaa8aae28b
Malvin <malvinsutanto@gmail.com>
2015-07-13 11:17:07 +0800

    Update the style of switcher to have a small indicator of it swapping

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/widget/OffOnViewSwitcher.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/widget/OffOnViewSwitcher.java
@@ -1,16 +1,22 @@
 package com.tradehero.th.widget;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Build;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.tradehero.th.R;
 import java.util.concurrent.TimeUnit;
 import rx.Observable;
@@ -18,11 +24,12 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Func1;
 import rx.subjects.PublishSubject;
 
-public class OffOnViewSwitcher extends ViewSwitcher
+public class OffOnViewSwitcher extends LinearLayout
         implements View.OnClickListener
 {
     private static final int ANIM_DELAY = 300;
 
+    @Bind(R.id.off_on_view_switcher) ViewSwitcher offOnViewSwitcher;
     private boolean mIsOn;
     private PublishSubject<OffOnViewSwitcherEvent> mSwitchSubject;
 
@@ -33,34 +40,51 @@ public class OffOnViewSwitcher extends ViewSwitcher
 
     public OffOnViewSwitcher(Context context, AttributeSet attrs)
     {
-        super(context, attrs);
-        init(attrs);
+        this(context, attrs, 0);
     }
 
-    private void init(AttributeSet attrs)
+    public OffOnViewSwitcher(Context context, AttributeSet attrs, int defStyleAttr)
     {
-        setAnimateFirstView(false);
+        super(context, attrs, defStyleAttr);
+        init(attrs, defStyleAttr, 0);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public OffOnViewSwitcher(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes)
+    {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void init(AttributeSet attrs, int defStyleAttr, int defStyleRes)
+    {
+        LayoutInflater.from(getContext()).inflate(R.layout.off_on_switcher_merged, this);
+        ButterKnife.bind(this);
+        setOrientation(HORIZONTAL);
+        setGravity(Gravity.CENTER_VERTICAL);
+        offOnViewSwitcher.setForegroundGravity(Gravity.CENTER_VERTICAL);
+        offOnViewSwitcher.setAnimateFirstView(false);
         mSwitchSubject = PublishSubject.create();
 
         TypedArray a = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.OffOnViewSwitcher, 0, 0);
 
-        @LayoutRes int offLayoutId = a.getResourceId(R.styleable.OffOnViewSwitcher_offLayout, R.layout.off_on_switcher_textview_off);
-        @LayoutRes int onLayoutId = a.getResourceId(R.styleable.OffOnViewSwitcher_onLayout, R.layout.off_on_switcher_textview_on);
+        @LayoutRes int offLayoutId = a.getResourceId(R.styleable.OffOnViewSwitcher_offLayout, R.layout.off_on_switcher_default_textview);
+        @LayoutRes int onLayoutId = a.getResourceId(R.styleable.OffOnViewSwitcher_onLayout, R.layout.off_on_switcher_default_textview);
 
-        View offView = LayoutInflater.from(getContext()).inflate(offLayoutId, this, false);
+        View offView = LayoutInflater.from(getContext()).inflate(offLayoutId, offOnViewSwitcher, false);
         if (offView instanceof TextView && TextUtils.isEmpty(((TextView) offView).getText()))
         {
             ((TextView) offView).setText(a.getString(R.styleable.OffOnViewSwitcher_offText));
         }
 
-        View onView = LayoutInflater.from(getContext()).inflate(onLayoutId, this, false);
+        View onView = LayoutInflater.from(getContext()).inflate(onLayoutId, offOnViewSwitcher, false);
         if (onView instanceof TextView && TextUtils.isEmpty(((TextView) onView).getText()))
         {
             ((TextView) onView).setText(a.getString(R.styleable.OffOnViewSwitcher_onText));
         }
 
-        addView(offView);
-        addView(onView);
+        offOnViewSwitcher.addView(offView);
+        offOnViewSwitcher.addView(onView);
 
         a.recycle();
     }
@@ -87,7 +111,7 @@ public class OffOnViewSwitcher extends ViewSwitcher
         boolean changed = this.mIsOn ^ isOn;
         this.mIsOn = isOn;
         setupAnimationToBeUsed(true);
-        setDisplayedChild(mIsOn ? 1 : 0);
+        offOnViewSwitcher.setDisplayedChild(mIsOn ? 1 : 0);
         if (changed)
         {
             mSwitchSubject.onNext(new OffOnViewSwitcherEvent(isFromUser, mIsOn));
@@ -96,8 +120,10 @@ public class OffOnViewSwitcher extends ViewSwitcher
 
     protected void setupAnimationToBeUsed(boolean animate)
     {
-        setInAnimation(animate ? AnimationUtils.loadAnimation(getContext(), mIsOn ? R.anim.push_up_in : R.anim.push_down_in) : null);
-        setOutAnimation(animate ? AnimationUtils.loadAnimation(getContext(), mIsOn ? R.anim.push_up_out : R.anim.push_down_out) : null);
+        offOnViewSwitcher.setInAnimation(
+                animate ? AnimationUtils.loadAnimation(getContext(), mIsOn ? R.anim.push_up_in : R.anim.push_down_in) : null);
+        offOnViewSwitcher.setOutAnimation(
+                animate ? AnimationUtils.loadAnimation(getContext(), mIsOn ? R.anim.push_up_out : R.anim.push_down_out) : null);
     }
 
     @NonNull public Observable<OffOnViewSwitcherEvent> getSwitchObservable()
