f45af54bbd9eb12303ccffcfe3052655a4e78de5
Xavier Lepretre <xavier.lepretre@gmail.com>
2015-07-15 19:37:23 +0800

    Fix compilation.

---
+++ TradeHero/TH_ANDROID/src/dagger1Game/java/com/tradehero/th/network/service/RetrofitGameLiveProtectedModule.java
@@ -0,0 +1,11 @@
+package com.tradehero.th.network.service;
+
+import dagger.Module;
+
+@Module(
+        complete = false,
+        library = true
+)
+public class RetrofitGameLiveProtectedModule
+{
+}
