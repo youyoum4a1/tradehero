0fa9d27e46feada1bd9d7a0b2e913f7115b1d9ec
Malvin <malvinsutanto@gmail.com>
2015-07-30 15:52:16 +0800

    Create custom observable for view focus change

---
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/rx/view/OnFocusChangeEvent.java
@@ -0,0 +1,27 @@
+package com.tradehero.th.rx.view;
+
+import android.view.View;
+
+public class OnFocusChangeEvent
+{
+    public final View view;
+    public final boolean hasFocus;
+
+    public static OnFocusChangeEvent create(View input, boolean hasFocus)
+    {
+        return new OnFocusChangeEvent(input, hasFocus);
+    }
+
+    public OnFocusChangeEvent(View input, boolean hasFocus)
+    {
+        if (input == null)
+        {
+            throw new NullPointerException("Null view");
+        }
+        else
+        {
+            this.view = input;
+            this.hasFocus = hasFocus;
+        }
+    }
+}
