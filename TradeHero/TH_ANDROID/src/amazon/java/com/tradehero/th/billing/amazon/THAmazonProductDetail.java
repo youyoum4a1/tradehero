14a19c20804cfda7262030d3199b4e28848b7da2
Xavier Lepretre <xavier.lepretre@gmail.com>
2015-08-04 18:13:24 +0800

    Moved Amazon IAP to v2.0.61.

--- TradeHero/TH_ANDROID/src/amazon/java/com/tradehero/th/billing/amazon/THAmazonProductDetail.java
+++ TradeHero/TH_ANDROID/src/amazon/java/com/tradehero/th/billing/amazon/THAmazonProductDetail.java
@@ -2,15 +2,12 @@ package com.tradehero.th.billing.amazon;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import com.amazon.device.iap.model.Price;
 import com.amazon.device.iap.model.Product;
 import com.tradehero.common.billing.amazon.AmazonProductDetail;
 import com.tradehero.common.billing.amazon.AmazonSKU;
 import com.tradehero.th.R;
 import com.tradehero.th.billing.ProductIdentifierDomain;
 import com.tradehero.th.billing.THProductDetail;
-import com.tradehero.th.models.number.THSignedMoney;
-import java.math.BigDecimal;
 
 public class THAmazonProductDetail extends AmazonProductDetail<AmazonSKU>
     implements THProductDetail<AmazonSKU>
@@ -54,29 +51,12 @@ public class THAmazonProductDetail extends AmazonProductDetail<AmazonSKU>
 
     @Override @Nullable public Double getPrice()
     {
-        Price price = product.getPrice();
-        if (price != null)
-        {
-            BigDecimal value = price.getValue();
-            if (value != null)
-            {
-                return value.doubleValue();
-            }
-        }
         return null;
     }
 
     @Override public String getPriceText()
     {
-        Price price = product.getPrice();
-        Double priceValue = getPrice();
-        if (price != null && priceValue != null)
-        {
-            return THSignedMoney.builder(priceValue)
-                    .currency(price.getCurrency().getSymbol())
-                    .build().toString();
-        }
-        return null;
+        return product.getPrice();
     }
 
     @Override @Nullable public String getDescription()
