0a196b474dad4c14d445ca5dbec364cddd356a52
alex <alex@tradehero.mobi>
2015-09-18 17:57:59 +0800

    new feature of delegation 10 show percent on buy what page

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/chinabuild/mainTab/MainTabBuyWhatAdapter.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/chinabuild/mainTab/MainTabBuyWhatAdapter.java
@@ -22,7 +22,8 @@ public class MainTabBuyWhatAdapter extends BaseAdapter implements View.OnClickLi
     private LeaderboardUserDTOList mDtoList = new LeaderboardUserDTOList();
     private LayoutInflater inflater;
 
-    @Inject public Lazy<PrettyTime> prettyTime;
+    @Inject
+    public Lazy<PrettyTime> prettyTime;
 
     public MainTabBuyWhatAdapter(Context context) {
         DaggerUtils.inject(this);
@@ -50,20 +51,39 @@ public class MainTabBuyWhatAdapter extends BaseAdapter implements View.OnClickLi
             convertView = inflater.inflate(R.layout.main_tab_buy_what_item, null);
         }
         LeaderboardUserDTO item = getItem(i);
+
         ImageView choiceImageView = (ImageView) convertView.findViewById(R.id.icon);
         ImageLoader.getInstance().displayImage(item.picture, choiceImageView, UniversalImageLoader.getAvatarImageLoaderOptions());
+
         TextView userName = (TextView) convertView.findViewById(R.id.user_name);
         userName.setText(item.displayName);
+
         TextView winRate = (TextView) convertView.findViewById(R.id.win_rate);
-        winRate.setText(String.valueOf(item.winRatio*100).substring(0,4)+"%");
+        String winRateString = String.valueOf(item.winRatio * 100);
+        int pointPosition = winRateString.indexOf(".");
+        int end = pointPosition + 3 > winRateString.length() ? winRateString.length() : pointPosition + 3;
+        winRate.setText(winRateString.substring(0, end) + "%");
+
         TextView monthlyRate = (TextView) convertView.findViewById(R.id.monthly_rate);
-        monthlyRate.setText(String.valueOf(item.monthlyRoi*100).substring(0,5)+"%");
+        String monthlyRateString = String.valueOf(item.monthlyRoi * 100);
+        int pointPosition2 = monthlyRateString.indexOf(".");
+        int end2 = pointPosition2 + 3 > monthlyRateString.length() ? monthlyRateString.length() : pointPosition2 + 3;
+        monthlyRate.setText(monthlyRateString.substring(0, end2) + "%");
+
         TextView securityName = (TextView) convertView.findViewById(R.id.security_name);
-        securityName.setText(item.securityName+"("+item.symbol+")");
+        securityName.setText(item.securityName + "(" + item.symbol + ")");
+
         TextView tradePrice = (TextView) convertView.findViewById(R.id.trade_price);
-        tradePrice.setText("成交价格 "+item.price);
+        tradePrice.setText("成交价格 " + item.price);
+
         TextView tradeTime = (TextView) convertView.findViewById(R.id.trade_time);
         tradeTime.setText(prettyTime.get().formatUnrounded(item.dateTimeUtc));
+
+        TextView percent = (TextView) convertView.findViewById(R.id.percent);
+        String percentString = String.valueOf(item.percent * 100);
+        int pointPosition3 = percentString.indexOf(".");
+        int end3 = pointPosition3 + 3 > percentString.length() ? percentString.length() : pointPosition3 + 3;
+        percent.setText(percentString.substring(0, end3) + "%");
 
 //        RelativeLayout upLayout = (RelativeLayout)convertView.findViewById(R.id.up_layout);
 //        upLayout.setOnClickListener(this);
@@ -83,6 +103,6 @@ public class MainTabBuyWhatAdapter extends BaseAdapter implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-        Timber.d("lyl onClick "+v.getId());
+        Timber.d("lyl onClick " + v.getId());
     }
 }
