df5332f487b8a811c9c43d977d52f2e00c610ed4
Li Hao <lihao6485@gmail.com>
2015-10-29 10:55:06 +0800

    update new endpoint for testing

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/NetworkConstants.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/network/NetworkConstants.java
@@ -4,7 +4,7 @@ import com.tradehero.th.utils.Constants;
 
 public class NetworkConstants
 {
-    public static final String TRADEHERO_PROD_ENDPOINT = "https://www.tradehero.mobi/";//"thvm-hadoop.cloudapp.net";//
+    public static final String TRADEHERO_PROD_ENDPOINT = "http://thapi-row-web-prod.chinacloudapp.cn";//"thvm-hadoop.cloudapp.net";//
     public static final String TRADEHERO_PROD_API_ENDPOINT = TRADEHERO_PROD_ENDPOINT + "api/";
     public static final String TRADEHERO_QA_ENDPOINT = "https://th-paas-test-dev1.cloudapp.net/";
     public static final String TRADEHERO_QA_API_ENDPOINT = TRADEHERO_QA_ENDPOINT + "api/";
