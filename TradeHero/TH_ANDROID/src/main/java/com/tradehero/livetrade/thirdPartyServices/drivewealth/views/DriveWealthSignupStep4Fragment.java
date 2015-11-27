9443ab05c1096bb70b31240ac6fa4cfbbd506483
alex <alex@tradehero.mobi>
2015-11-27 11:43:45 +0800

    4.2 10 update chinese string check on dw signup page 4

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/livetrade/thirdPartyServices/drivewealth/views/DriveWealthSignupStep4Fragment.java
+++ TradeHero/TH_ANDROID/src/main/java/com/tradehero/livetrade/thirdPartyServices/drivewealth/views/DriveWealthSignupStep4Fragment.java
@@ -33,6 +33,8 @@ import com.tradehero.th.R;
 import com.tradehero.th.fragments.base.DashboardFragment;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.inject.Inject;
 
 /**
@@ -154,23 +156,10 @@ public class DriveWealthSignupStep4Fragment extends DashboardFragment {
         pushFragment(DriveWealthSignupStep5Fragment.class, new Bundle());
     }
 
-    public boolean isChinese(String str){
-        char[] chars=str.toCharArray();
-        boolean isGB2312=false;
-        for(int i=0;i<chars.length;i++){
-            byte[] bytes=(""+chars[i]).getBytes();
-            if(bytes.length==2){
-                int[] ints=new int[2];
-                ints[0]=bytes[0]& 0xff;
-                ints[1]=bytes[1]& 0xff;
-                if(ints[0]>=0x81 && ints[0]<=0xFE &&
-                        ints[1]>=0x40 && ints[1]<=0xFE){
-                    isGB2312=true;
-                    break;
-                }
-            }
-        }
-        return isGB2312;
+    private boolean isChinese(String text) {
+        Pattern p= Pattern.compile("[\u4e00-\u9fa5]");
+        Matcher m=p.matcher(text);
+        return m.matches();
     }
 
     @OnTextChanged({R.id.lastName, R.id.firstName, R.id.lastNameEnglish, R.id.firstNameEnglish, R.id.idNumber, R.id.address})
