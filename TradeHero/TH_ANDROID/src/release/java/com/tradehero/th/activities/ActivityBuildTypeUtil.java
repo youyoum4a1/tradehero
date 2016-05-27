ce3921f44eadaeae5b22eae1e17f502cc5cef036
Ayush <ayushnvijay@gmail.com>
2016-05-27 16:29:54 +0800

    Fabric(Crashlytics) added

--- TradeHero/TH_ANDROID/src/release/java/com/tradehero/th/activities/ActivityBuildTypeUtil.java
+++ TradeHero/TH_ANDROID/src/release/java/com/tradehero/th/activities/ActivityBuildTypeUtil.java
@@ -2,18 +2,22 @@ package com.tradehero.th.activities;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+
 import com.crashlytics.android.Crashlytics;
 import com.tradehero.th.api.users.UserBaseKey;
 import com.tradehero.th.utils.Constants;
+
 import java.util.Date;
+
+import io.fabric.sdk.android.Fabric;
 
 public class ActivityBuildTypeUtil
 {
     public static void startCrashReports(@NonNull Context context)
     {
-        Crashlytics.start(context);
-        Crashlytics.setString(Constants.TH_CLIENT_TYPE,
-                String.format("%s:%s", Constants.DEVICE_TYPE, Constants.TAP_STREAM_TYPE.name()));
+        Fabric.with(context, new Crashlytics());
+        //Crashlytics.start(context);
+        Crashlytics.setString(Constants.TH_CLIENT_TYPE, String.format("%s:%s", Constants.DEVICE_TYPE, Constants.TAP_STREAM_TYPE.name()));
     }
 
     public static void setUpCrashReports(@NonNull UserBaseKey currentUserKey)
