70a099b63f3151aa7d647fef210aa48675a11357
Ayush <ayushnvijay@gmail.com>
2016-05-27 16:13:45 +0800

    Removed Analytics, adding crashlytics

--- TradeHero/TH_ANDROID/src/main/java/com/tradehero/th/utils/metrics/appsflyer/AppsFlyerConstants.java
+++
@@ -1,7 +0,0 @@
-package com.tradehero.th.utils.metrics.appsflyer;
-
-public class AppsFlyerConstants
-{
-    public static final String REGISTRATION_EMAIL = "registrationByEmail";
-    public static final String REGISTRATION_SOCIAL = "registrationBy%s";
-}
