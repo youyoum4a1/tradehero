3a3f25d4fbbfdf01da7e869a4b8ec6dae91a683e
Jeff Gan <hero.jeff.gan@gmail.com>
2016-09-10 14:50:09 +0800

    Removed Weibo, version bumped, Fabric updated

--- TradeHero/TH_ANDROID/src/main/java/com/androidth/general/auth/weibo/OperatorSsoHandler.java
+++ TradeHero/TH_ANDROID/src/main/java/com/androidth/general/auth/weibo/OperatorSsoHandler.java
@@ -1,51 +1,51 @@
-package com.androidth.general.auth.weibo;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import com.sina.weibo.sdk.auth.WeiboAuth;
-import com.sina.weibo.sdk.auth.WeiboAuthListener;
-import com.sina.weibo.sdk.auth.sso.SsoHandler;
-import com.sina.weibo.sdk.exception.WeiboException;
-import rx.Observable;
-import rx.Subscriber;
-
-public class OperatorSsoHandler implements Observable.OnSubscribe<Bundle>
-{
-    @NonNull private SsoHandler ssoHandler;
-
-    //<editor-fold desc="Constructors">
-    public OperatorSsoHandler(@NonNull Activity activity, @NonNull WeiboAuth weiboAuth)
-    {
-        this.ssoHandler = new SsoHandler(activity, weiboAuth);
-    }
-    //</editor-fold>
-
-    @Override public void call(@NonNull final Subscriber<? super Bundle> subscriber)
-    {
-        ssoHandler.authorize(new WeiboAuthListener()
-        {
-            @Override public void onComplete(Bundle bundle)
-            {
-                subscriber.onNext(bundle);
-                subscriber.onCompleted();
-            }
-
-            @Override public void onWeiboException(WeiboException e)
-            {
-                subscriber.onError(e);
-            }
-
-            @Override public void onCancel()
-            {
-                subscriber.onError(new WeiboCancelledException());
-            }
-        });
-    }
-
-    void authorizeCallBack(int requestCode, int resultCode, Intent data)
-    {
-        ssoHandler.authorizeCallBack(requestCode, resultCode, data);
-    }
-}
+//package com.androidth.general.auth.weibo;
+//
+//import android.app.Activity;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.support.annotation.NonNull;
+////import com.sina.weibo.sdk.auth.WeiboAuth;
+////import com.sina.weibo.sdk.auth.WeiboAuthListener;
+////import com.sina.weibo.sdk.auth.sso.SsoHandler;
+////import com.sina.weibo.sdk.exception.WeiboException;
+//import rx.Observable;
+//import rx.Subscriber;
+//
+//public class OperatorSsoHandler implements Observable.OnSubscribe<Bundle>
+//{
+//    @NonNull private SsoHandler ssoHandler;
+//
+//    //<editor-fold desc="Constructors">
+//    public OperatorSsoHandler(@NonNull Activity activity, @NonNull WeiboAuth weiboAuth)
+//    {
+//        this.ssoHandler = new SsoHandler(activity, weiboAuth);
+//    }
+//    //</editor-fold>
+//
+//    @Override public void call(@NonNull final Subscriber<? super Bundle> subscriber)
+//    {
+//        ssoHandler.authorize(new WeiboAuthListener()
+//        {
+//            @Override public void onComplete(Bundle bundle)
+//            {
+//                subscriber.onNext(bundle);
+//                subscriber.onCompleted();
+//            }
+//
+//            @Override public void onWeiboException(WeiboException e)
+//            {
+//                subscriber.onError(e);
+//            }
+//
+//            @Override public void onCancel()
+//            {
+//                subscriber.onError(new WeiboCancelledException());
+//            }
+//        });
+//    }
+//
+//    void authorizeCallBack(int requestCode, int resultCode, Intent data)
+//    {
+//        ssoHandler.authorizeCallBack(requestCode, resultCode, data);
+//    }
+//}
--- TradeHero/TH_ANDROID/src/main/java/com/androidth/general/auth/weibo/WeiboAuthenticationProvider.java
+++ TradeHero/TH_ANDROID/src/main/java/com/androidth/general/auth/weibo/WeiboAuthenticationProvider.java
@@ -7,8 +7,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.androidth.general.network.share.SocialConstants;
-import com.sina.weibo.sdk.auth.Oauth2AccessToken;
-import com.sina.weibo.sdk.auth.WeiboAuth;
+//import com.sina.weibo.sdk.auth.Oauth2AccessToken;
+//import com.sina.weibo.sdk.auth.WeiboAuth;
 import com.androidth.general.common.activities.ActivityResultRequester;
 import com.androidth.general.api.social.SocialNetworkEnum;
 import com.androidth.general.auth.AuthData;
@@ -31,7 +31,7 @@ public class WeiboAuthenticationProvider extends SocialAuthenticationProvider
     private static final String WEIBO_PACKAGE = "com.sina.weibo";
 
     @NonNull private final WeiboAppAuthData appAuthData;
-    @Nullable private OperatorSsoHandler operatorSsoHandler;
+//    @Nullable private OperatorSsoHandler operatorSsoHandler;
 
     //<editor-fold desc="Constructors">
     @Inject public WeiboAuthenticationProvider(
@@ -45,7 +45,7 @@ public class WeiboAuthenticationProvider extends SocialAuthenticationProvider
     @Override public void logout()
     {
         // FIXME Nothing to do?
-        operatorSsoHandler = null;
+//        operatorSsoHandler = null;
     }
 
     @Override protected Observable<AuthData> createAuthDataObservable(final Activity activity)
@@ -59,40 +59,41 @@ public class WeiboAuthenticationProvider extends SocialAuthenticationProvider
         }
         else
         {
-            operatorSsoHandler = new OperatorSsoHandler(
-                    activity,
-                    new WeiboAuth(activity, appAuthData.appId, appAuthData.redirectUrl, appAuthData.scope));
-            return Observable.create(operatorSsoHandler)
-                    .map(new Func1<Bundle, AuthData>()
-                    {
-                        @Override public AuthData call(Bundle bundle)
-                        {
-                            operatorSsoHandler = null;
-                            Oauth2AccessToken token = Oauth2AccessToken.parseAccessToken(bundle);
-                            if (token.isSessionValid())
-                            {
-                                return new AuthData(
-                                        SocialNetworkEnum.WB,
-                                        null, // TODO expiry
-                                        token.getToken());
-                            }
-                            Timber.e(new Exception(), "Token is not valid");
-                            // 以下几种情况，您会收到 Code：
-                            // 1. 当您未在平台上注册的应用程序的包名与签名时；
-                            // 2. 当您注册的应用程序包名与签名不正确时；
-                            // 3. 当您在平台上注册的包名和签名与您当前测试的应用的包名和签名不匹配时。
-                            throw new RuntimeException("Token is invalid");
-                        }
-                    });
+//            operatorSsoHandler = new OperatorSsoHandler(
+//                    activity,
+//                    new WeiboAuth(activity, appAuthData.appId, appAuthData.redirectUrl, appAuthData.scope));
+//            return Observable.create(operatorSsoHandler)
+//                    .map(new Func1<Bundle, AuthData>()
+//                    {
+//                        @Override public AuthData call(Bundle bundle)
+//                        {
+//                            operatorSsoHandler = null;
+//                            Oauth2AccessToken token = Oauth2AccessToken.parseAccessToken(bundle);
+//                            if (token.isSessionValid())
+//                            {
+//                                return new AuthData(
+//                                        SocialNetworkEnum.WB,
+//                                        null, // TODO expiry
+//                                        token.getToken());
+//                            }
+//                            Timber.e(new Exception(), "Token is not valid");
+//                            // 以下几种情况，您会收到 Code：
+//                            // 1. 当您未在平台上注册的应用程序的包名与签名时；
+//                            // 2. 当您注册的应用程序包名与签名不正确时；
+//                            // 3. 当您在平台上注册的包名和签名与您当前测试的应用的包名和签名不匹配时。
+//                            throw new RuntimeException("Token is invalid");
+//                        }
+//                    });
+            return null;
         }
     }
 
     @Override public void onActivityResult(@NonNull Activity activity, int requestCode, int resultCode, Intent data)
     {
-        // Request code is always 32973 ? SsoHandler.REQUEST_CODE_SSO_AUTH
-        if (operatorSsoHandler != null)
-        {
-            operatorSsoHandler.authorizeCallBack(requestCode, resultCode, data);
-        }
+//        // Request code is always 32973 ? SsoHandler.REQUEST_CODE_SSO_AUTH
+//        if (operatorSsoHandler != null)
+//        {
+//            operatorSsoHandler.authorizeCallBack(requestCode, resultCode, data);
+//        }
     }
 }
