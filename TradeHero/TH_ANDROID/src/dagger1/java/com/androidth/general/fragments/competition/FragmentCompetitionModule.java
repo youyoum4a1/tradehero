6605d34dc3dfee3aba7ebb259e586297d2c4eb04
Jeff Gan <hero.jeff.gan@gmail.com>
2016-10-19 13:52:38 +0800

    Conflicts merged

--- TradeHero/TH_ANDROID/src/dagger1/java/com/androidth/general/fragments/competition/FragmentCompetitionModule.java
+++ TradeHero/TH_ANDROID/src/dagger1/java/com/androidth/general/fragments/competition/FragmentCompetitionModule.java
@@ -1,10 +1,7 @@
 package com.androidth.general.fragments.competition;
 
 import com.androidth.general.fragments.competition.zone.FragmentCompetitionZoneModule;
-<<<<<<< HEAD
 import com.androidth.general.fragments.live.LiveViewFragment;
-=======
->>>>>>> 2c39b22b64dcfe1b9cf42f64f9cf4e997862756f
 
 import dagger.Module;
 
