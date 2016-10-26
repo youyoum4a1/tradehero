8ed19f77aca2aebb191624d8ab1d01e222af6a60
Jeff Gan <hero.jeff.gan@gmail.com>
2016-10-26 15:10:58 +0800

    Live PositionDto setup

--- TradeHero/TH_ANDROID/src/tradeHeroLive/java/com/androidth/general/api/live1b/LivePositionDTO.java
+++ TradeHero/TH_ANDROID/src/tradeHeroLive/java/com/androidth/general/api/live1b/LivePositionDTO.java
@@ -1,11 +1,13 @@
 package com.androidth.general.api.live1b;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 
 import com.androidth.general.api.security.SecurityCompactDTO;
 import com.androidth.general.common.persistence.DTO;
 
-public class LivePositionDTO implements DTO {
+public class LivePositionDTO implements DTO, Parcelable{
 
     public String OrderId;
     public Integer Side;
@@ -17,7 +19,7 @@ public class LivePositionDTO implements DTO {
     public Double StopLoss;
     public Double TakeProfit;
     public Integer PositionResponseType;
-    @Nullable public LiveSecurityCompactDTO SecurityCompactDto;
+    @Nullable public SecurityCompactDTO SecurityCompactDto;
 //    public PositionResponseTypeEnum PositionResponseType;
 //    @JsonProperty("Side")
 //    public OrderSideEnum side;
@@ -43,19 +45,10 @@ public class LivePositionDTO implements DTO {
     public LivePositionDTO()
     {
     }
-    //</editor-fold>
 
-//    @JsonIgnore
-//    public PositionDTOKey getLiveOwnedPositionId()
-//    {
-//        return new LiveOwnedPositionId(0,0,0);
-//    }
-//
-//    @JsonIgnore
-//    public PositionDTOKey getLivePositionDTOKey()
-//    {
-//        return getLiveOwnedPositionId();
-//    }
+    public LivePositionDTO(SecurityCompactDTO securityCompactDto) {
+        SecurityCompactDto = securityCompactDto;
+    }
 
     @Override
     public String toString() {
@@ -70,28 +63,59 @@ public class LivePositionDTO implements DTO {
                 ", StopLoss=" + StopLoss +
                 ", TakeProfit=" + TakeProfit +
                 ", PositionResponseType=" + PositionResponseType +
+                ", SecurityCompactDto=" + SecurityCompactDto +
                 '}';
     }
 
-    public class LiveSecurityCompactDTO {
-        public Integer id;
-        public String symbol;
-        public Integer securityType;
-        public String name;
-        public String country;
-        public Integer exchangeId;
-        public String exchange;
-        public String yahooSymbol;
-        public String reutersSymbol;
-        public String chartDataSource;
-        public String currencyDisplay;
-        public String currencyISO;
-        public String parentCurrencyISO;
-        public String symbol_ay;
-        public String id_ay;
-        @Nullable public Double marketCap;
-        @Nullable public Double lastPrice;
-        public Double risePercent;
-        public String imageBlobUrl;
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
+
+    //Parcelable implementations
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.OrderId);
+        dest.writeValue(this.Side);
+        dest.writeValue(this.Qty);
+        dest.writeString(this.Product);
+        dest.writeString(this.SecurityId);
+        dest.writeValue(this.EntryPrice);
+        dest.writeValue(this.UPL);
+        dest.writeValue(this.StopLoss);
+        dest.writeValue(this.TakeProfit);
+        dest.writeValue(this.PositionResponseType);
+        dest.writeParcelable(this.SecurityCompactDto, flags);
+    }
+
+    protected LivePositionDTO(Parcel in) {
+        this.OrderId = in.readString();
+        this.Side = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.Qty = (Double) in.readValue(Double.class.getClassLoader());
+        this.Product = in.readString();
+        this.SecurityId = in.readString();
+        this.EntryPrice = (Double) in.readValue(Double.class.getClassLoader());
+        this.UPL = (Double) in.readValue(Double.class.getClassLoader());
+        this.StopLoss = (Double) in.readValue(Double.class.getClassLoader());
+        this.TakeProfit = (Double) in.readValue(Double.class.getClassLoader());
+        this.PositionResponseType = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.SecurityCompactDto = in.readParcelable(SecurityCompactDTO.class.getClassLoader());
+    }
+
+    public static final Creator<LivePositionDTO> CREATOR = new Creator<LivePositionDTO>() {
+        @Override
+        public LivePositionDTO createFromParcel(Parcel source) {
+            return new LivePositionDTO(source);
+        }
+
+        @Override
+        public LivePositionDTO[] newArray(int size) {
+            return new LivePositionDTO[size];
+        }
+    };
 }
